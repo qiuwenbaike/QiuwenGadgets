380e075b122f0e7e6a752ec504f6a7e8c53ed2ff
安忆 <i@anyi.in>
2023-10-29 19:44:22 +0800

    refactor(AccessKeyCheatsheet): separate into modules

---
+++ src/AccessKeyCheatsheet/modules/core.ts
@@ -0,0 +1,63 @@
+import {getMessage} from './i18n';
+
+const $accessKeysTableItems: JQuery[] = $('[accesskey]')
+	.map((_index: number, element: HTMLElement): JQuery => {
+		return $('<tr>').append(
+			$('<th>').text(element.accessKey.toUpperCase()),
+			$('<td>').text(
+				(
+					element.getAttribute('aria-label') ||
+					element.title ||
+					element.textContent ||
+					(element as HTMLInputElement).value ||
+					$(`label[for="${element.id}"]`).text()
+				).replace(/\s?\[.+?\]/, '')
+			)
+		);
+	})
+	.get();
+
+const $accessKeysTable: JQuery = $('<table>').addClass('wikitable').css('width', '100%').append($accessKeysTableItems);
+
+let messageDialog: OO.ui.MessageDialog;
+
+const messageDialogProperty: OO.ui.WindowManager.WindowOpeningData = {
+	title: getMessage('Title'),
+	message: $accessKeysTable,
+	actions: [
+		{
+			action: 'close',
+			flags: ['safe', 'close'],
+			label: getMessage('Close'),
+		},
+	],
+	size: 'medium',
+};
+
+const $accessKeyCheatsheetOpener: JQuery = $('<a>')
+	.attr('accesskey', '?')
+	.text(getMessage('Text'))
+	.hide()
+	.on('click', (event: JQuery.ClickEvent): void => {
+		event.preventDefault();
+		if (messageDialog) {
+			if (messageDialog.isOpened()) {
+				messageDialog.close();
+			} else {
+				messageDialog.open(messageDialogProperty);
+			}
+			return;
+		}
+		messageDialog = new OO.ui.MessageDialog();
+		const windowManager: OO.ui.WindowManager = new OO.ui.WindowManager();
+		windowManager.$element.css('width', '35em').appendTo(document.body);
+		windowManager.addWindows([messageDialog]);
+		windowManager.openWindow(messageDialog, messageDialogProperty);
+	});
+
+/**
+ * @description Adds alt+shift+? as an access key to show a list of all default access keys
+ */
+export const accessKeyCheatsheet = (): void => {
+	$accessKeyCheatsheetOpener.appendTo(document.body);
+};
