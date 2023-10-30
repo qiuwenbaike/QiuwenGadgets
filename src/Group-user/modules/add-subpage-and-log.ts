c4a17d13c532f9d6452439af91ae9aeddc90a9ee
WaitSpring <me@waitspring.com>
2023-10-30 16:56:15 +0800

    chore(Group-user): add dependencies
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/Group-user/modules/add-subpage-and-log.ts
+++ src/Group-user/modules/add-subpage-and-log.ts
@@ -1,21 +1,23 @@
 export const addSubpageAndLog = (): void => {
-	/* 向侧边栏添加“子页面”“日志”菜单 */
-	const NSWithoutSubpages = [-1, 0, 14];
-	if (!NSWithoutSubpages.includes(mw.config.get('wgNamespaceNumber'))) {
-		mw.util.addPortletLink(
-			'p-tb',
-			`/wiki/Special:PrefixIndex/${mw.config.get('wgPageName')}`,
-			window.wgULS('子页面', '子頁面'),
-			't-subpage'
-		);
-	}
-	const NSWithLog = [-1, 2, 3];
-	if (!NSWithLog.includes(mw.config.get('wgNamespaceNumber'))) {
-		mw.util.addPortletLink(
-			'p-tb',
-			`/wiki/Special:Logs?page=${mw.config.get('wgPageName')}`,
-			window.wgULS('日志', '日誌'),
-			't-log'
-		);
-	}
+	mw.loader.using(['ext.gadget.i18n']).done(() => {
+		/* 向侧边栏添加“子页面”“日志”菜单 */
+		const NSWithoutSubpages = [-1, 0, 14];
+		if (!NSWithoutSubpages.includes(mw.config.get('wgNamespaceNumber'))) {
+			mw.util.addPortletLink(
+				'p-tb',
+				`/wiki/Special:PrefixIndex/${mw.config.get('wgPageName')}`,
+				window.wgULS('子页面', '子頁面'),
+				't-subpage'
+			);
+		}
+		const NSWithLog = [-1, 2, 3];
+		if (!NSWithLog.includes(mw.config.get('wgNamespaceNumber'))) {
+			mw.util.addPortletLink(
+				'p-tb',
+				`/wiki/Special:Logs?page=${mw.config.get('wgPageName')}`,
+				window.wgULS('日志', '日誌'),
+				't-log'
+			);
+		}
+	});
 };
