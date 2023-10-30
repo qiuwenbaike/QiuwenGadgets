b63926df2b57079c02ba63839ad1fb535d23b01f
WaitSpring <me@waitspring.com>
2023-10-30 23:19:07 +0800

    chore(util.ts): remove unused code
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/util.ts
+++ src/util.ts
@@ -2,14 +2,4 @@ const ding = (value: string, autoHide = false, type: 'default' | 'info' | 'succe
 	autoHide ? DingExposedInterface(value, type) : DingExposedInterface(value, type, 'long');
 };
 
-const cleanSvgText = (svg: string): string => {
-	const doc: Document = new DOMParser().parseFromString(svg, 'application/xml');
-	const element: HTMLElement = document.importNode(doc.documentElement, true);
-	return element.outerHTML;
-};
-
-const generateSvgDataUrl = (svg: string, prefix = 'data:image/svg+xml;'): string => {
-	return `${prefix}${encodeURIComponent(cleanSvgText(svg))}`;
-};
-
-export {ding, generateSvgDataUrl};
+export {ding};
