4909fe30798381fdc1a3a9ea30d5c3a76a77a6ac
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-11-01 00:43:50 +0800

    fix(GadgetsDefinition): remove unneeded wrap and redundant code (#97)

--- src/GadgetsDefinition/modules/core.ts
+++ src/GadgetsDefinition/modules/core.ts
@@ -87,21 +87,3 @@ export const processGadgetDefinition = (innerHTML: string) => {
 			})
 	);
 };
-
-$(() => {
-	const $parserOutput = $('.mw-parser-output');
-	// Process gadget definitions in lists.
-	$parserOutput.find('li').each((_index, element) => {
-		// Add id so that gadget definitions can be highlighted when we click a link
-		// to them.
-		const gadgetName = getGadgetName(element.innerHTML);
-		if (gadgetName) {
-			element.id = makeGadgetId(gadgetName);
-		}
-		element.innerHTML = processGadgetDefinition(element.innerHTML);
-	});
-	// Process gadget definitions in pre tags.
-	$parserOutput.find('pre').each((_index, element) => {
-		element.innerHTML = element.innerHTML.replace(/[^\n]+/g, processGadgetDefinition);
-	});
-});
