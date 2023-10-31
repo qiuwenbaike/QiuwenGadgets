a4243bff72fe72d2bd04241e3f3aad90e8b7f7fa
安忆 <i@anyi.in>
2023-11-01 01:08:24 +0800

    chore(AutoLink): revert b1c1266

--- src/AutoLink/AutoLink.ts
+++ src/AutoLink/AutoLink.ts
@@ -1,23 +1,8 @@
-import {
-	IS_DIFF_ACTION,
-	IS_WG_EDIT_OR_SUBMIT_ACTION,
-	IS_WG_HISTORY_ACTION,
-	IS_WG_SPECIAL_PAGE,
-	WG_CANONICAL_NAMESPACE,
-} from './modules/constant';
+import {IS_WG_SPECIAL_PAGE, WG_CANONICAL_NAMESPACE} from './modules/constant';
 import {TargetElements, getTargetElements} from './modules/util/getTargetElements';
 import {processElement} from './modules/processElement';
 
 $(function autoLink(): void {
-	if (
-		WG_CANONICAL_NAMESPACE !== 'Special' &&
-		!IS_WG_HISTORY_ACTION &&
-		!IS_WG_EDIT_OR_SUBMIT_ACTION &&
-		!IS_DIFF_ACTION
-	) {
-		return; // Disabled in non-special pages with non-defined actions
-	}
-
 	// Special crono pages where this script is enabled
 	if (WG_CANONICAL_NAMESPACE === 'Special' && !IS_WG_SPECIAL_PAGE) {
 		return; // Disabled in the other special pages
