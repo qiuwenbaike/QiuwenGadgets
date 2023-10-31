f5ba489c34943b096d90efe34ed8f81a027761e2
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-10-31 19:10:31 +0800

    refactor(GadgetsDefinition): move load funtion to GadgetsDefinition.ts (#88)

--- src/GadgetsDefinition/GadgetsDefinition.ts
+++ src/GadgetsDefinition/GadgetsDefinition.ts
@@ -1,4 +1,4 @@
-import {gadgetsDefinition} from './modules/core';
+import {getGadgetName, makeGadgetId, processGadgetDefinition} from './modules/core';
 
 (() => {
 	// Only operate on [[MediaWiki:Gadgets-definition]] when the text is visible.
@@ -11,5 +11,26 @@ import {gadgetsDefinition} from './modules/core';
 	) {
 		return;
 	}
-	gadgetsDefinition();
+
+	const gadgetsDefinitionLoad = () => {
+		() => {
+			const $parserOutput = $('.mw-parser-output');
+			// Process gadget definitions in lists.
+			$parserOutput.find('li').each((_index, element) => {
+				// Add id so that gadget definitions can be highlighted when we click a link
+				// to them.
+				const gadgetName = getGadgetName(element.innerHTML);
+				if (gadgetName) {
+					element.id = makeGadgetId(gadgetName);
+				}
+				element.innerHTML = processGadgetDefinition(element.innerHTML);
+			});
+			// Process gadget definitions in pre tags.
+			$parserOutput.find('pre').each((_index, element) => {
+				element.innerHTML = element.innerHTML.replace(/[^\n]+/g, processGadgetDefinition);
+			});
+		};
+	};
+
+	$(gadgetsDefinitionLoad);
 })();
