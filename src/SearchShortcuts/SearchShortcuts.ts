f6330fa1baa18d5dbea74b80efc17d2bfc5610d9
WaitSpring <me@waitspring.com>
2023-10-31 22:53:34 +0800

    refactor(SearchShortcuts): split core and index

--- src/SearchShortcuts/SearchShortcuts.ts
+++ src/SearchShortcuts/SearchShortcuts.ts
@@ -1,38 +1,5 @@
-$(function searchShortcuts(): void {
-	const $searchInput: JQuery = $('#searchInput');
-	const searchShortcutsMain = (): void => {
-		const $searchInputVal = String($searchInput.val());
-		if (!$searchInputVal) {
-			return;
-		}
-		const $searchInputValPure = $searchInputVal.trim();
-		if (!/^\{\{[^{}]+}}$/.test($searchInputValPure)) {
-			return;
-		}
-		if ($searchInputValPure.startsWith('{{#')) {
-			if ($searchInputValPure.startsWith('{{#invoke:')) {
-				$searchInput.val(
-					$searchInputVal
-						.replace('{{#invoke:', 'Module:')
-						.replace(/\s*\|.*/, '')
-						.replace('}}', '')
-				);
-			} else {
-				$searchInput.val(
-					$searchInputVal
-						.replace('{{#', 'H:MW#')
-						.replace(/\s*\|.*/, '')
-						.replace('}}', '')
-				);
-			}
-		} else {
-			$searchInput.val(
-				$searchInputVal
-					.replace('{{', 'Template:')
-					.replace(/\s*\|.*/, '')
-					.replace('}}', '')
-			);
-		}
-	};
+import {$searchInput, searchShortcutsMain} from './modules/core';
+
+$(() => {
 	$searchInput.on('keydown', searchShortcutsMain);
 });
