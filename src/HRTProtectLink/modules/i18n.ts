b8b9f2c5d028e94c77e61bab51ce9293be03542a
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-10-31 12:00:30 +0800

    refactor(HRTProtectLink): split i18n and core; add index; update LICENSE (#75)
    Signed-off-by: WaitSpring <me@waitspring.com>

---
+++ src/HRTProtectLink/modules/i18n.ts
@@ -0,0 +1,26 @@
+const getI18nMessages: GetI18nMessages = () => {
+	const {localize} = i18n;
+	return {
+		HRT: localize({
+			en: 'High-risk template',
+			'zh-hans': '高风险模板',
+			'zh-hant': '高風險模板',
+		}),
+		'Full-protect HRT': localize({
+			en: 'Enforce full-protection to this high-risk template',
+			'zh-hans': '全保护高风险模板',
+			'zh-hant': '全保護高風險模板',
+		}),
+		'Semi-protect HRT': localize({
+			en: 'Enforce semi-protection to this high-risk template',
+			'zh-hans': '半保护高风险模板',
+			'zh-hant': '半保護高風險模板',
+		}),
+	};
+};
+
+const i18nMessages = getI18nMessages();
+
+export const getMessage = (key: string): string => {
+	return i18nMessages[key] || key;
+};
