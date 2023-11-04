dbdaea4210d7cbf2f299a277fc0c33bac0fea774
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-11-04 15:21:08 +0800

    fix(defaultSummaries): remove `actions` from definition.json (#147)
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/DefaultSummaries/DefaultSummaries.ts
+++ src/DefaultSummaries/DefaultSummaries.ts
@@ -1,3 +1,13 @@
 import {defaultSummariesLoad} from './modules/core';
 
-$(defaultSummariesLoad);
+if (['edit', 'submit'].includes(mw.config.get('wgAction'))) {
+	$(defaultSummariesLoad);
+} else {
+	mw.loader.using(['ext.visualEditor.desktopArticleTarget.init'], (): void => {
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		/* @ts-ignore */
+		mw.libs.ve.addPlugin((): void => {
+			$(defaultSummariesLoad);
+		});
+	});
+}
