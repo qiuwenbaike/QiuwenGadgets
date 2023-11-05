31cc9b631862b7bf4c7f0754c9c84aa689222b72
安忆 <i@anyi.in>
2023-11-06 02:36:35 +0800

    refactor(CopyCodeBlock): separate into modules

---
+++ src/CopyCodeBlock/modules/constant.ts
@@ -0,0 +1,5 @@
+const CLASS_CODE_BLOCK = 'copycodeblock__code-block';
+const CLASS_COPY_BUTTON = 'copycodeblock__copy-button';
+const NOTIFY_TAG = 'ext.gadget.CopyCodeBlock';
+
+export {CLASS_CODE_BLOCK, CLASS_COPY_BUTTON, NOTIFY_TAG};
