b2c1ed771d2a551c4a77aef79c9282c97e54d457
安忆 <i@anyi.in>
2023-11-06 00:53:50 +0800

    refactor(ConfirmLogout): separate into modules

--- src/ConfirmLogout/modules/logout.ts
+++ src/ConfirmLogout/modules/logout.ts
@@ -1,9 +1,8 @@
-import {ding, initMwApi} from '../../util';
-import {WG_WIKI_ID} from './constant';
+import {api} from './api';
+import {ding} from '../../util';
 import {getMessage} from './i18n';
 
 const logout = async (): Promise<void> => {
-	const api: mw.Api = initMwApi(`Qiuwen/1.1 (ConfirmLogout/1.1; ${WG_WIKI_ID})`);
 	try {
 		await api.postWithEditToken({
 			action: 'logout',
