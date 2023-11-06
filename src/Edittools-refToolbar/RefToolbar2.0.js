3be6d1f470ad216c8aef3060533d465ea4866157
安忆 <i@anyi.in>
2023-11-06 21:24:58 +0800

    chore(RefToolbar2.0): format code (#167)

--- src/Edittools-refToolbar/RefToolbar2.0.js
+++ src/Edittools-refToolbar/RefToolbar2.0.js
@@ -954,7 +954,9 @@ export const refToolbar2 = () => {
 	};
 
 	// Escape spaces and quotes for use in HTML classes/ids
-	CiteTB.escStr = (s) => {return s.replace(' ', '-').replace("'", "\\'").replace('"', '\\"')};
+	CiteTB.escStr = (s) => {
+		return s.replace(' ', '-').replace("'", "\\'").replace('"', '\\"');
+	};
 
 	// MISC FUNCTIONS
 	// Determine which template form is open, and get the template object for it
