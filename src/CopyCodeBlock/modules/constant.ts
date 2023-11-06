c1d7578575e2acd51dded09098db213a2190bd88
安忆 <i@anyi.in>
2023-11-06 21:22:15 +0800

    refactor(CopyCodeBlock): css names use BEM code naming conventions

--- src/CopyCodeBlock/modules/constant.ts
+++ src/CopyCodeBlock/modules/constant.ts
@@ -1,5 +1,5 @@
-const CLASS_CODE_BLOCK = 'copycodeblock__code-block';
-const CLASS_COPY_BUTTON = 'copycodeblock__copy-button';
+const CLASS_CODE_BLOCK = 'gadget-copy_code_block__code-block';
+const CLASS_COPY_BUTTON = 'gadget-copy_code_block__copy-button';
 const NOTIFY_TAG = 'ext.gadget.CopyCodeBlock';
 
 export {CLASS_CODE_BLOCK, CLASS_COPY_BUTTON, NOTIFY_TAG};
