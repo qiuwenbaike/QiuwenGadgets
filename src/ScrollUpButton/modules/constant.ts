36359eba87360aa8201144b716e36da2e0c3cb87
安忆 <i@anyi.in>
2023-11-08 05:26:24 +0800

    refactor(ScrollUpButton): separate into modules (#205)

---
+++ src/ScrollUpButton/modules/constant.ts
@@ -0,0 +1,5 @@
+const CLASS_BUTTON = 'gadget-scroll_up_button';
+const CLASS_BUTTON_DOWN = `${CLASS_BUTTON}--down`;
+const CLASS_BUTTON_UP = `${CLASS_BUTTON}--up`;
+
+export {CLASS_BUTTON, CLASS_BUTTON_DOWN, CLASS_BUTTON_UP};
