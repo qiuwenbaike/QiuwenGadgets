e608abd64dd5b1552cf64af839938feeabfc627b
WaitSpring <me@waitspring.com>
2023-11-07 22:55:09 +0800

    feat(ScrollUpButton): add tip using `tippy.js`
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/ScrollUpButton/modules/core.ts
+++ src/ScrollUpButton/modules/core.ts
@@ -3,7 +3,6 @@ import {getMessage} from './i18n';
 import scrollButton from '../images/scroll-button.svg';
 
 export const scrollUpButton = (): void => {
-	const $window: JQuery<Window> = $(window);
 	const changeOpacity = (event: JQuery.TriggeredEvent): void => {
 		event.currentTarget.style.opacity = event.type === 'mouseenter' ? '1' : '0.7';
 	};
@@ -15,6 +14,7 @@ export const scrollUpButton = (): void => {
 			660
 		);
 	};
+
 	const $scrollButton: JQuery = $('<img>')
 		.addClass('noprint')
 		.attr({
@@ -29,7 +29,7 @@ export const scrollUpButton = (): void => {
 			title: getMessage('ToBottom'),
 		})
 		.on('click', (): void => {
-			scrollTop(($(document).height() ?? 0) - ($window.height() ?? 0));
+			scrollTop(($(document).height() ?? 0) - ($(window).height() ?? 0));
 		});
 	const $scrollUpButton: JQuery = $scrollButton
 		.clone()
@@ -41,10 +41,25 @@ export const scrollUpButton = (): void => {
 		.on('click', (): void => {
 			scrollTop(0);
 		});
+
 	for (const $element of [$scrollDownButton, $scrollUpButton]) {
 		$element.on('mouseenter mouseleave', changeOpacity).appendTo(document.body);
 	}
-	$window.on('scroll selectionchange', (): void => {
+
+	mw.loader.using('ext.gadget.Tippy').then(() => {
+		window.tippy($scrollUpButton.get(0) as HTMLElement, {
+			arrow: true,
+			content: getMessage('ToTop'),
+			placement: 'left',
+		});
+		window.tippy($scrollDownButton.get(0) as HTMLElement, {
+			arrow: true,
+			content: getMessage('ToBottom'),
+			placement: 'left',
+		});
+	});
+
+	const scrollListener = (): void => {
 		if ($('#cat_a_lot').length || $('#proveit').length || $('.wordcount').length) {
 			$scrollDownButton.css('bottom', '78px');
 			$scrollUpButton.css('bottom', '120px');
@@ -52,5 +67,7 @@ export const scrollUpButton = (): void => {
 			$scrollDownButton.css('bottom', '36px');
 			$scrollUpButton.css('bottom', '78px');
 		}
-	});
+	};
+	const scrollListenerWithToggle = mw.util.throttle(scrollListener, 200);
+	$(window).on('scroll selectionchange', scrollListenerWithToggle);
 };
