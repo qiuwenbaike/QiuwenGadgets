19eb0e17a65185ddf8fba3938450fc68d436c528
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-11-07 14:31:51 +0800

    1107 svgo (#187)
    * build: update SVGO settings
    
    Signed-off-by: WaitSpring <me@waitspring.com>
    
    * chore: minify SVGs
    
    Signed-off-by: WaitSpring <me@waitspring.com>
    
    ---------
    
    Signed-off-by: WaitSpring <me@waitspring.com>

--- svgo.config.cjs
+++ svgo.config.cjs
@@ -2,9 +2,8 @@
 module.exports = {
 	js2svg: {
 		eol: 'lf',
-		finalNewline: true,
+		finalNewline: false,
 		indent: '\t',
-		pretty: true,
 	},
 	multipass: true,
 	plugins: [
@@ -12,19 +11,11 @@ module.exports = {
 			name: 'preset-default',
 			params: {
 				overrides: {
-					cleanupNumericValues: false,
 					collapseGroups: false,
 					convertPathData: false,
 					convertTransform: false,
 					inlineStyles: false,
 					minifyStyles: false,
-					removeComments: false,
-					removeDesc: false,
-					removeEditorsNSData: false,
-					removeEmptyContainers: false,
-					removeHiddenElems: false,
-					removeMetadata: false,
-					removeTitle: false,
 					removeUnknownsAndDefaults: false,
 					removeViewBox: false,
 					removeXMLProcInst: false,
