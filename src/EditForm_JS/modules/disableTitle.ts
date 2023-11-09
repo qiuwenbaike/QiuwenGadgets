8dd38558fb7db904e37e6191d751db3e60fa3d97
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-11-09 18:45:29 +0800

    fix(EditForm_JS): add items to noSectionTitlePages[] (#237)
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/EditForm_JS/modules/disableTitle.ts
+++ src/EditForm_JS/modules/disableTitle.ts
@@ -7,8 +7,13 @@ export const disableTitle = (): void => {
 		$wpSummary.val('');
 	}
 	// 可视化编辑器 / 新wikitext模式
-	const noSectionTitlePages: string[] = [];
-	if (noSectionTitlePages.includes(mw.config.get('wgPageName')) && mw.util.getParamValue('section') === 'new') {
+	const noSectionTitlePages: string[] = ['Qiuwen_talk:侵权提报', 'Qiuwen_talk:存废讨论/关注度提报'];
+	const noSectionTitlePagesRegex = /^Qiuwen_talk:存废讨论/;
+	if (
+		(noSectionTitlePages.includes(mw.config.get('wgPageName')) ||
+			noSectionTitlePagesRegex.test(mw.config.get('wgPageName'))) &&
+		mw.util.getParamValue('section') === 'new'
+	) {
 		mw.util.addCSS('h2.ve-ui-init-desktopArticleTarget-sectionTitle{display:none}');
 	}
 };
