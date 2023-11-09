91d99bafb1cc21da4d73f45a04d50173c327094a
WaitSpring <me@waitspring.com>
2023-11-09 18:44:23 +0800

    fix(EditForm_JS): add items to noSectionTitlePages[]
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/Group-user_JS/modules/loadEditFormJs.ts
+++ src/Group-user_JS/modules/loadEditFormJs.ts
@@ -1,6 +1,9 @@
 export const loadEditFormJS = (): void => {
 	/* 加载编辑界面脚本 */
-	if (['edit', 'submit'].includes(mw.config.get('wgAction'))) {
+	if (
+		['edit', 'submit'].includes(mw.config.get('wgAction')) ||
+		(mw.config.get('wgAction') === 'view' && document.getElementById('ca-addsection'))
+	) {
 		mw.loader.load(['ext.gadget.EditForm', 'ext.gadget.EditForm_JS']);
 	} else {
 		mw.loader.using(['ext.visualEditor.desktopArticleTarget.init'], (): void => {
