61483d1ab2af0a323481a116527639c265b70143
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-11-10 14:32:04 +0800

    fix: typo (no top-level async); replace with `mw.loader.load` (#257)
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/ToolsRedirect-opt-bolds/ToolsRedirect-opt-bolds.ts
+++ src/ToolsRedirect-opt-bolds/ToolsRedirect-opt-bolds.ts
@@ -3,10 +3,11 @@ import {initMwApi} from '../util';
 if (!mw.user.options.get('gadget-ToolsRedirect')) {
 	const api = initMwApi(`Qiuwen/1.1 (ToolsRedirect-opt-bolds/2.0; ${mw.config.get('wgWikiID')})`);
 
-	await api.postWithEditToken({
+	api.postWithEditToken({
 		action: 'options',
 		format: 'json',
 		change: 'gadget-ToolsRedirect=1',
+	}).done(() => {
+		mw.loader.load('ext.gadget.ToolsRedirect');
 	});
-	mw.loader.using('ext.gadget.ToolsRedirect');
 }
--- src/Wikiplus-highlight/Wikiplus-highlight.ts
+++ src/Wikiplus-highlight/Wikiplus-highlight.ts
@@ -3,10 +3,11 @@ import {initMwApi} from '../util';
 if (!mw.user.options.get('gadget-Wikiplus')) {
 	const api = initMwApi(`Qiuwen/1.1 (Wikiplus-highlight/2.0; ${mw.config.get('wgWikiID')})`);
 
-	await api.postWithEditToken({
+	api.postWithEditToken({
 		action: 'options',
 		format: 'json',
 		change: 'gadget-Wikiplus=1',
+	}).done(() => {
+		mw.loader.load('ext.gadget.Wikiplus');
 	});
-	mw.loader.using('ext.gadget.Wikiplus');
 }
