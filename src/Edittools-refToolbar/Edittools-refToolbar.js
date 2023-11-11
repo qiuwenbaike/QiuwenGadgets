de1ca1f005749609a5abe0cdcc5d77a6965ab59a
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-11-11 09:31:30 +0800

    refactor(Edittools-refToolbar): re-construct the directory; replace with addEventListener (#265)
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/Edittools-refToolbar/Edittools-refToolbar.js
+++ src/Edittools-refToolbar/Edittools-refToolbar.js
@@ -1,4 +1,4 @@
-import {initializeRefTools} from './RefToolbar';
+import {initializeRefTools} from './modules/RefToolbar';
 
 if (['edit', 'submit'].includes(mw.config.get('wgAction'))) {
 	// Double check if user.options is loaded, to prevent errors when copy pasted accross installations
