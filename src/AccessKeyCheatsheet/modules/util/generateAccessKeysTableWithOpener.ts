91f6cc73d6c8472b6f52f46a2ad9c4d3fa2aa8da
安忆 <i@anyi.in>
2023-11-12 20:26:38 +0800

    refactor(AccessKeyCheatsheet): separate into modules

---
+++ src/AccessKeyCheatsheet/modules/util/generateAccessKeysTableWithOpener.ts
@@ -0,0 +1,43 @@
+import {getMessage} from '../i18n';
+
+const generateAccessKeysTableWithOpener = (
+	$body: JQuery
+): {
+	$accessKeysTable: JQuery;
+	$accessKeyCheatsheetOpener: JQuery;
+} => {
+	const $accessKeysTableItems: JQuery[] = $body
+		.find('[accesskey]')
+		.map((_index: number, element: HTMLElement): JQuery => {
+			return $('<tr>').append(
+				$('<th>').text(element.accessKey.toUpperCase()),
+				$('<td>').text(
+					(
+						element.getAttribute('aria-label') ||
+						element.title ||
+						element.textContent ||
+						(element as HTMLInputElement).value ||
+						$(`label[for="${element.id}"]`).text()
+					).replace(/\s?\[.+?\]/, '')
+				)
+			);
+		})
+		.get();
+	const $accessKeysTable: JQuery = $('<table>')
+		.addClass('wikitable')
+		.css('width', '100%')
+		.append($accessKeysTableItems);
+
+	const $accessKeyCheatsheetOpener: JQuery = $('<a>')
+		.attr('accesskey', '?')
+		.text(getMessage('Text'))
+		.hide()
+		.appendTo($body);
+
+	return {
+		$accessKeysTable,
+		$accessKeyCheatsheetOpener,
+	};
+};
+
+export {generateAccessKeysTableWithOpener};
