19c1988fe2c48b4b08698b34f5145e4bbf42c5d2
WaitSpring <me@waitspring.com>
2023-11-12 21:31:50 +0800

    fix: typo
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/Edittools-refToolbar/modules/util.js
+++ src/Edittools-refToolbar/modules/util.js
@@ -1,4 +1,4 @@
-export const getMessages = (key, ...args) => {
+export const getMessage = (key, ...args) => {
 	// eslint-disable-next-line mediawiki/msg-doc
 	return args.length ? mw.message.apply(mw.message, [key, ...args]).parse() : mw.message(key).plain();
 };
