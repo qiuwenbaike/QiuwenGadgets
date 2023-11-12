e19c8698c1455aec1d582394f6c123b00347c12c
WaitSpring <me@waitspring.com>
2023-11-12 11:16:17 +0800

    refactor(Report*, ScrollUpButton*): split LESS
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/ScrollUpButton/modules/element.ts
+++ src/ScrollUpButton/modules/element.ts
@@ -1,17 +1,23 @@
-/* eslint-disable mediawiki/class-doc */
 import {CLASS_BUTTON, CLASS_BUTTON_DOWN, CLASS_BUTTON_UP} from './constant';
-import {generateSvgDataUrl, scrollTop} from '../../util';
 import {getMessage} from './i18n';
-import scrollButton from '../images/scroll-button.svg';
+import {scrollTop} from '../../util';
 
-const $scrollButton: JQuery = $('<img>')
-	.addClass(`${CLASS_BUTTON} noprint`)
-	.attr({
-		src: generateSvgDataUrl(scrollButton),
-		draggable: false,
-	});
+const scrollButton = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1' height='1'%3E%3C/svg%3E";
+
+// The following classes are used here:
+// * see ./constant.ts
+// * for more information
+const $scrollButton: JQuery = $('<img>').addClass(`${CLASS_BUTTON} noprint`).attr({
+	src: scrollButton,
+	draggable: false,
+	width: '32',
+	height: '32',
+});
 
 const toBottom: string = getMessage('ToBottom');
+// The following classes are used here:
+// * see ./constant.ts
+// * for more information
 const $scrollDownButton: JQuery = $scrollButton
 	.clone()
 	.addClass(CLASS_BUTTON_DOWN)
@@ -24,6 +30,9 @@ const $scrollDownButton: JQuery = $scrollButton
 	});
 
 const toTop: string = getMessage('ToTop');
+// The following classes are used here:
+// * see ./constant.ts
+// * for more information
 const $scrollUpButton: JQuery = $scrollButton
 	.clone()
 	.addClass(CLASS_BUTTON_UP)
