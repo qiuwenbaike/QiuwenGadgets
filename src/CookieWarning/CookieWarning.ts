1627e9fbf85c523966cd47ba5f1d81b8e3cc41bc
WaitSpring <me@waitspring.com>
2023-11-13 14:03:12 +0800

    refactor(CookieWarning): disable CookieWarning for logged-in users
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/CookieWarning/CookieWarning.ts
+++ src/CookieWarning/CookieWarning.ts
@@ -1,8 +1,18 @@
 import {LAST_STORAGE_VALUE, WG_USER_NAME} from './modules/constant';
+import {api} from './modules/api';
 import {insertElement} from './modules/insertElement';
 
 $(function cookieWarning(): void {
-	if (WG_USER_NAME || LAST_STORAGE_VALUE === '1' || location.search.includes('consentRead=')) {
+	if (WG_USER_NAME) {
+		api.postWithEditToken({
+			action: 'options',
+			format: 'json',
+			change: 'gadget-LoginToEdit=0',
+		});
+		return;
+	}
+
+	if (LAST_STORAGE_VALUE === '1' || location.search.includes('consentRead=')) {
 		return;
 	}
 
