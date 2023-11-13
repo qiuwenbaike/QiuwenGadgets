20695bbeb2bb7d995afd375c48df0830312bf780
安忆 <i@anyi.in>
2023-11-13 14:03:41 +0800

    feat(SiteCommon_JS): add tippy for citizen skin header

--- src/SiteCommon_JS/modules/tippy.ts
+++ src/SiteCommon_JS/modules/tippy.ts
@@ -1,15 +1,8 @@
+import {WG_SKIN} from './constant';
 import {getMessage} from './i18n';
 
-export const loadTippy = (): void => {
-	mw.loader.using(['ext.DarkMode']).done(() => {
-		tippy(document.getElementById('darkmode-button') as HTMLElement, {
-			arrow: true,
-			content: getMessage('EnableDisableDarkMode'),
-			placement: 'left',
-		});
-	});
-
-	mw.loader.using(['ext.CollapsibleSidebar.js']).done(() => {
+const loadTippy = (): void => {
+	mw.loader.using('ext.CollapsibleSidebar.js').then((): void => {
 		tippy(document.getElementById('sidebarButton') as HTMLElement, {
 			arrow: true,
 			content: getMessage('CollapseExpandSidebar'),
@@ -17,8 +10,8 @@ export const loadTippy = (): void => {
 		});
 	});
 
-	if (mw.config.get('skin') === 'vector') {
-		mw.loader.using(['ext.CollapsibleSidebar.vector']).done(() => {
+	if (WG_SKIN) {
+		mw.loader.using('ext.CollapsibleSidebar.vector').then(() => {
 			tippy(document.getElementById('sidebarCollapse') as HTMLElement, {
 				arrow: true,
 				content: getMessage('CollapseExpandSidebar'),
@@ -26,4 +19,38 @@ export const loadTippy = (): void => {
 			});
 		});
 	}
+
+	mw.loader.using('ext.DarkMode').then((): void => {
+		tippy(document.getElementById('darkmode-button') as HTMLElement, {
+			arrow: true,
+			content: getMessage('EnableDisableDarkMode'),
+			placement: 'left',
+		});
+	});
+
+	$(function tippyForCitizenHeader(): void {
+		if (WG_SKIN !== 'citizen') {
+			return;
+		}
+		for (const element of $(
+			'.citizen-header label[title],.citizen-header .mw-echo-notifications-badge,citizen-header__logo a'
+		)) {
+			const $element = $(element);
+			const title: string | undefined = $element.attr('title');
+			if (!title) {
+				continue;
+			}
+			$element.attr({
+				'aria-label': title,
+				title: '',
+			});
+			tippy($element.get(0) as HTMLElement, {
+				arrow: true,
+				content: title,
+				placement: 'bottom',
+			});
+		}
+	});
 };
+
+export {loadTippy};
