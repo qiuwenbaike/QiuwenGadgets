c621787b6cc2f94e93476e561e8294d8e1a9d369
安忆 <i@anyi.in>
2023-11-13 14:56:21 +0800

    refactor: use `mw.util.getParamValue` instead of `location.href/search.includes` (#318)

--- src/CookieWarning/CookieWarning.ts
+++ src/CookieWarning/CookieWarning.ts
@@ -1,8 +1,8 @@
-import {LAST_STORAGE_VALUE, WG_USER_NAME} from './modules/constant';
+import {LAST_STORAGE_VALUE, URL_CONSENT_READ, WG_USER_NAME} from './modules/constant';
 import {insertElement} from './modules/insertElement';
 
 $(function cookieWarning(): void {
-	if (WG_USER_NAME || LAST_STORAGE_VALUE === '1' || location.search.includes('consentRead=')) {
+	if (WG_USER_NAME || LAST_STORAGE_VALUE === '1' || URL_CONSENT_READ) {
 		return;
 	}
 
--- src/EditForm_JS/modules/cancelAutoSummary.ts
+++ src/EditForm_JS/modules/cancelAutoSummary.ts
@@ -1,7 +1,7 @@
 export const cancelAutoSummary = (): void => {
 	// 取消修订编辑摘要修正
 	const autoSummary: HTMLInputElement | null = document.querySelector('[name=wpAutoSummary]');
-	if (location.search.includes('undo=') && autoSummary) {
+	if (mw.util.getParamValue('undo') && autoSummary) {
 		autoSummary.value = '';
 	}
 };
