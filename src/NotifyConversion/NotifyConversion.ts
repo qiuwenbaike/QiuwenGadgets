5436f2bb2a61e81c9f1e10688136598494505064
WaitSpring <me@waitspring.com>
2023-11-14 17:14:58 +0800

    refactor(NotifyConversion): merge PreserveVariant with NotifyConversion
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/NotifyConversion/NotifyConversion.ts
+++ src/NotifyConversion/NotifyConversion.ts
@@ -1,14 +1,16 @@
 import {GADGET_NAME, GOOD_VARIANTS, wgUserName} from './modules/constant';
 import {notifyConversion} from './modules/core';
+import {preserveVariant} from './modules/preserveVariant';
 
-const preferredVariant = (mw.storage.get(GADGET_NAME) || '').toString();
-
-if (mw.config.get('wgAction') === 'view') {
-	// 只在浏览页面时显示
+(() => {
+	if (mw.config.get('wgAction') !== 'view') {
+		return; // 只在浏览页面时显示
+	}
+	const preferredVariant = (mw.storage.get(GADGET_NAME) || '').toString();
 	if (!['hide', ...GOOD_VARIANTS].includes(preferredVariant)) {
 		// 是否点过“不再提示”或完成过设置
 		$(notifyConversion);
-	} else if (['hide', ...GOOD_VARIANTS].includes(preferredVariant) && !wgUserName) {
-		mw.loader.load(['ext.gadget.PreserveVariant']);
+	} else if (!wgUserName) {
+		$(preserveVariant);
 	}
-}
+})();
