037d451590b194679053e6f266bcff1a2e05977f
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-11-14 21:04:52 +0800

    fix(ConfirmLogout): rename variable
    Signed-off-by: WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>

--- src/ConfirmLogout/ConfirmLogout.ts
+++ src/ConfirmLogout/ConfirmLogout.ts
@@ -1,8 +1,8 @@
-import {LOGIN_ELEMENT_SELECTOR, WG_USER_NAME} from './modules/constant';
+import {LOGOUT_ELEMENT_SELECTOR, WG_USER_NAME} from './modules/constant';
 import {addListener} from './modules/addListener';
 
 $(function confirmLogout(): void {
-	const $element: JQuery<HTMLAnchorElement> = $<HTMLAnchorElement>(LOGIN_ELEMENT_SELECTOR);
+	const $element: JQuery<HTMLAnchorElement> = $<HTMLAnchorElement>(LOGOUT_ELEMENT_SELECTOR);
 	if (!$element.length || !WG_USER_NAME) {
 		return;
 	}
