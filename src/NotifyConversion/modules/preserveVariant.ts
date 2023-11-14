02d6995176b747a51b7525daed6c9ec0a8dc94fe
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-11-14 17:24:36 +0800

    refactor(NotifyConversion): merge PreserveVariant with NotifyConversion (#339)
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/NotifyConversion/modules/preserveVariant.ts
+++ src/NotifyConversion/modules/preserveVariant.ts
@@ -1,6 +1,13 @@
-import {userVariant} from './constant';
+import {wgUserVariant} from './constant';
 
 export const preserveVariant = () => {
+	if (
+		!wgUserVariant ||
+		!wgUserVariant.includes('zh-') ||
+		!(window.location.pathname.includes(`/${wgUserVariant}/`) && mw.util.getParamValue('variant') !== wgUserVariant)
+	) {
+		return;
+	}
 	$(document).on('keydown mousedown touchstart', 'a', function (): void {
 		const originalHref = $(this).attr('href');
 		let uri;
@@ -14,9 +21,9 @@ export const preserveVariant = () => {
 		}
 		if (!('variant' in uri.query)) {
 			if (originalHref.includes('/wiki/')) {
-				this.href = `/${userVariant}${originalHref.slice(5)}`;
+				this.href = `/${wgUserVariant}${originalHref.slice(5)}`;
 			} else if (originalHref.includes('/index.php?')) {
-				this.href = uri.extend({variant: userVariant}).getRelativePath();
+				this.href = uri.extend({variant: wgUserVariant}).getRelativePath();
 			}
 		}
 	});
