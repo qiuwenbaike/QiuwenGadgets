858a51f3b23e626b872902b12fc83553fd01db27
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-11-14 17:24:44 +0800

    fix(LoginToEdit): fix LOGIN_ELEMENT_SELECTOR (#340)
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/LoginToEdit/modules/constant.ts
+++ src/LoginToEdit/modules/constant.ts
@@ -1,3 +1,5 @@
+const LOGIN_ELEMENT_SELECTOR = '#pt-login>a,.vector-user-menu-login>a,#user-tools>a[href*="Special"]';
+
 const WG_USER_NAME: string | null = mw.config.get('wgUserName');
 
-export {WG_USER_NAME};
+export {LOGIN_ELEMENT_SELECTOR, WG_USER_NAME};
