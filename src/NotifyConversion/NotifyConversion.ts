86e793d0a80c66b974c2350857aa211bf70fb6bf
WaitSpring <me@waitspring.com>
2023-11-14 16:21:02 +0800

    refactor(NotifyConversion): optimize code logic
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/NotifyConversion/NotifyConversion.ts
+++ src/NotifyConversion/NotifyConversion.ts
@@ -3,11 +3,12 @@ import {notifyConversion} from './modules/core';
 
 const preferredVariant = (mw.storage.get(GADGET_NAME) || '').toString();
 
-if (
-	mw.config.get('wgAction') === 'view' && // 只在浏览页面时显示
-	!['hide', ...GOOD_VARIANTS].includes(preferredVariant) // 是否点过“不再提示”或完成过设置
-) {
-	$(notifyConversion);
-} else if (GOOD_VARIANTS.includes(preferredVariant) && !wgUserName) {
-	mw.loader.using(['ext.gadget.PreserveVariant']);
+if (mw.config.get('wgAction') === 'view') {
+	// 只在浏览页面时显示
+	if (!['hide', ...GOOD_VARIANTS].includes(preferredVariant)) {
+		// 是否点过“不再提示”或完成过设置
+		$(notifyConversion);
+	} else if (['hide', ...GOOD_VARIANTS].includes(preferredVariant) && !wgUserName) {
+		mw.loader.load(['ext.gadget.PreserveVariant']);
+	}
 }
