8647cfb4159c2e0df08030e35583b3b9d225e681
WaitSpring <me@waitspring.com>
2023-11-14 22:07:52 +0800

    fix(Report): revision 0 problem
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/Report/modules/constant.ts
+++ src/Report/modules/constant.ts
@@ -3,7 +3,7 @@ const CLASS_REPORT_BUTTON = 'gadget-report__button';
 
 const URL: string = mw.util.getUrl('Special:Contact/Report', {
 	report_title: mw.config.get('wgPageName'),
-	report_revision: mw.config.get('wgRevisionId').toString(),
+	report_revision: (mw.config.get('wgRevisionId') || mw.config.get('wgCurRevisionId')).toString(),
 });
 
 export {CLASS_REPORT_BUTTON, URL};
