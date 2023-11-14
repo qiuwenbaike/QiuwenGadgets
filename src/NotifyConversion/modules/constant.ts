63632debf5883db1c137eee65270a486f6140885
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-11-14 13:27:56 +0800

    refactor(NotifyConversion)!: refactor with OOUI (#333)
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/NotifyConversion/modules/constant.ts
+++ src/NotifyConversion/modules/constant.ts
@@ -1,1 +1,19 @@
 export const GADGET_NAME = 'ext.gadget.NotifyConversion';
+
+export const SYSTEM_SCRIPT_LIST = [
+	'滥用过滤器',
+	'求闻编者',
+	'求闻百科社区',
+	'Example',
+	'External contributors',
+	'Maintenance script',
+	'MediaWiki default',
+	'MediaWiki message delivery',
+	'New user page',
+];
+
+export const WEBMASTER_LIST = ['QiuWen', 'QiuWen for Accounts', 'QiuwenSocialMedia'];
+
+export const wgUserName: string = mw.config.get('wgUserName') || '';
+export const wgUserGroups: string[] = mw.config.get('wgUserGroups') || [];
+export const wgUserVariant: string | null = mw.config.get('wgUserVariant');
---
+++ src/NotifyConversion/modules/removeWindowResizeHandler.ts
@@ -0,0 +1,8 @@
+const removeWindowResizeHandler = (windowManager: OO.ui.WindowManager): void => {
+	$(window).off({
+		'orientationchange resize': (windowManager as unknown as {onWindowResizeHandler: () => void})
+			.onWindowResizeHandler,
+	});
+};
+
+export {removeWindowResizeHandler};
