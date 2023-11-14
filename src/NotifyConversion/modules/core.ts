c32027991f88db7ae09e953399b2cf0e86c1bea4
WaitSpring <me@waitspring.com>
2023-11-14 13:21:34 +0800

    refactor(NotifyConversion): refactor with OOUI
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/NotifyConversion/modules/core.ts
+++ src/NotifyConversion/modules/core.ts
@@ -1,72 +1,142 @@
-import {GADGET_NAME} from './constant';
-import {ding} from '../../util';
+import {GADGET_NAME, SYSTEM_SCRIPT_LIST, WEBMASTER_LIST, wgUserGroups, wgUserName, wgUserVariant} from './constant';
+import {getMessage} from './i18n';
+import {removeWindowResizeHandler} from './removeWindowResizeHandler';
+
+const locationHref: string = location.href;
+
+const isExperiencedUser = (): boolean => {
+	if (!wgUserName || !wgUserGroups) {
+		return false;
+	}
+	return (
+		['sysop', 'senioreditor', 'steward', 'qiuwen'].some((element) => {
+			return wgUserGroups.includes(element);
+		}) ||
+		SYSTEM_SCRIPT_LIST.includes(wgUserName) ||
+		WEBMASTER_LIST.includes(wgUserName)
+	);
+};
+
+const isLanguageSet = (): boolean => {
+	if (mw.util.getParamValue('variant') || mw.util.getParamValue('uselang')) {
+		return true;
+	}
+	return false;
+};
+
+const isWrongURL = (): boolean => {
+	if (locationHref.includes('/zh/') || locationHref.includes('/zh-hans/') || locationHref.includes('/zh-hant/')) {
+		return true;
+	}
+	return false;
+};
+
+const showDialog = (): void => {
+	const dialogTitleIcon = new OO.ui.IconWidget({
+		icon: 'language',
+		title: getMessage('dialogTitle'),
+	});
+	const dialogTitle = new OO.ui.LabelWidget({
+		label: getMessage('dialogTitle'),
+	});
+
+	const $dialogMessage = $('<div>')
+		.addClass('gadget-notify_conversion__message')
+		.append(
+			$('<p>').text(getMessage('dialogDesc')),
+			$('<p>').addClass('gadget-notify_conversion__message_extend').text(getMessage('dialogDescExtend1')),
+			$('<p>').addClass('gadget-notify_conversion__message_extend').text(getMessage('dialogDescExtend2'))
+		);
+
+	const generateButton = (variant: string): OO.ui.ButtonOptionWidget => {
+		return new OO.ui.ButtonOptionWidget({
+			data: variant,
+			label: getMessage(`variant-${variant}`),
+			title: getMessage(`variant-${variant}`),
+		});
+	};
+	// Create a new ButtonSelectWidget and add the button options to it
+	// via the ButtonSelectWidget's 'items' config option.
+	const buttonSelect = new OO.ui.ButtonSelectWidget({
+		items: [
+			generateButton('zh-cn'),
+			generateButton('zh-hk'),
+			generateButton('zh-tw'),
+			generateButton('zh-sg'),
+			generateButton('zh-my'),
+			generateButton('zh-mo'),
+		],
+	});
+
+	if (preferredVariant === null) {
+		buttonSelect.selectItemByData('zh-cn');
+	} else {
+		buttonSelect.selectItemByData(preferredVariant);
+	}
+
+	const windowManager = new OO.ui.WindowManager();
+	windowManager.$element.appendTo($('body'));
+
+	const messageDialog = new OO.ui.MessageDialog();
+	const messageDialogProperty = {
+		title: $('<span>')
+			.addClass('gadget-notify_conversion__title')
+			.append(dialogTitleIcon.$element, dialogTitle.$element),
+		message: $dialogMessage.append(buttonSelect.$element),
+		actions: [
+			{
+				action: 'confirm',
+				flags: ['primary', 'progressive'],
+				label: $('<b>').text(getMessage('confirm')),
+			},
+			{
+				action: 'cancel',
+				flags: ['safe', 'close'],
+				label: $('<b>').text(getMessage('cancel')),
+			},
+		],
+	};
+
+	windowManager.addWindows([messageDialog]);
+	windowManager.openWindow(messageDialog, messageDialogProperty);
+
+	messageDialog.getActionProcess = (action): OO.ui.Process => {
+		const clearWindows = () => {
+			mw.storage.set(GADGET_NAME, 'hide');
+			windowManager.clearWindows();
+		};
+
+		return new OO.ui.Process((): void => {
+			if (action === 'confirm') {
+				const URL_REGEX = /(\/\/[^/]+\/)([^/]+)(\/)/;
+				const selectedItem = buttonSelect.findSelectedItem() as OO.ui.OptionWidget;
+				clearWindows();
+				const variant = selectedItem.getData();
+				location.href = locationHref.replace(URL_REGEX, `$1${variant}$3`);
+			} else {
+				OO.ui.confirm(getMessage('Are you sure?')).done((confirmed) => {
+					if (confirmed) {
+						clearWindows();
+					}
+				});
+			}
+		});
+	};
+
+	removeWindowResizeHandler(windowManager);
+};
+
+const preferredVariant: string | null =
+	(mw.config.get('wgUserName') ? mw.user.options.get('variant') : mw.config.get('wgUserVariant')) ?? null;
 
 export const notifyConversion = (): void => {
-	const messageHansBefore =
-		'您现在使用的中文变体可能会影响一些词语繁简转换的效果。建议您根据您的偏好切换到下列变体之一：<ul class="hlist hlist-comma">';
-	const messageHansAfter =
-		'</ul><span id="gadget-notify_conversion__info">（<a id="gadget-notify_conversion__donotshow" class="gadget-notify_conversion__link" href="#">下次不再提示</a> | <a id="gadget-notify_conversion__showmore" class="gadget-notify_conversion__link" rel="noopener" target="_blank" href="/wiki/Help:字词转换的模式选择说明">了解更多</a>）</span>';
-	const messageHantBefore =
-		'您現在使用的中文變體可能會影響一些詞語繁簡轉換的效果。建議您根據您的偏好切換到下列變體之一：<ul class="hlist hlist-comma">';
-	const messageHantAfter =
-		'</ul><span id="gadget-notify_conversion__info">（<a id="gadget-notify_conversion__donotshow" class="gadget-notify_conversion__link" href="#">下次不再提示</a> | <a id="gadget-notify_conversion__showmore" class="gadget-notify_conversion__link" rel="noopener" target="_blank" href="/wiki/Help:字词转换的模式选择说明">了解更多</a>）</span>';
-	const variants = {
-		'zh-cn': '中国大陆简体',
-		'zh-hk': '中國香港繁體',
-		'zh-mo': '中國澳門繁體',
-		'zh-my': '马来西亚简体',
-		'zh-sg': '新加坡简体',
-		'zh-tw': '中國臺灣繁體',
-	};
-	// 注意：这里若 wgUserVariant 是 zh-hans（zh/zh-hant 同理），有多种可能：
-	// (1) 登录用户在参数设定里把内容语言变种设成了 zh-hans；
-	// (2) 用户自己在 URL 后面添加了 ?variant=zh-hans 或者 ?uselang=zh-hans 的参数；
-	// (3) URL 是 /zh-hans/example 而不是 /wiki/example。
-	// 我们这里只针对 (1) 和 (3) 的情况通知用户。
-	// 对于 (2)，我们认为这样做的用户不是新手（例如在技术测试），不进行提示。
-	// 所以，我们使用正则截取 URL //www.qiuwenbaike.cn/zh-hans/ 中的 zh-hans 部分，
-	// 这样做是为了最大程度地保留 URL 中原有的参数（例如查看日志时或其他小工具），只替换变体部分。
-	// 若这个部分是 / 的话，说明目前 URL 是 /index.php?title=x 的形式，也不进行提示。
-	const url: string = location.href;
-	const urlRegex = /(\/\/[^/]+\/)([^/]+)(\/)/;
-	if (mw.util.getParamValue('variant') || mw.util.getParamValue('uselang')) {
+	if (!wgUserVariant) {
+		return; // Special pages
+	}
+	if (isExperiencedUser() || isLanguageSet()) {
 		return;
 	}
-	if (!url.includes('/zh/') && !url.includes('/zh-hans/') && !url.includes('/zh-hant/')) {
-		return;
+	if (preferredVariant === null || isWrongURL()) {
+		showDialog();
 	}
-	const makeLinks = (): string => {
-		let links = '';
-		for (const [variant, name] of Object.entries(variants)) {
-			links += `<li><a class="gadget-notify_conversion__link" href="${url.replace(
-				urlRegex,
-				`$1${variant}$3`
-			)}">${name}</a></li>`;
-		}
-		return links;
-	};
-	// 根据简繁体显示不同提示文字
-	const wgUserVariant: string | null = mw.config.get('wgUserVariant');
-	if (url.includes('/zh/') || url.includes('/zh-hans/') || wgUserVariant === 'zh' || wgUserVariant === 'zh-hans') {
-		ding(
-			`<span id="gadget-notify_conversion__area" class="gadget-notify_conversion">${messageHansBefore}${makeLinks()}${messageHansAfter}</span>`,
-			false
-		);
-	} else if (url.includes('/zh-hant/') || wgUserVariant === 'zh-hant') {
-		ding(
-			`<span id="gadget-notify_conversion__area" class="gadget-notify_conversion">${messageHantBefore}${makeLinks()}${messageHantAfter}</span>`,
-			false
-		);
-	}
-	// 点击“不再提示”，在LocalStorage添加条目
-	$('#gadget-notify_conversion__donotshow').on('click', (event: JQuery.ClickEvent): void => {
-		event.preventDefault();
-		mw.storage.set(GADGET_NAME, 'hide');
-		$('.gadget-notify_conversion').parent().hide();
-	});
-	$('#gadget-notify_conversion__showmore').on('click', (event: JQuery.ClickEvent): void => {
-		// 不触发上层 onclick 事件（即点击该链接不会关闭横幅）
-		// 预期用户点击“了解更多”后会返回来继续切换变体
-		event.stopPropagation();
-	});
 };
