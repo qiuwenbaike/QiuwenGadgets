529be297a281374935afc9c536f5b75b95c91d86
安忆 <i@anyi.in>
2023-11-17 00:17:27 +0800

    chore(AccessKeyCheatsheet): improve translation

--- src/AccessKeyCheatsheet/modules/i18n.ts
+++ src/AccessKeyCheatsheet/modules/i18n.ts
@@ -4,18 +4,21 @@ const getI18nMessages: GetI18nMessages = () => {
 		Close: localize({
 			en: 'Close',
 			ja: '閉じる',
+			ru: 'Закрыть',
 			'zh-hans': '关闭',
 			'zh-hant': '關閉',
 		}),
 		Text: localize({
 			en: 'Open access keys cheatsheet',
 			ja: 'アクセスキーのチートシート',
+			ru: 'Открыть подсказку с клавиатурными ярлыками',
 			'zh-hans': '显示键盘快捷键提示',
 			'zh-hant': '顯示鍵盤快速鍵提示',
 		}),
 		Title: localize({
 			en: 'Access keys cheatsheet',
 			ja: 'アクセスキーのチートシート',
+			ru: 'Подсказки клавиатурных ярлыков',
 			'zh-hans': '键盘快捷键提示',
 			'zh-hant': '鍵盤快速鍵提示',
 		}),
