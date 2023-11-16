32eab927f48e41de39b9020804eb249e90367277
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-11-16 16:47:48 +0800

    fix(historyDisclaimer): judge if `#historyDisclaimer` exists (#351)
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/HistoryDisclaimer/HistoryDisclaimer.ts
+++ src/HistoryDisclaimer/HistoryDisclaimer.ts
@@ -2,7 +2,8 @@ const historyDisclaimer = (): void => {
 	if (
 		(mw.config.get('wgCurRevisionId') || -1) > 0 &&
 		(mw.config.get('wgRevisionId') || -1) > 0 &&
-		(mw.config.get('wgCurRevisionId') || -1) > (mw.config.get('wgRevisionId') || -1)
+		(mw.config.get('wgCurRevisionId') || -1) > (mw.config.get('wgRevisionId') || -1) &&
+		!$('#historyDisclaimer').length
 	) {
 		$('<div>').attr('id', 'historyDisclaimer').appendTo($('body'));
 	}
