e7c1c71053e648dc788f076bf6978806034f4eb6
安忆 <i@anyi.in>
2023-11-17 01:06:27 +0800

    refactor(AddSectionPlus): add AddSectionPlus-pagestyles

--- src/AddSectionPlus/AddSectionPlus.ts
+++ src/AddSectionPlus/AddSectionPlus.ts
@@ -2,12 +2,16 @@ import {WG_SKIN} from './modules/constant';
 import {processElement} from './modules/processElement';
 
 $(function addSectionPlus(): void {
+	if (WG_SKIN === 'citizen') {
+		return; // style is already added by [[MediaWiki:Gadget-AddSectionPlus-pagestyles.css]]
+	}
+
 	const $wrapper: JQuery = $('#ca-addsection');
 	if (!$wrapper.length) {
 		return;
 	}
 
-	const $target: JQuery = ['citizen', 'write'].includes(WG_SKIN) ? $wrapper.find('a') : $wrapper.find('span');
+	const $target: JQuery = WG_SKIN === 'write' ? $wrapper.find('a') : $wrapper.find('span');
 	if (!$target.length) {
 		return;
 	}
--- src/AddSectionPlus/modules/processElement.ts
+++ src/AddSectionPlus/modules/processElement.ts
@@ -3,12 +3,6 @@ import {WG_SKIN} from './constant';
 const processElement = ($element: JQuery): void => {
 	const title: string = $element.attr('title') ?? $element.attr('aria-label') ?? $element.text().trim();
 	switch (WG_SKIN) {
-		case 'citizen':
-			$element.attr('style', '').css({
-				'font-size': '0',
-				gap: '0',
-			});
-			break;
 		case 'gongbi':
 			$element.attr('title', title).html('&#8203');
 			break;
