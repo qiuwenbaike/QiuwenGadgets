78d8ba95d3a5de3db15bf1525a2ffce96c493f89
WaitSpring <me@waitspring.com>
2023-11-16 23:06:00 +0800

    fix(MarkRights): add `$(...).parent().parent().find()` to avoid duplicate insertion
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/MarkRights/modules/core.ts
+++ src/MarkRights/modules/core.ts
@@ -79,6 +79,9 @@ export const markUserRights = ($content: JQuery): void => {
 	const done = (): void => {
 		$userLinks.each((_index: number, element: HTMLElement): void => {
 			const $element: JQuery = $(element);
+			if ($($element).parent().parent().find('.gadgets-markrights').length > 0) {
+				return;
+			}
 			const username: string = getUsername($element.attr('href') ?? '');
 			if (!username) {
 				return;
