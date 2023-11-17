81dba3ae35a06966aedebb0f807b2c9c90c19c87
安忆 <i@anyi.in>
2023-11-17 17:52:39 +0800

    refactor(AjaxLogin): reuse jQuery query

--- src/AjaxLogin/AjaxLogin.ts
+++ src/AjaxLogin/AjaxLogin.ts
@@ -2,14 +2,16 @@ import {LOGIN_ELEMENT_SELECTOR, WG_USER_NAME} from './modules/constant';
 import {ajaxLogin} from './modules/core';
 import {checkA11yConfirmKey} from '../util';
 
-$(function initAutoLogin() {
-	const $element: JQuery<HTMLAnchorElement> = $(LOGIN_ELEMENT_SELECTOR);
+$(function initAutoLogin(): void {
+	const $body: JQuery<HTMLBodyElement> = $('body');
+
+	const $element: JQuery<HTMLAnchorElement> = $body.find<HTMLAnchorElement>(LOGIN_ELEMENT_SELECTOR);
 	if (WG_USER_NAME || !$element.length) {
 		return;
 	}
 
 	const windowManager = new OO.ui.WindowManager();
-	windowManager.$element.appendTo($('body'));
+	windowManager.$element.appendTo($body);
 
 	const fakeToastifyInstance: ToastifyInstance = {
 		hideToast: () => {},
@@ -19,6 +21,7 @@ $(function initAutoLogin() {
 		if (!checkA11yConfirmKey(event)) {
 			return;
 		}
+
 		event.preventDefault();
 		ajaxLogin(windowManager, fakeToastifyInstance);
 	};
