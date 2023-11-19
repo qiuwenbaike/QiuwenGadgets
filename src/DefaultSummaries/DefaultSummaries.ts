93f8e1435015f4981307a190b42aa75d16ac4f4c
WaitSpring <me@waitspring.com>
2023-11-19 10:19:18 +0800

    chore: update format
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/DefaultSummaries/DefaultSummaries.ts
+++ src/DefaultSummaries/DefaultSummaries.ts
@@ -3,7 +3,7 @@ import {defaultSummariesLoad} from './modules/core';
 if (['edit', 'submit'].includes(mw.config.get('wgAction'))) {
 	$(defaultSummariesLoad);
 } else {
-	mw.loader.using(['ext.visualEditor.desktopArticleTarget.init'], (): void => {
+	mw.loader.using('ext.visualEditor.desktopArticleTarget.init').done((): void => {
 		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		/* @ts-ignore */
 		mw.libs.ve.addPlugin((): void => {
