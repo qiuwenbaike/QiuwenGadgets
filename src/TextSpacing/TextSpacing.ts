eeae37d9ec4d17c8baad726f13d30fe6ce1adca1
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-11-21 10:04:24 +0800

    build: disable compat/compat inline to ignore error (follow #436) (#437)

--- src/TextSpacing/TextSpacing.ts
+++ src/TextSpacing/TextSpacing.ts
@@ -26,6 +26,7 @@ const mutationObserver = new MutationObserver((records: MutationRecord[]): void 
 		}
 
 		// Optimization: prevent forced reflows
+		// eslint-disable-next-line compat/compat
 		requestAnimationFrame((): void => {
 			for (const node of addedNodes) {
 				if (node instanceof HTMLElement) {
