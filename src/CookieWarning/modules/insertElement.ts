3a39fbcf1055faab8e69d49b3057c6117aef4fee
安忆 <i@anyi.in>
2023-11-23 13:12:10 +0800

    refactor(CookieWarning): use existing variables (#472)

--- src/CookieWarning/modules/insertElement.ts
+++ src/CookieWarning/modules/insertElement.ts
@@ -1,18 +1,18 @@
-import {CLASS_CONSENT_NOTICE, STORAGE_KEY} from './constant';
+import {CLASS_NAME_CONSENT_NOTICE, STORAGE_KEY} from './constant';
 import {getMessage} from './i18n';
 
 const insertElement = (): void => {
+	const $agreeButton: JQuery = $('<span>')
+		.addClass('mw-ui-button mw-ui-progressive')
+		.attr('role', 'button')
+		.html(getMessage('Agree'));
 	const $consentNotice: JQuery = $('<div>')
 		// The following classes are used here:
 		// * see ./constant.ts
 		// * for more information
-		.addClass(`${CLASS_CONSENT_NOTICE} noprint`)
-		.append(
-			$('<p>').html(getMessage('TOSNotice')),
-			$('<p>').html(getMessage('CookieNotice')),
-			$('<span>').addClass('mw-ui-button mw-ui-progressive').attr('role', 'button').html(getMessage('Agree'))
-		);
-	$consentNotice.find('span').on('click', (): void => {
+		.addClass(`${CLASS_NAME_CONSENT_NOTICE} noprint`)
+		.append($('<p>').html(getMessage('TOSNotice')), $('<p>').html(getMessage('CookieNotice')), $agreeButton);
+	$agreeButton.on('click', (): void => {
 		mw.storage.set(STORAGE_KEY, '1', 60 * 60 * 1000 * 24 * 30);
 		$consentNotice.fadeOut().remove();
 	});
