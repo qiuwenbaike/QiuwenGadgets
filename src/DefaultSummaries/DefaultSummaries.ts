ea2419dfafcaac3ed62e74a8265d261822e5c555
安忆 <i@anyi.in>
2023-11-25 01:39:59 +0800

    refactor(edit0): use `mediawiki.Uri` instead of `mediawiki.util` (#521)

--- src/DefaultSummaries/DefaultSummaries.ts
+++ src/DefaultSummaries/DefaultSummaries.ts
@@ -2,10 +2,8 @@ import {WG_ACTION} from './modules/constant';
 import {addHook} from './modules/addHook';
 import {addVisualEditorPlugin} from './modules/addVisualEditorPlugin';
 
-(() => {
-	if (['edit', 'submit'].includes(WG_ACTION)) {
-		addHook();
-	} else {
-		addVisualEditorPlugin();
-	}
-})();
+if (['edit', 'submit'].includes(WG_ACTION)) {
+	addHook();
+} else {
+	addVisualEditorPlugin();
+}
--- src/Edit0/modules/constant.ts
+++ src/Edit0/modules/constant.ts
@@ -1,4 +1,3 @@
-const WG_ACTION: MediaWikiConfigMapWgAction = mw.config.get('wgAction');
 const WG_NAMESPACE_NUMBER: number = mw.config.get('wgNamespaceNumber');
 
-export {WG_ACTION, WG_NAMESPACE_NUMBER};
+export {WG_NAMESPACE_NUMBER};
