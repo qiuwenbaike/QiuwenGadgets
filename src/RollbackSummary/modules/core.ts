051e8d8eb79b379bc29c28d3e5d9f5e54c20ca7e
WaitSpring <me@waitspring.com>
2023-11-24 15:21:30 +0800

    refactor(RollbackSummary): split modules; optimize logic
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/RollbackSummary/modules/core.ts
+++ src/RollbackSummary/modules/core.ts
@@ -1,36 +1,34 @@
+import {getMessage} from './i18n';
 import {messages} from './messages';
 
-export const rollbackSummary = (): void => {
+const updateLinks = (): void => {
 	messages();
-	const updateLinks = (): void => {
-		const $body = $('body');
-		$body.find('.mw-rollback-link a').off('click');
-		$body
-			.find('.mw-rollback-link a')
-			.on('click', function (event: JQuery.ClickEvent): void {
-				event.preventDefault();
-				let {href} = this as HTMLAnchorElement;
-				let summary: string | null = prompt(
-					window.wgULS(
-						'请输入自定义回退摘要（留空则使用系统默认摘要）',
-						'請輸入自定義回退摘要（留空則使用系統預設摘要）'
-					)
-				);
-				if (summary === null) {
-					/* empty */
-				} else if (summary === '') {
-					location.assign(href);
-				} else {
-					const username: string | null = mw.util.getParamValue('from', href);
-					username
-						? (summary = mw.message('rollback-summary-custom', username).plain() + summary)
-						: (summary = mw.message('rollback-summary-nouser').plain() + summary);
-					href += `&summary=${encodeURIComponent(summary)}`;
-					location.assign(href);
-				}
-			})
-			.css('color', '#099');
-	};
+	const $body = $('body');
+	const $mwRollbackLinkA = $body.find('.mw-rollback-link a');
+	$mwRollbackLinkA.off('click');
+	$mwRollbackLinkA.on('click', function (event: JQuery.ClickEvent): void {
+		event.preventDefault();
+		let {href} = this as HTMLAnchorElement;
+		let summary: string | null = prompt(getMessage('Prompt'));
+		if (summary === null) {
+			/* empty */
+		} else if (summary === '') {
+			location.assign(href);
+		} else {
+			const username: string | null = mw.util.getParamValue('from', href);
+			if (username) {
+				summary = mw.message('rollback-summary-custom', username).plain() + summary;
+			} else {
+				summary = mw.message('rollback-summary-nouser').plain() + summary;
+			}
+			href += `&summary=${encodeURIComponent(summary)}`;
+			location.assign(href);
+		}
+	});
+	$mwRollbackLinkA.css('color', '#099');
+};
+
+export const rollbackSummary = (): void => {
 	mw.hook('wikipage.content').add((): void => {
 		updateLinks();
 	});
---
+++ src/RollbackSummary/modules/i18n.ts
@@ -0,0 +1,16 @@
+const getI18nMessages: GetI18nMessages = () => {
+	const {localize} = i18n;
+	return {
+		Prompt: localize({
+			en: 'Please enter a custom rollback summary (leave blank to use the system default summary)',
+			'zh-hans': '请输入自定义回退摘要（留空则使用系统默认摘要）',
+			'zh-hant': '請輸入自定義回退摘要（留空則使用系統預設摘要）',
+		}),
+	};
+};
+
+const i18nMessages = getI18nMessages();
+
+export const getMessage = (key: string): string => {
+	return i18nMessages[key] || key;
+};
