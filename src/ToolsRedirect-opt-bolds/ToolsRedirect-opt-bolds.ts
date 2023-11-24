72eb04ad1da5acd9d4600f89e9ed6f356e8797f2
WaitSpring <me@waitspring.com>
2023-11-24 21:27:56 +0800

    refactor(ToolsRedirect): convert to async func
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/ToolsRedirect-opt-bolds/ToolsRedirect-opt-bolds.ts
+++ src/ToolsRedirect-opt-bolds/ToolsRedirect-opt-bolds.ts
@@ -1,13 +1,17 @@
 import {initMwApi} from '../util';
 
-if (!mw.user.options.get('gadget-ToolsRedirect')) {
-	const api = initMwApi(`Qiuwen/1.1 (ToolsRedirect-opt-bolds/2.0; ${mw.config.get('wgWikiID')})`);
+(async () => {
+	if (!mw.user.options.get('gadget-ToolsRedirect')) {
+		return;
+	}
 
-	api.postWithEditToken({
+	const api = initMwApi(`Qiuwen/1.1 (ToolsRedirect-opt-bolds-names/2.0; ${mw.config.get('wgWikiID')})`);
+
+	await api.postWithEditToken({
 		action: 'options',
 		format: 'json',
 		change: 'gadget-ToolsRedirect=1',
-	}).then(() => {
-		mw.loader.using(['ext.gadget.ToolsRedirect', 'ext.gadget.ToolsRedirect-pagestyles']);
 	});
-}
+
+	mw.loader.using(['ext.gadget.ToolsRedirect']);
+})();
