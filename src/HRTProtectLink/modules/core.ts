577deb720a9cfea5d40fe6493ebbd5e80f8ce54d
WaitSpring <me@waitspring.com>
2023-11-24 15:22:53 +0800

    refactor(HRTPL): split modules; optimize logic
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/HRTProtectLink/modules/core.ts
+++ src/HRTProtectLink/modules/core.ts
@@ -1,30 +1,19 @@
 import {getMessage} from './i18n';
 
-const semiProtectURL = mw.util.getUrl(mw.config.get('wgPageName'), {
-	action: 'protect',
-	'mwProtect-level-edit': 'autoconfirmed',
-	'mwProtect-level-move': 'autoconfirmed',
-	'mwProtect-reason': getMessage('HRT'),
-});
-
-const fullProtectURL = mw.util.getUrl(mw.config.get('wgPageName'), {
-	action: 'protect',
-	'mwProtect-level-edit': 'sysop',
-	'mwProtect-level-move': 'sysop',
-	'mwProtect-reason': getMessage('HRT'),
-});
-
 export const HRTProtectLink = (): void => {
-	mw.util.addPortletLink(
-		document.querySelector('#p-cactions') ? 'p-cactions' : 'p-tb',
-		semiProtectURL,
-		getMessage('Semi-protect HRT'),
-		't-hrt-semiprotect'
-	);
-	mw.util.addPortletLink(
-		document.querySelector('#p-cactions') ? 'p-cactions' : 'p-tb',
-		fullProtectURL,
-		getMessage('Full-protect HRT'),
-		't-hrt-fullprotect'
-	);
+	const semiProtectURL = mw.util.getUrl(mw.config.get('wgPageName'), {
+		action: 'protect',
+		'mwProtect-level-edit': 'autoconfirmed',
+		'mwProtect-level-move': 'autoconfirmed',
+		'mwProtect-reason': getMessage('HRT'),
+	});
+	const fullProtectURL = mw.util.getUrl(mw.config.get('wgPageName'), {
+		action: 'protect',
+		'mwProtect-level-edit': 'sysop',
+		'mwProtect-level-move': 'sysop',
+		'mwProtect-reason': getMessage('HRT'),
+	});
+	const portletId = document.querySelector('#p-cactions') ? 'p-cactions' : 'p-tb';
+	mw.util.addPortletLink(portletId, semiProtectURL, getMessage('Semi-protect HRT'), 't-hrt-semiprotect');
+	mw.util.addPortletLink(portletId, fullProtectURL, getMessage('Full-protect HRT'), 't-hrt-fullprotect');
 };
