4b07de8ce7fdd54e497b91b2e2e1f612b4070424
安忆 <i@anyi.in>
2023-11-25 01:39:07 +0800

    refactor(edit0): use `mediawiki.Uri` instead of `mediawiki.util`

--- src/Edit0/Edit0.ts
+++ src/Edit0/Edit0.ts
@@ -1,14 +1,6 @@
-import {WG_ACTION, WG_NAMESPACE_NUMBER} from './modules/constant';
+import {WG_NAMESPACE_NUMBER} from './modules/constant';
 import {edit0} from './modules/core';
 
-(() => {
-	if (WG_ACTION !== 'view') {
-		return;
-	}
-
-	if (WG_NAMESPACE_NUMBER < 0) {
-		return;
-	}
-
+if (WG_NAMESPACE_NUMBER >= 0) {
 	$(edit0);
-})();
+}
--- src/Edit0/modules/core.ts
+++ src/Edit0/modules/core.ts
@@ -1,35 +1,50 @@
 import {getMessage} from './i18n';
 
-export const edit0 = (): void => {
-	const localTitle: string = getMessage('Edit0');
+const edit0 = (): void => {
 	const $body = $('body');
-	const $ourContent: JQuery = $body.find('#content, #mw_content').first();
-	const $span1: JQuery = $ourContent.find('span.mw-editsection:not(.plainlinks)').first();
+
+	const $content: JQuery = $body.find('#content, #mw_content').first();
+	const $span1: JQuery = $content.find('span.mw-editsection:not(.plainlinks)').first();
 	if (!$span1.length) {
 		return;
 	}
+
 	const $span0: JQuery = $span1.clone();
+
 	$body.find('body:not(.skin-citizen) #content h1#firstHeading').append($span0);
 	$body.find('body.skin-citizen .mw-indicators').prepend($span0);
-	$span0.find('a').each((_index: number, element: HTMLAnchorElement): void => {
-		const $a: JQuery<HTMLAnchorElement> = $(element);
-		const href: string = $a.attr('href') ?? '';
-		$a.attr('title', localTitle);
-		if (!/&(ve|)section=T/.test(href)) {
+
+	for (const element of $span0.find('a')) {
+		element.title = getMessage('Edit0');
+
+		const {href} = element;
+		const urlSearch = {
+			summary: '/* top */ ',
+			section: '0',
+		};
+
+		if (!/&(?:ve)?section=T/.test(href)) {
 			// not transcluded
-			$a.attr('href', href.replace(/&(ve|)section=\d+/, '&$1section=0&summary=/*%20top%20*/%20'));
+			element.href = new mw.Uri(href).extend(urlSearch).toString();
 		} else if (/&vesection=/.test(href)) {
 			// transcluded, VE
-			$a.attr(
-				'href',
-				`${mw.util.getUrl(mw.config.get('wgPageName'))}?veaction=edit&vesection=0&summary=/*%20top%20*/%20`
-			);
+			element.href = new mw.Uri(href)
+				.extend({
+					...urlSearch,
+					veaction: 'edit',
+					vesection: '0',
+				})
+				.toString();
 		} else {
 			// transcluded, not VE
-			$a.attr(
-				'href',
-				`${mw.util.getUrl(mw.config.get('wgPageName'))}?action=edit&section=0&summary=/*%20top%20*/%20`
-			);
+			element.href = new mw.Uri(href)
+				.extend({
+					...urlSearch,
+					action: 'edit',
+				})
+				.toString();
 		}
-	});
+	}
 };
+
+export {edit0};
