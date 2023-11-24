b12d24db23a01e1aca5fbdb2e398c2ca74938ccc
WaitSpring <me@waitspring.com>
2023-11-24 21:31:32 +0800

    refactor(DefaultSummaries): convert to async func; add IIFE wrap
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/DefaultSummaries/modules/addVisualEditorPlugin.ts
+++ src/DefaultSummaries/modules/addVisualEditorPlugin.ts
@@ -1,12 +1,12 @@
 import {addHook} from './addHook';
 
-const addVisualEditorPlugin = (): void => {
-	mw.loader.using('ext.visualEditor.desktopArticleTarget.init').then((): void => {
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		/* @ts-ignore */
-		mw.libs.ve.addPlugin((): void => {
-			addHook();
-		});
+const addVisualEditorPlugin = async (): Promise<void> => {
+	await mw.loader.using('ext.visualEditor.desktopArticleTarget.init');
+
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	/* @ts-ignore */
+	mw.libs.ve.addPlugin((): void => {
+		addHook();
 	});
 };
 export {addVisualEditorPlugin};
