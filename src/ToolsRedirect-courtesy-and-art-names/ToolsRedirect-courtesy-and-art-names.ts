c38b8e0518f07cb6e3b61978d16235efbdaf31f3
WaitSpring <me@waitspring.com>
2023-11-25 14:07:11 +0800

    refactor(TR*): remove mw.loader.using()
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/ToolsRedirect-courtesy-and-art-names/ToolsRedirect-courtesy-and-art-names.ts
+++ src/ToolsRedirect-courtesy-and-art-names/ToolsRedirect-courtesy-and-art-names.ts
@@ -1,17 +1,15 @@
 import {initMwApi} from '../util';
 
-(async () => {
+(() => {
 	if (!mw.user.options.get('gadget-ToolsRedirect')) {
 		return;
 	}
 
 	const api = initMwApi(`Qiuwen/1.1 (ToolsRedirect-courtesy-and-art-names/2.0; ${mw.config.get('wgWikiID')})`);
 
-	await api.postWithEditToken({
+	api.postWithEditToken({
 		action: 'options',
 		format: 'json',
 		change: 'gadget-ToolsRedirect=1',
 	});
-
-	mw.loader.using(['ext.gadget.ToolsRedirect']);
 })();
