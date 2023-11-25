c53f6c3d71559179d7ea5f3ce826c1e010402285
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-11-25 14:11:46 +0800

    refactor(TR*; Edittools*): optimize logic (#528)
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/Edittools-customizeToolbar/Edittools-customizeToolbar.js
+++ src/Edittools-customizeToolbar/Edittools-customizeToolbar.js
@@ -1,15 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-const customizeToolbar = (customizer) => {
+const customizeToolbar = async (customizer) => {
 	/* Check if we are in edit mode and the required modules are available and then customize the toolbar */
-	if (['edit', 'submit'].includes(mw.config.get('wgAction')) && mw.user.options.get('usebetatoolbar')) {
-		mw.loader.using('ext.wikiEditor').then(() => {
-			$(function callCustomizer() {
-				const $body = $('body');
-				customizer.call($body.find('#wpTextbox1'));
-			});
-		});
+	if (!['edit', 'submit'].includes(mw.config.get('wgAction'))) {
+		return;
 	}
+
+	if (!mw.user.options.get('usebetatoolbar')) {
+		return;
+	}
+
+	await mw.loader.using('ext.wikiEditor');
+
+	$(function callCustomizer() {
+		const $body = $('body');
+		customizer.call($body.find('#wpTextbox1'));
+	});
 };
 
 /* Generic code */
