775b24651e983e7cc1e1f37bc3a25f9f1fe4a1ac
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-11-26 10:56:01 +0800

    chore: minor fix (follow commit 7bcff96) (#537)
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/Group-user_JS/modules/linkSearchEditButton.ts
+++ src/Group-user_JS/modules/linkSearchEditButton.ts
@@ -1,3 +1,5 @@
+import {getMessage} from './i18n';
+
 export const linkSearchEditButton = (): void => {
 	/* 在[[Special:LinkSearch]]显示“编辑”按钮 */
 	if (mw.config.get('wgCanonicalSpecialPageName') !== 'LinkSearch') {
@@ -7,7 +9,11 @@ export const linkSearchEditButton = (): void => {
 	$body.find('.mw-spcontent a[href^="/wiki"]').each((_index, element): void => {
 		$('<span>')
 			.append($(document.createTextNode(' （')))
-			.append($(`<a href="${(element as HTMLAnchorElement).href}?action=edit">编辑</a>`))
+			.append(
+				$('<a>')
+					.attr('href', `${(element as HTMLAnchorElement).href}?action=edit`)
+					.text(getMessage('Edit'))
+			)
 			.append($(document.createTextNode('）')))
 			.insertAfter(element);
 	});
