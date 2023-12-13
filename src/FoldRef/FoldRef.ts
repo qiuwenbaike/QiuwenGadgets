b129a836f2ecf5de651de68c29ba66c0c013b217
安忆 <i@anyi.in>
2023-12-14 02:11:23 +0800

    refactor(FoldRef): separate into modules

--- src/FoldRef/FoldRef.ts
+++ src/FoldRef/FoldRef.ts
@@ -1,6 +1,7 @@
 import {WG_NAMESPACE_NUMBER} from './modules/constant';
 import {foldRef} from './modules/core';
+import {getBody} from '~/util';
 
 if (WG_NAMESPACE_NUMBER === 0) {
-	$(foldRef);
+	getBody().then(foldRef);
 }
