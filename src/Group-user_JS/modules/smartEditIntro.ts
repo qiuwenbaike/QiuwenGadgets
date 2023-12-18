da620a8b69d7fab1e7fddd07610efbfa0fa6c193
WaitSpring <me@waitspring.com>
2023-12-18 14:17:58 +0800

    chore(GroupUser_JS): rename variables
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/Group-user_JS/modules/smartEditIntro.ts
+++ src/Group-user_JS/modules/smartEditIntro.ts
@@ -11,43 +11,43 @@ export const editIntro = (): void => {
 				element.href = `${$(element).attr('href')}&editintro=${name}`;
 			});
 	};
-	const cats: string[] = mw.config.get('wgCategories');
-	if (!cats) {
+	const categories: string[] = mw.config.get('wgCategories');
+	if (!categories) {
 		return;
 	}
 	switch (mw.config.get('wgNamespaceNumber')) {
 		case 0:
-			if (cats.includes('全部消歧义页面')) {
+			if (categories.includes('全部消歧义页面')) {
 				addEditIntro('Template:Disambig_editintro');
 			}
-			if (cats.includes('在世人物')) {
+			if (categories.includes('在世人物')) {
 				addEditIntro('Template:BLP_editintro');
 			}
 			if (
 				/抗日?[战戰][争爭]?[牺犧]牲|烈士|[战戰][斗鬥鬦]英雄|英雄?模[範范]?|英雄?烈士?|人民(教育家|[艺藝][术術]家|科[学學]家|英雄|楷模)|共和[国國][勋勳]章|[七八]一[勋勳]章[獲获]得者|[一特]等功臣/.test(
-					cats.toString()
+					categories.toString()
 				) === true
 			) {
 				addEditIntro('Template:BLP_editintro');
 			}
 			break;
 		case 4:
-			if (cats.includes('求闻百科方针完整列表')) {
+			if (categories.includes('求闻百科方针完整列表')) {
 				addEditIntro('Template:Policy_editintro');
 			}
 			break;
 		case 8:
 		case 12:
-			if (cats.includes('CC-BY-NC-SA-4.0')) {
+			if (categories.includes('CC-BY-NC-SA-4.0')) {
 				addEditIntro('Template:NonCommercial_editintro');
 			}
-			if (cats.includes('GPL-3.0')) {
+			if (categories.includes('GPL-3.0')) {
 				addEditIntro('Template:GPL-3.0_editintro');
 			}
-			if (cats.includes('GPL-2.0-or-later')) {
+			if (categories.includes('GPL-2.0-or-later')) {
 				addEditIntro('Template:GPL-2.0-or-later_editintro');
 			}
-			if (cats.includes('MIT许可证')) {
+			if (categories.includes('MIT许可证')) {
 				addEditIntro('Template:MIT_editintro');
 			}
 			if (
