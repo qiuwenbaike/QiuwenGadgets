855c58c31f325ebc44c5eb01c3e75a4f451b7aed
安忆 <i@anyi.in>
2023-12-19 22:33:25 +0800

    refactor(HideLongSummaries): optimize logic

--- src/HideLongSummaries/modules/constant.ts
+++ src/HideLongSummaries/modules/constant.ts
@@ -1,1 +1,6 @@
-export const LIMIT: number = 250;
+const CLASS_NAME: string = 'gadget-hide_long_summaries';
+const CLASS_NAME_LONG: string = `${CLASS_NAME}--long`;
+const CLASS_NAME_SHORT: string = `${CLASS_NAME}--short`;
+const CLASS_NAME_SWITCH: string = `${CLASS_NAME}-switch`;
+
+export {CLASS_NAME_LONG, CLASS_NAME_SHORT, CLASS_NAME_SWITCH};
