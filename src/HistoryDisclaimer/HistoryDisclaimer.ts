f9b61e2f28e7edb7681340fd9aebc2b2066e05cf
安忆 <i@anyi.in>
2023-12-19 22:51:30 +0800

    refactor(HistoryDisclaimer): separate into modules

--- src/HistoryDisclaimer/HistoryDisclaimer.ts
+++ src/HistoryDisclaimer/HistoryDisclaimer.ts
@@ -1,12 +1,17 @@
-const historyDisclaimer = (): void => {
-	if (
-		(mw.config.get('wgCurRevisionId') || -1) > 0 &&
-		(mw.config.get('wgRevisionId') || -1) > 0 &&
-		(mw.config.get('wgCurRevisionId') || -1) > (mw.config.get('wgRevisionId') || -1) &&
-		!document.querySelector('#historyDisclaimer')
-	) {
-		$('<div>').attr('id', 'historyDisclaimer').appendTo($('body'));
-	}
-};
+import {CLASS_NAME, WG_CUR_REVISION_ID, WG_REVISION_ID} from './modules/constant';
+import {getBody} from '~/util';
 
-$(historyDisclaimer);
+getBody().then(function historyDisclaimer($body: JQuery<HTMLBodyElement>): void {
+	if (!WG_CUR_REVISION_ID || !WG_REVISION_ID || WG_CUR_REVISION_ID <= WG_REVISION_ID) {
+		return;
+	}
+
+	if ($body.find(`.${CLASS_NAME}`).length) {
+		return;
+	}
+
+	// The following classes are used here:
+	// * see constant.ts
+	// * for more information
+	$('<div>').addClass(CLASS_NAME).appendTo($body);
+});
---
+++ src/HistoryDisclaimer/modules/constant.ts
@@ -0,0 +1,6 @@
+const CLASS_NAME: string = 'gadget-history_disclaimer';
+
+const WG_CUR_REVISION_ID: number = mw.config.get('wgCurRevisionId');
+const WG_REVISION_ID: number = mw.config.get('wgRevisionId');
+
+export {CLASS_NAME, WG_CUR_REVISION_ID, WG_REVISION_ID};
