6b0322c67be88f4a62fb2177e0474590f5233a08
安忆 <i@anyi.in>
2023-12-20 17:14:30 +0800

    refactor(ListUsersLastActive): separate into modules (#703)

---
+++ src/ListUsersLastActive/modules/constant.ts
@@ -0,0 +1,4 @@
+const WG_CANONICAL_SPECIAL_PAGE_NAME: string | false = mw.config.get('wgCanonicalSpecialPageName');
+const WG_WIKI_ID: string = mw.config.get('wgWikiID');
+
+export {WG_CANONICAL_SPECIAL_PAGE_NAME, WG_WIKI_ID};
