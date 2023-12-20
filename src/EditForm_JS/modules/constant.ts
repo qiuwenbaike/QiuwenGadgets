6b1af0f2b8b657382b14e64a194d636029d827a6
安忆 <i@anyi.in>
2023-12-21 02:02:11 +0800

    refactor(EditForm): optimize logic

--- src/EditForm_JS/modules/constant.ts
+++ src/EditForm_JS/modules/constant.ts
@@ -1,6 +1,6 @@
 const WG_ACTION: MediaWikiConfigMapWgAction = mw.config.get('wgAction');
 const WG_PAGE_NAME: string = mw.config.get('wgPageName');
-const WG_USER_GROUPS: string[] | null = mw.config.get('wgUserGroups');
+const WG_USER_GROUPS: string[] = mw.config.get('wgUserGroups') ?? [];
 
 const IS_WG_EDIT_OR_SUBMIT_ACTION: boolean = ['edit', 'submit'].includes(WG_ACTION);
 
