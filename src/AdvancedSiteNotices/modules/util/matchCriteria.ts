5fb39675404f74b77c64aa7b5c4215990334ae28
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2023-12-20 22:10:52 +0800

    refactor(ASN): fix typo; re-construct (#707)
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/AdvancedSiteNotices/modules/util/matchCriteria.ts
+++ src/AdvancedSiteNotices/modules/util/matchCriteria.ts
@@ -1,34 +1,35 @@
 /* eslint-disable camelcase */
 import {WG_USER_GROUPS, WG_USER_LANGUAGE} from '../constant';
 
-const in_group = (group: string): boolean => {
-	const wgUserGroups: string[] | null = WG_USER_GROUPS;
-	return wgUserGroups?.includes(group) ?? false;
-};
-
-const only_for = (userLanguage: string): boolean => {
-	return userLanguage === WG_USER_LANGUAGE;
-};
-
-const testCriteria = (criteria: string): boolean => {
-	// FIXME: This shouldn't be using eval on data entered in wikitext.
-	// If that data is malformed it will throw an exception e.g. criteria = "(false))"
-	try {
-		// eslint-disable-next-line no-eval
-		return window.eval(criteria);
-	} catch {
-		return false;
-	}
-};
-
 const matchCriteria = ($notice: JQuery): boolean => {
 	const cache: boolean | undefined = $notice.data('asn-cache');
 	if (cache !== undefined) {
 		return cache;
 	}
 
+	const in_group = (group: string): boolean => {
+		const wgUserGroups: string[] | null = WG_USER_GROUPS;
+		return wgUserGroups?.includes(group) ?? false;
+	};
+
+	const only_for = (userLanguage: string): boolean => {
+		return userLanguage === WG_USER_LANGUAGE;
+	};
+
+	const testCriteria = (criteria: string): boolean => {
+		// FIXME: This shouldn't be using eval on data entered in wikitext.
+		// If that data is malformed it will throw an exception e.g. criteria = "(false))"
+		try {
+			// eslint-disable-next-line no-eval
+			return window.eval(criteria);
+		} catch {
+			return false;
+		}
+	};
+
 	const criteriaData: string | undefined = $notice.attr('data-asn-criteria');
 	let result: boolean;
+
 	if (criteriaData) {
 		let criteria: string | undefined = mw.Uri.decode(criteriaData);
 		criteria = criteria.trim() || 'true';
@@ -39,7 +40,7 @@ const matchCriteria = ($notice: JQuery): boolean => {
 			criteria ||= in_group('sysop');
 		}
 		if ($notice.hasClass('only_logged')) {
-			criteria ||= in_group('sysop');
+			criteria ||= in_group('user');
 		}
 		if ($notice.hasClass('only_anon')) {
 			criteria ||= !in_group('user');
