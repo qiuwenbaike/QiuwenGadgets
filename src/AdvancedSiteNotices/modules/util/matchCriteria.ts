8301da6bf3b83fbefb91c55f7be8ec50deecc916
WaitSpring <me@waitspring.com>
2023-12-20 22:13:55 +0800

    fix(ASN): add all admin. groups
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/AdvancedSiteNotices/modules/util/matchCriteria.ts
+++ src/AdvancedSiteNotices/modules/util/matchCriteria.ts
@@ -20,8 +20,8 @@ const matchCriteria = ($notice: JQuery): boolean => {
 		// FIXME: This shouldn't be using eval on data entered in wikitext.
 		// If that data is malformed it will throw an exception e.g. criteria = "(false))"
 		try {
-			// eslint-disable-next-line no-eval
-			return window.eval(criteria);
+			// eslint-disable-next-line no-eval, security/detect-eval-with-expression
+			return eval(criteria);
 		} catch {
 			return false;
 		}
@@ -37,7 +37,7 @@ const matchCriteria = ($notice: JQuery): boolean => {
 	} else {
 		let criteria: string | boolean | undefined = !$notice.attr('class');
 		if ($notice.hasClass('only_sysop')) {
-			criteria ||= in_group('sysop');
+			criteria ||= in_group('sysop') || in_group('steward') || in_group('qiuwen');
 		}
 		if ($notice.hasClass('only_logged')) {
 			criteria ||= in_group('user');
