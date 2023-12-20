7eabb66c3e5c9fd22c5a2a208ad543b88831410a
WaitSpring <me@waitspring.com>
2023-12-20 21:54:08 +0800

    fix(ASN): eval()-related issues
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/AdvancedSiteNotices/modules/util/matchCriteria.ts
+++ src/AdvancedSiteNotices/modules/util/matchCriteria.ts
@@ -1,20 +1,20 @@
-/* eslint-disable camelcase, no-eval, @typescript-eslint/ban-ts-comment, @typescript-eslint/no-unused-vars */
+/* eslint-disable camelcase */
 import {WG_USER_GROUPS, WG_USER_LANGUAGE} from '../constant';
 
-const testCriteria = (criteria: string): boolean => {
-	// @ts-ignore
-	const in_group = (group: string): boolean => {
-		const wgUserGroups: string[] | null = WG_USER_GROUPS;
-		return wgUserGroups?.includes(group) ?? false;
-	};
-	// @ts-ignore
-	const only_for = (userLanguage: string): boolean => {
-		return userLanguage === WG_USER_LANGUAGE;
-	};
+const in_group = (group: string): boolean => {
+	const wgUserGroups: string[] | null = WG_USER_GROUPS;
+	return wgUserGroups?.includes(group) ?? false;
+};
 
+const only_for = (userLanguage: string): boolean => {
+	return userLanguage === WG_USER_LANGUAGE;
+};
+
+const testCriteria = (criteria: string): boolean => {
 	// FIXME: This shouldn't be using eval on data entered in wikitext.
 	// If that data is malformed it will throw an exception e.g. criteria = "(false))"
 	try {
+		// eslint-disable-next-line no-eval
 		return window.eval(criteria);
 	} catch {
 		return false;
@@ -27,42 +27,44 @@ const matchCriteria = ($notice: JQuery): boolean => {
 		return cache;
 	}
 
-	let criteria: string | undefined = $notice.attr('data-asn-criteria');
-	if (criteria) {
-		criteria = mw.Uri.decode(criteria);
+	const criteriaData: string | undefined = $notice.attr('data-asn-criteria');
+	let result: boolean;
+	if (criteriaData) {
+		let criteria: string | undefined = mw.Uri.decode(criteriaData);
 		criteria = criteria.trim() || 'true';
+		result = testCriteria(criteria);
 	} else {
-		criteria = $notice.attr('class') ? 'false' : 'true';
+		let criteria: string | boolean | undefined = !$notice.attr('class');
 		if ($notice.hasClass('only_sysop')) {
-			criteria += '||in_group("sysop")';
+			criteria ||= in_group('sysop');
 		}
 		if ($notice.hasClass('only_logged')) {
-			criteria += '||in_group("user")';
+			criteria ||= in_group('sysop');
 		}
 		if ($notice.hasClass('only_anon')) {
-			criteria += '||!in_group("user")';
+			criteria ||= !in_group('user');
 		}
 		if ($notice.hasClass('only_zh_cn')) {
-			criteria += '||only_for("zh-cn")';
+			criteria ||= only_for('zh-cn');
 		}
 		if ($notice.hasClass('only_zh_sg')) {
-			criteria += '||only_for("zh-sg")';
+			criteria ||= only_for('zh-sg');
 		}
 		if ($notice.hasClass('only_zh_my')) {
-			criteria += '||only_for("zh-my")';
+			criteria ||= only_for('zh-my');
 		}
 		if ($notice.hasClass('only_zh_hk')) {
-			criteria += '||only_for("zh-hk")';
+			criteria ||= only_for('zh-hk');
 		}
 		if ($notice.hasClass('only_zh_mo')) {
-			criteria += '||only_for("zh-mo")';
+			criteria ||= only_for('zh-mo');
 		}
 		if ($notice.hasClass('only_zh_tw')) {
-			criteria += '||only_for("zh-tw")';
+			criteria ||= only_for('zh-tw');
 		}
+		result = criteria;
 	}
 
-	const result: boolean = testCriteria(criteria);
 	$notice.data('asn-cache', result);
 
 	return result;
