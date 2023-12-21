7fe5d2a7d90aac34f32fd8a4cf87e1c3ea653eb9
安忆 <i@anyi.in>
2023-12-21 19:41:24 +0800

    refactor(LogFilter): split into modules

--- src/LogFilter/modules/core.ts
+++ src/LogFilter/modules/core.ts
@@ -1,177 +1,169 @@
-export const filterLists = {
-	i18n: {
-		'filter-portlet-text': '过滤',
-		'filter-portlet-description': '打开交互式日志正则过滤器',
-		'filter-gadget-page': 'MediaWiki:Gadget-LogFilter.js',
-		'filter-rights-list-instructions': '正则表达式过滤以空格分隔的权限列表。',
-		'filter-other-list-instructions': '正则表达式过滤本列表的纯文本样式。',
-		'filter-legend': '过滤列表',
-		'filter-option-added': '添加权限',
-		'filter-option-removed': '删除权限',
-		'filter-option-added-or-removed': '添加或删除权限',
-		'filter-option-added-removed-static': '已添加/已移除/保持不变',
-		'filter-regex-label': '正则表达式：',
-		'filter-invert-label': '反选',
-		'filter-case-label': '大小写敏感',
-		'filter-filter-button': '过滤',
-		'filter-highlight-button': '高亮',
-	},
-	rightsLogOnly: location.href.includes('type=rights') || location.href.includes('Log/rights'),
-	lastClicked: false,
-	msg(key: string): string {
-		key = `filter-${key}`;
-		// Messages that can be used here:
-		// * see above `i18n`
-		// * for more information
-		return mw.message(key).plain();
-	},
-	load(): void {
-		const {msg, i18n} = filterLists;
+import {URL_LIFILTER, URL_LIFILTER_CASE, URL_LIFILTER_EXPR, URL_LIFILTER_HILIGHT, URL_LIFILTER_INV} from './constant';
+import {MESSAGES} from './messages';
+
+class LogFilter {
+	private readonly rightsLogOnly = location.href.includes('type=rights') || location.href.includes('Log/rights');
+	private lastClicked = false;
+	private $body: JQuery<HTMLBodyElement>;
+
+	public constructor($body: JQuery<HTMLBodyElement>) {
+		this.$body = $body;
 		// Define interface messages
-		mw.messages.set(i18n);
+		mw.messages.set(MESSAGES);
+	}
+
+	public addPortletLink(): void {
 		// Create portlet link
-		const portletId: 'p-cactions' | 'p-tb' = document.querySelector('#p-cactions') ? 'p-cactions' : 'p-tb';
-		const portletlink: HTMLLIElement | null = mw.util.addPortletLink(
-			portletId,
+		const portletLink: HTMLLIElement | null = mw.util.addPortletLink(
+			this.$body.find('#p-cactions').length ? 'p-cactions' : 'p-tb',
 			'#',
-			msg('portlet-text'),
+			LogFilter.msg('portlet-text'),
 			'ca-rxfilter',
-			msg('portlet-description')
+			LogFilter.msg('portlet-description')
 		);
 		// Bind click listener
-		if (portletlink) {
-			$(portletlink).on('click', (event: JQuery.ClickEvent<HTMLLIElement>): void => {
+		if (portletLink) {
+			const $portletLink: JQuery = $(portletLink);
+			$portletLink.find('a').on('click', (event: JQuery.ClickEvent): void => {
 				event.preventDefault();
-				filterLists.buildForm();
+				this.buildForm();
 			});
 		}
-		// Display form direclty ?
-		if (mw.util.getParamValue('lifilter') || mw.util.getParamValue('lifilterexpr')) {
-			filterLists.buildForm();
+		// Display form direclty?
+		if (URL_LIFILTER || URL_LIFILTER_EXPR) {
+			this.buildForm();
 		}
-	},
-	buildForm(): void {
-		let fieldsetHtml: string;
-		let oldInput: string;
-		const {msg} = filterLists;
-		const instructions: string = this.rightsLogOnly
-			? msg('rights-list-instructions')
-			: msg('other-list-instructions');
-		const $body: JQuery<HTMLBodyElement> = $('body');
-		$body.find('#ca-rxfilter').hide();
-		const $pageTop: JQuery = $body.find('#contentSub, #topbar');
+	}
+
+	private buildForm(): void {
+		const $pageTop: JQuery = this.$body.find('#contentSub,#topbar');
 		if (!$pageTop.length) {
 			return;
 		}
+
+		let fieldsetHtml: string = '';
+		let oldInput: string = '';
+		const instructions: string = this.rightsLogOnly
+			? LogFilter.msg('rights-list-instructions')
+			: LogFilter.msg('other-list-instructions');
+
+		this.$body.find('#ca-rxfilter').hide();
+
 		// Set up config
-		const value: string | null = mw.util.getParamValue('lifilterexpr');
-		const casing: boolean = mw.util.getParamValue('lifiltercase') === '1';
-		const inverted: boolean = mw.util.getParamValue('lifilterinv') === '1';
+		const casing: boolean = URL_LIFILTER_CASE === '1';
+		const hilight: boolean = URL_LIFILTER_HILIGHT === '1';
+		const inverted: boolean = URL_LIFILTER_INV === '1';
+
 		// Generate the form
 		const $fieldSet: JQuery = $('<fieldset>').attr('id', 'rightsFilter').text(instructions);
-		const $legend: JQuery = $('<legend>').text(msg('legend'));
+		const $legend: JQuery = $('<legend>').text(LogFilter.msg('legend'));
 		$fieldSet.append($legend);
-		fieldsetHtml = '';
+
 		if (this.rightsLogOnly) {
 			fieldsetHtml += '<select id="rfSelect">';
-			fieldsetHtml += `<option>${mw.html.escape(msg('option-added'))}</option>`;
-			fieldsetHtml += `<option>${mw.html.escape(msg('option-removed'))}</option>`;
-			fieldsetHtml += `<option>${mw.html.escape(msg('option-added-or-removed'))}</option>`;
-			fieldsetHtml += `<option>${mw.html.escape(msg('option-added-removed-static'))}</option>`;
+			fieldsetHtml += `<option>${mw.html.escape(LogFilter.msg('option-added'))}</option>`;
+			fieldsetHtml += `<option>${mw.html.escape(LogFilter.msg('option-removed'))}</option>`;
+			fieldsetHtml += `<option>${mw.html.escape(LogFilter.msg('option-added-or-removed'))}</option>`;
+			fieldsetHtml += `<option>${mw.html.escape(LogFilter.msg('option-added-removed-static'))}</option>`;
 			fieldsetHtml += '</select>';
 		}
-		fieldsetHtml += `<label for="rfRegex">${mw.html.escape(msg('regex-label'))}</label><input id="rfRegex">`;
+		fieldsetHtml += `<label for="rfRegex">${mw.html.escape(
+			LogFilter.msg('regex-label')
+		)}</label><input id="rfRegex">`;
 		fieldsetHtml += `<input id="rfInvert" type="checkbox"><label for="rfInvert">${mw.html.escape(
-			msg('invert-label')
+			LogFilter.msg('invert-label')
 		)}</label>`;
 		fieldsetHtml += `<input id="rfCase" type="checkbox"><label for="rfCase">${mw.html.escape(
-			msg('case-label')
+			LogFilter.msg('case-label')
 		)}</label>`;
-		fieldsetHtml += `<button type="button" value="0">${mw.html.escape(msg('filter-button'))}</button>`;
-		fieldsetHtml += `<button type="button" value="1">${mw.html.escape(msg('highlight-button'))}</button>`;
+		fieldsetHtml += `<button type="button" value="0">${mw.html.escape(LogFilter.msg('filter-button'))}</button>`;
+		fieldsetHtml += `<button type="button" value="1">${mw.html.escape(LogFilter.msg('highlight-button'))}</button>`;
 		// Inject the html into the fieldset
 		$fieldSet.append(fieldsetHtml);
 		$pageTop.after($fieldSet);
+
 		// Set passed values
-		$body.find('#rfRegex').val(value ?? '');
-		$body.find('#rfCase').prop('checked', !casing);
-		$body.find('#rfInvert').prop('checked', inverted);
-		$body.find('#rfRegex').trigger('focus');
+		this.$body.find('#rfRegex').val(URL_LIFILTER_EXPR ?? '');
+		this.$body.find('#rfCase').prop('checked', !casing);
+		this.$body.find('#rfInvert').prop('checked', inverted);
+		this.$body.find('#rfRegex').trigger('focus');
+
 		// Bind click and change listeners
-		$body
+		this.$body
 			.find('#rightsFilter')
 			.find('input')
-			.on('keyup change', function (): void {
-				const $this: JQuery<HTMLInputElement> = $(this);
-				if ($this.attr('id') !== 'rfRegex') {
-					filterLists.filterLi();
+			.on('keyup change', (event): void => {
+				const $element: JQuery = $(event.currentTarget);
+				if ($element.attr('id') !== 'rfRegex') {
+					this.filterLi();
 					return;
 				}
-				const curInput: string = $this.val() as string;
+				const curInput: string = $element.val() as string;
 				if (oldInput !== curInput) {
 					oldInput = curInput;
-					filterLists.filterLi();
+					this.filterLi();
 				}
 			});
-		$fieldSet.find('button').on('click', function (): void {
-			filterLists.lastClicked = Boolean($(this).val());
-			filterLists.filterLi();
+		$fieldSet.find('button').on('click', (event): void => {
+			const $element: JQuery = $(event.currentTarget);
+			this.lastClicked = !!$element.val();
+			this.filterLi();
 		});
-		if (value) {
-			this.filterLi(mw.util.getParamValue('lifilterhilight') === '1');
+
+		if (URL_LIFILTER_EXPR) {
+			this.filterLi(hilight);
 		}
-	},
-	filterLi(hilight?: boolean) {
-		let type: string | undefined;
-		let rx: RegExp;
-		const $body: JQuery<HTMLBodyElement> = $('body');
-		// Grab options from form
-		if (this.rightsLogOnly) {
-			type = $body.find('#rfSelect').attr('selectedIndex');
-		}
-		const invert: boolean = $body.find('#rfInvert').prop('checked');
-		const search: string = $body.find('#rfRegex').val()?.toString() ?? '';
-		const flags: 'ig' | 'g' = $body.find('#rfCase').prop('checked') ? 'ig' : 'g';
-		hilight = !!(hilight || this.lastClicked.toString() === '1');
-		try {
-			rx = new RegExp(`${search}`, `${flags}`);
-		} catch (error) {
-			console.log(error);
-		}
+	}
+
+	private filterLi(hilight?: boolean): void {
 		// Grab the list of all <li> in the content
-		const $listItems: JQuery = $body.find('#bodyContent').find('li');
+		const $listItems: JQuery = this.$body.find('#bodyContent').find('li');
 		if (!$listItems.length) {
 			return;
 		}
+
+		hilight = !!(hilight || this.lastClicked.toString() === '1');
+
+		// Grab options from form
+		let type: string | undefined;
+		if (this.rightsLogOnly) {
+			type = this.$body.find('#rfSelect').attr('selectedIndex');
+		}
+
+		const invert: boolean = this.$body.find('#rfInvert').prop('checked');
+		const search: string = this.$body.find('#rfRegex').val()?.toString() ?? '';
+		const flags: string = this.$body.find('#rfCase').prop('checked') ? 'ig' : 'g';
+		let regexRf: RegExp | undefined;
+		try {
+			regexRf = new RegExp(`${search}`, `${flags}`);
+		} catch (error) {
+			console.log(error);
+		}
+
 		// The regex used to grab the Special:Log/rights changes
-		const rightrx: RegExp = /^.*?user:.*? from (.*?) to (.*?) \u200E.*?$/i;
+		const REGEX_RIGHT: RegExp = /^.*?user:.*?\sfrom\s(.*?)\sto\s(.*?)\s\u200E.*?$/i;
+
 		// Iterate
-		$listItems.each((_index: number, element: HTMLElement): void => {
-			let look: string = '';
-			let oldr: string;
-			let newr: string;
-			let oldrl: string[];
-			let newrl: string[];
-			let remr: string;
-			let addr: string;
+		for (const element of $listItems) {
 			const $element: JQuery = $(element);
-			// if (this.rightsLogOnly) {
-			if (location.href.includes('type=rights') || location.href.includes('Log/rights')) {
+			let look: string = '';
+
+			if (this.rightsLogOnly) {
 				// Special:Log/rights
-				oldr = $element.text().replace(rightrx, '$1');
-				newr = $element.text().replace(rightrx, '$2');
-				oldrl = oldr.split(', ');
-				newrl = newr.split(', ');
-				for (let j: number = 0; j < oldrl.length; j++) {
-					for (let k: number = 0; k < newrl.length; k++) {
-						if (oldrl[j] === newrl[k]) {
-							oldrl[j] = '';
-							newrl[k] = '';
+				const oldr: string = $element.text().replace(REGEX_RIGHT, '$1');
+				const newr: string = $element.text().replace(REGEX_RIGHT, '$2');
+				const oldrl: string[] = oldr.split(', ');
+				const newrl: string[] = newr.split(', ');
+				for (let i: number = 0; i < oldrl.length; i++) {
+					for (let j: number = 0; j < newrl.length; j++) {
+						if (oldrl[i] === newrl[j]) {
+							oldrl[i] = '';
+							newrl[j] = '';
 						}
 					}
 				}
-				remr = oldrl.join(',');
-				addr = newrl.join(',');
+				const remr: string = oldrl.join(',');
+				const addr: string = newrl.join(',');
 				switch (type) {
 					case '0':
 						look = addr;
@@ -190,15 +182,27 @@ export const filterLists = {
 				// Any other list of <li> objects
 				look = $element.text();
 			}
-			$element.show();
+
 			$element.css('background-color', '');
-			if ((look?.search(rx) !== -1 && !invert) || (look?.search(rx) === -1 && invert)) {
+			$element.show();
+
+			if (regexRf && ((look?.search(regexRf) === -1 && invert) || (look?.search(regexRf) !== -1 && !invert))) {
 				if (hilight) {
 					$element.css('background-color', '#ffff99');
 				}
 			} else if (!hilight) {
 				$element.hide();
 			}
-		});
-	},
-};
+		}
+	}
+
+	private static msg(key: string): string {
+		key = `filter-${key}`;
+		// Messages that can be used here:
+		// * see messages.ts
+		// * for more information
+		return mw.message(key).plain();
+	}
+}
+
+export {LogFilter};
