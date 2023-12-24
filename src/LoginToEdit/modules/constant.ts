032c167211c127dbf7f759d88727d3e94f30c45d
安忆 <i@anyi.in>
2023-12-25 00:05:59 +0800

    refactor(LoginToEdit): split into modules (#735)

--- src/LoginToEdit/modules/constant.ts
+++ src/LoginToEdit/modules/constant.ts
@@ -1,8 +1,8 @@
-const LOGIN_ELEMENT_SELECTOR: string = '#pt-login>a,.vector-user-menu-login>a,#user-tools>a[href*="Special"]';
-
 const WG_ACTION: MediaWikiConfigMapWgAction = mw.config.get('wgAction');
+const WG_PAGE_NAME: string = mw.config.get('wgPageName');
+const WG_SKIN: string = mw.config.get('skin');
 const WG_USER_NAME: string | null = mw.config.get('wgUserName');
 
 const IS_WG_EDIT_OR_SUBMIT_ACTION: boolean = ['edit', 'submit'].includes(WG_ACTION);
 
-export {LOGIN_ELEMENT_SELECTOR, WG_USER_NAME, IS_WG_EDIT_OR_SUBMIT_ACTION};
+export {WG_PAGE_NAME, WG_SKIN, WG_USER_NAME, IS_WG_EDIT_OR_SUBMIT_ACTION};
