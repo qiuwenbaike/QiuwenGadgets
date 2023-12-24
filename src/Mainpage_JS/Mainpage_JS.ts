f8136c6f48f98bfb769a3c500bc8a131ac3c3d9b
安忆 <i@anyi.in>
2023-12-25 00:39:14 +0800

    refactor(Mainpage_JS): split into modules

--- src/Mainpage_JS/Mainpage_JS.ts
+++ src/Mainpage_JS/Mainpage_JS.ts
@@ -1,6 +1,11 @@
-/* 隐藏首页红色链接 */
-if (mw.config.get('wgIsMainPage') && mw.config.get('wgAction') === 'view') {
-	/* Remove red links */
-	const $body: JQuery<HTMLBodyElement> = $('body');
+import {WG_ACTION, WG_IS_NAIN_PAGE} from './modules/constant';
+import {getBody} from '~/util';
+
+void getBody().then(($body: JQuery<HTMLBodyElement>): void => {
+	if (WG_ACTION !== 'view' || !WG_IS_NAIN_PAGE) {
+		return;
+	}
+
+	// 隐藏红色链接
 	$body.find('#mw-content-text a.new').contents().unwrap();
-}
+});
