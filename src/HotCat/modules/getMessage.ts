dd397bcccf24802ba366a9bf4ad226f4f8b07fb7
WaitSpring <me@waitspring.com>
2023-12-31 15:35:32 +0800

    fix(HC): all messages should be plain
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/HotCat/modules/getMessage.ts
+++ src/HotCat/modules/getMessage.ts
@@ -2,7 +2,7 @@ const getMessage = (key: string, ...args: string[]): string => {
 	// Messages that can be used here:
 	// * see RefToobarMessages.js
 	// * for more information
-	return args.length ? mw.message(key, ...args).parse() : mw.message(key).plain();
+	return args.length ? mw.message(key, ...args).plain() : mw.message(key).plain();
 };
 
 export {getMessage};
