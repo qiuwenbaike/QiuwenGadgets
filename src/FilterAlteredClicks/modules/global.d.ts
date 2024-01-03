96484fe0d1b7c2fc8eea8c267d57219ba305cff4
安忆 <i@anyi.in>
2024-01-03 19:56:18 +0800

    feat: add filter-altered-clicks as dependency (#845)

---
+++ src/FilterAlteredClicks/modules/global.d.ts
@@ -0,0 +1,5 @@
+type FilterAlteredClicks = typeof import('filter-altered-clicks').default;
+
+declare module 'ext.gadget.FilterAlteredClicks' {
+	export const filterAlteredClicks: FilterAlteredClicks;
+}
