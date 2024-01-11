52ee2c0f5a168a7ec16fbb05eb9c25ad667a2e2f
WaitSpring <me@waitspring.com>
2024-01-11 15:20:21 +0800

    refactor(WordCount): refactor

--- src/WordCount/WordCount.tsx
+++ src/WordCount/WordCount.tsx
@@ -3,29 +3,33 @@ import React from 'ext.gadget.React';
 import {getBody} from 'ext.gadget.Util';
 import {getMessage} from './modules/i18n';
 
-const countCJK = (text: string): number => {
-	text = text
-		.replace(/\./g, '')
-		.replace(
-			/[\u2E80-\u2E99\u2E9B-\u2EF3\u2F00-\u2FD5\u3005\u3007\u3021-\u3029\u3038-\u303B\u3400-\u4DB5\u4E00-\u9FCC\uF900-\uFA6D\uFA70-\uFAD9]|[\uD840-\uD868][\uDC00-\uDFFF]|\uD869[\uDC00-\uDED6\uDF00-\uDFFF]|[\uD86A-\uD86C][\uDC00-\uDFFF]|\uD86D[\uDC00-\uDF34\uDF40-\uDFFF]|\uD86E[\uDC00-\uDC1D]|\uD87E[\uDC00-\uDE1D]/g,
-			'.'
-		)
-		.replace(/[^.]/g, '');
-	return text.length;
-};
+const countLength = (text: string): number => text.length;
 
-const countByte = (text: string): number => {
-	text = text
-		.replace(/[\u0000-\u007F]/g, '.')
-		.replace(/[\u0080-\u07FF\uD800-\uDFFF]/g, '..')
-		.replace(/[\u0800-\uD7FF\uE000-\uFFFF]/g, '...');
-	return text.length;
-};
+const countCJK = (text: string): number =>
+	countLength(
+		text
+			.replace(/\./g, '')
+			.replace(
+				/[\u2E80-\u2E99\u2E9B-\u2EF3\u2F00-\u2FD5\u3005\u3007\u3021-\u3029\u3038-\u303B\u3400-\u4DB5\u4E00-\u9FCC\uF900-\uFA6D\uFA70-\uFAD9]|[\uD840-\uD868][\uDC00-\uDFFF]|\uD869[\uDC00-\uDED6\uDF00-\uDFFF]|[\uD86A-\uD86C][\uDC00-\uDFFF]|\uD86D[\uDC00-\uDF34\uDF40-\uDFFF]|\uD86E[\uDC00-\uDC1D]|\uD87E[\uDC00-\uDE1D]/g,
+				'.'
+			)
+			.replace(/[^.]/g, '')
+	);
 
-const getCountByTextLength = (text: string) =>
-	`${text.length}${getMessage('character(s)')}${getMessage('(')}${countCJK(text)}${getMessage('CJK')}${getMessage(
-		')'
-	)}<br>${countByte(text)}${getMessage('byte(s) in UTF-8 encoding')}`;
+const countByte = (text: string): number =>
+	countLength(
+		text
+			.replace(/[\u0000-\u007F]/g, '.')
+			.replace(/[\u0080-\u07FF\uD800-\uDFFF]/g, '..')
+			.replace(/[\u0800-\uD7FF\uE000-\uFFFF]/g, '...')
+	);
+
+const getCountByTextLength = (text: string) => `${countLength(text)}${getMessage('character(s)')}`;
+
+const getCJKCountByTextLength = (text: string) =>
+	`${getMessage('(')}${countCJK(text)}${getMessage('CJK')}${getMessage(')')}`;
+
+const getUTF8CountByTextLength = (text: string) => `${countByte(text)}${getMessage('byte(s) in UTF-8 encoding')}`;
 
 const wordCountCore = ($body: JQuery<HTMLBodyElement>): void => {
 	$body.find(`.${CLASS_NAME}`).remove();
@@ -35,7 +39,12 @@ const wordCountCore = ($body: JQuery<HTMLBodyElement>): void => {
 	}
 
 	const rootElement = () => (
-		<div className={[CLASS_NAME, 'noprint ui-state-highlight ui-corner-all']}>{getCountByTextLength(text)}</div>
+		<div className={[CLASS_NAME, 'noprint ui-state-highlight ui-corner-all']}>
+			{getCountByTextLength(text)}
+			{getCJKCountByTextLength(text)}
+			<br />
+			{getUTF8CountByTextLength(text)}
+		</div>
 	);
 
 	$body.append(rootElement());
@@ -47,10 +56,6 @@ const wordCountCore = ($body: JQuery<HTMLBodyElement>): void => {
 
 void getBody().then(function wordCount($body: JQuery<HTMLBodyElement>) {
 	const wordCountLoad = (): void => {
-		if (mw.config.get('wgAction') !== 'view') {
-			return;
-		}
-
 		let events;
 		if ('ontouchstart' in document) {
 			events = 'touchstart touchend';
