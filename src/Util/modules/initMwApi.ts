be9aff779c65d1d39d450dba1be748dd65baaac6
安忆 <i@anyi.in>
2024-01-11 18:47:35 +0800

    docs(Util): update comments (#879)

--- src/Util/modules/initMwApi.ts
+++ src/Util/modules/initMwApi.ts
@@ -1,5 +1,10 @@
 type InitMwApi = (userAgent?: string) => mw.Api;
 
+/**
+ * @requires mediawiki.api
+ * @param {string} [userAgent]
+ * @return {mw.Api}
+ */
 const initMwApi: InitMwApi = (userAgent) => {
 	return new mw.Api({
 		ajax: {
