f8e1d4acc5096086381e551bc6e3e5db1e018e74
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-01-17 12:12:22 +0800

    fix(CookieWarning): add a function to clear old cookies under subdomains (#900)
    * fix(CookieWarning): add a function to clear old cookies under subdomains
    
    * fix(CookieWarning): add a function to clear old cookies under subdomains
    
    * fix(CookieWarning): add a function to clear old cookies under subdomains

--- src/CookieWarning/modules/constant.ts
+++ src/CookieWarning/modules/constant.ts
@@ -2,10 +2,25 @@ const CLASS_NAME: string = 'gadget-cookie_warning';
 
 const STORAGE_KEY: string = 'ext.gadget.CookieWarning_cookieConsent';
 
+const COOKIE_CLEARED_KEY: string = `${STORAGE_KEY}__cookieCleared`;
+
 const LAST_STORAGE_VALUE: string | null = mw.storage.get(STORAGE_KEY) as string | null;
+
+const LAST_COOKIE_CLEARED_VALUE: string | null = mw.storage.get(COOKIE_CLEARED_KEY) as string | null;
 
 const URL_CONSENT_READ: string | null = mw.util.getParamValue('consentRead');
 
 const WG_USER_NAME: string | null = mw.config.get('wgUserName');
 
-export {CLASS_NAME, LAST_STORAGE_VALUE, STORAGE_KEY, URL_CONSENT_READ, WG_USER_NAME};
+const WG_WIKI_ID: string = mw.config.get('wgWikiID');
+
+export {
+	CLASS_NAME,
+	COOKIE_CLEARED_KEY,
+	LAST_COOKIE_CLEARED_VALUE,
+	LAST_STORAGE_VALUE,
+	STORAGE_KEY,
+	URL_CONSENT_READ,
+	WG_USER_NAME,
+	WG_WIKI_ID,
+};
