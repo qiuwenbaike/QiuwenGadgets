ddd4f9af1d23062e25bb9cbe151630650a687a1a
WaitSpring <me@waitspring.com>
2024-01-17 14:10:32 +0800

    update dist

--- src/DisamAssist/modules/constant.ts
+++ src/DisamAssist/modules/constant.ts
@@ -1,3 +1,4 @@
+const WG_ACTION: MediaWikiConfigMapWgAction = mw.config.get('wgAction');
 const WG_WIKI_ID: string = mw.config.get('wgWikiID');
 
-export {WG_WIKI_ID};
+export {WG_ACTION, WG_WIKI_ID};
