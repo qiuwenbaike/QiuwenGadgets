fe0a8240d52d376e21604e4c4cdc7575d6fecb1d
安忆 <i@anyi.in>
2024-01-22 15:16:14 +0800

    refactor(AjaxLogin): reuse `OO.ui.WindowManager` (#927)

--- src/AjaxLogin/modules/util/checkValid.tsx
+++ src/AjaxLogin/modules/util/checkValid.tsx
@@ -1,44 +1,53 @@
 import React from 'ext.gadget.React';
 import {getMessage} from '../i18n';
+import {oouiConfirm} from './oouiConfirm';
 import {toastify} from 'ext.gadget.Toastify';
 
 type NeedToCheckElements = [OO.ui.CheckboxInputWidget, OO.ui.TextInputWidget, OO.ui.TextInputWidget];
 
-const checkValid = (
+const checkValid = async (
 	[agreeTosCheckbox, nameInput, pwdInput]: NeedToCheckElements,
+	windowManager: OO.ui.WindowManager,
 	toastifyInstance: ToastifyInstance
-): {
+): Promise<{
+	isAgreeTos: boolean;
 	isValid: boolean;
 	toastifyInstance: ToastifyInstance;
-} => {
+}> => {
 	let isAgreeTos: boolean = agreeTosCheckbox.isSelected();
 	const isFill: boolean = ![nameInput.getValue(), pwdInput.getValue()].includes('');
 
 	toastifyInstance.hideToast();
+	await windowManager.clearWindows();
 
 	if (!isAgreeTos) {
-		void OO.ui
-			.confirm($((<span>{getMessage('DoubleCheckAgreedOrNot')}</span>) as HTMLElement), {
-				actions: [
-					{label: getMessage('Cancel'), action: 'cancel', flags: ['safe', 'close']},
-					{label: getMessage('I agree'), action: 'accept', flags: ['primary', 'progressive']},
-				],
-			})
-			.then((confirmed: boolean) => {
-				if (confirmed) {
-					isAgreeTos = true;
-					agreeTosCheckbox.setSelected(true);
-				} else {
-					isAgreeTos = false;
-					toastifyInstance = toastify(
-						{
-							text: getMessage('AgreedOrNot'),
-							duration: -1,
-						},
-						'info'
-					);
-				}
-			});
+		isAgreeTos = await oouiConfirm(windowManager, {
+			message: $(<span innerHTML={getMessage('DoubleCheckAgreedOrNot')} />) as JQuery,
+			actions: [
+				{
+					label: $(<b>{getMessage('Cancel')}</b>),
+					action: 'cancel',
+					flags: ['safe', 'close'],
+				},
+				{
+					label: $(<b>{getMessage('I agree')}</b>),
+					action: 'accept',
+					flags: ['primary', 'progressive'],
+				},
+			],
+		});
+
+		await windowManager.clearWindows();
+
+		if (!isAgreeTos) {
+			toastifyInstance = toastify(
+				{
+					text: getMessage('AgreedOrNot'),
+					duration: -1,
+				},
+				'info'
+			);
+		}
 	} else if (!isFill) {
 		toastifyInstance = toastify(
 			{
@@ -52,6 +61,7 @@ const checkValid = (
 	const isValid: boolean = isAgreeTos && isFill;
 
 	return {
+		isAgreeTos,
 		isValid,
 		toastifyInstance,
 	};
---
+++ src/AjaxLogin/modules/util/oouiConfirm.ts
@@ -0,0 +1,13 @@
+const oouiConfirm = async (windowManager: OO.ui.WindowManager, options: OO.ui.MessageDialog.SetupDataMap) => {
+	const messageDialog: OO.ui.MessageDialog = new OO.ui.MessageDialog();
+	windowManager.addWindows([messageDialog]);
+
+	const instance: OO.ui.WindowInstance = windowManager.openWindow(messageDialog, options);
+
+	const data: void = await instance.closed;
+	const _data = data as {action: string} | void;
+
+	return !!(_data?.action === 'accept');
+};
+
+export {oouiConfirm};
