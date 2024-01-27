c44d3fae852b4fa26604ec50a10ac912f2540d44
安忆 <i@anyi.in>
2024-01-27 15:18:03 +0800

    refactor: import type definitions

--- src/FilterAlteredClicks/modules/global.d.ts
+++ src/FilterAlteredClicks/modules/global.d.ts
@@ -1,3 +1,3 @@
 declare module 'ext.gadget.FilterAlteredClicks' {
-	export const filterAlteredClicks: typeof import('filter-altered-clicks').default;
+	export {default as filterAlteredClicks} from 'filter-altered-clicks';
 }
