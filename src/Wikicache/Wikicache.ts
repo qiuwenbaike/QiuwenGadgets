81cb36186b9fee71d1ea17da911f2a57f8712723
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-01-29 14:28:06 +0800

    fix(wikicache): disable on edit page without #wpTextbox1 or #wpSummary (#962)
    fix(wikicache): disable wikicache on submit pages without #wpTextbox1 or #wpSummary
    
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/Wikicache/Wikicache.ts
+++ src/Wikicache/Wikicache.ts
@@ -2,7 +2,7 @@ import {init} from './modules/core';
 import {wgAction} from './modules/constant';
 
 (() => {
-	if (!window.localStorage) {
+	if (!window.localStorage || !document.querySelector('#wpTextbox1') || !document.querySelector('#wpSummary')) {
 		return;
 	}
 
