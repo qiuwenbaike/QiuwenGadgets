50a9744e47088ac4afe8c99e1ccacc0f32a32b08
WaitSpring <me@waitspring.com>
2024-02-07 21:15:59 +0800

    feat(EditForm_JS): add introACH

--- src/EditForm_JS/modules/constant.ts
+++ src/EditForm_JS/modules/constant.ts
@@ -1,7 +1,8 @@
 const WG_ACTION: MediaWikiConfigMapWgAction = mw.config.get('wgAction');
+const WG_NAMESPACE_NUMBER: number = mw.config.get('wgNamespaceNumber');
 const WG_PAGE_NAME: string = mw.config.get('wgPageName');
 const WG_USER_GROUPS: string[] = mw.config.get('wgUserGroups') ?? [];
 
 const IS_WG_EDIT_OR_SUBMIT_ACTION: boolean = ['edit', 'submit'].includes(WG_ACTION);
 
-export {WG_PAGE_NAME, WG_USER_GROUPS, IS_WG_EDIT_OR_SUBMIT_ACTION};
+export {WG_PAGE_NAME, WG_NAMESPACE_NUMBER, WG_USER_GROUPS, IS_WG_EDIT_OR_SUBMIT_ACTION};
