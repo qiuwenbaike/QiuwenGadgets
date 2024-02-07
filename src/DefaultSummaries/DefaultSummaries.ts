c0e06106e932920559eed897c5c4ec1ce2709bb5
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-02-07 20:21:02 +0800

    fix(DefaultSummaries): follow commit 13a0e4ea (#998)

--- src/DefaultSummaries/DefaultSummaries.ts
+++ src/DefaultSummaries/DefaultSummaries.ts
@@ -8,11 +8,6 @@ void getBody().then(function defaultSummaries($body: JQuery<HTMLBodyElement>): v
 	});
 
 	mw.hook('ve.saveDialog.stateChanged').add((): void => {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-		mw.libs['ve'].addPlugin((): void => {
-			mw.hook('ve.saveDialog.stateChanged').add(() => {
-				processVisualEditor();
-			});
-		});
+		processVisualEditor();
 	});
 });
