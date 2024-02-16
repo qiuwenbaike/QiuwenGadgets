baa2143e2fcca00a9e8723613a7ddd7220152507
安忆 <i@anyi.in>
2024-02-16 22:05:37 +0800

    fix(Util): #1012

--- src/Util/modules/initMwApi.ts
+++ src/Util/modules/initMwApi.ts
@@ -1,28 +1,28 @@
-type InitMwApi = (userAgent?: string, apiUri?: string) => mw.Api | mw.ForeignApi;
+type InitMwApi = typeof initMwApi;
 
 /**
- * @requires mediawiki.apiUri
+ * @requires mediawiki.api
  * @param {string} [userAgent]
  * @param {string} [apiUri]
  * @return {mw.Api|mw.ForeignApi}
  */
-const initMwApi: InitMwApi = (userAgent, apiUri) => {
-	if (apiUri) {
-		return new mw.ForeignApi(apiUri, {
-			ajax: {
-				headers: {
-					'Api-User-Agent': userAgent ? `Qiuwen/1.1 (${userAgent})` : 'Qiuwen/1.1',
-				},
-			},
-		});
-	}
-	return new mw.Api({
+function initMwApi(userAgent?: string): mw.Api;
+function initMwApi(userAgent: string, apiUri: string): mw.ForeignApi;
+// eslint-disable-next-line func-style
+function initMwApi(userAgent?: string, apiUri?: string): mw.Api | mw.ForeignApi {
+	const apiOptions = {
 		ajax: {
 			headers: {
 				'Api-User-Agent': userAgent ? `Qiuwen/1.1 (${userAgent})` : 'Qiuwen/1.1',
 			},
 		},
-	});
-};
+	};
+
+	if (apiUri) {
+		return new mw.ForeignApi(apiUri, apiOptions);
+	}
+
+	return new mw.Api(apiOptions);
+}
 
 export {type InitMwApi, initMwApi};
