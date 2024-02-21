41fefc93d496814cdea8e0884b536f9b5015bd9a
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-02-21 11:39:50 +0800

    refactor(MarkRights-Userpage): split TSX modules; add type userRights (#1041)

--- src/MarkRights-Userpage/MarkRights-Userpage.ts
+++ src/MarkRights-Userpage/MarkRights-Userpage.ts
@@ -5,8 +5,6 @@ if (WG_RELEVANT_USER_NAME && mw.config.get('wgNamespaceNumber') === 2 && mw.conf
 	const relevantUserPageName: string = new mw.Title(WG_RELEVANT_USER_NAME, 2).toText();
 	const pageName: string = new mw.Title(mw.config.get('wgPageName')).toText();
 	if (relevantUserPageName === pageName) {
-		$(() => {
-			void getPermissions();
-		});
+		void getPermissions();
 	}
 }
