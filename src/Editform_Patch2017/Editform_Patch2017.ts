7eb871fcfd5ffada795929968569aba18e91e0b8
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-02-24 19:25:54 +0800

    refactor(EditForm_*): replace placeholder with message texts; split constant (#1092)
    * refactor(Editform_AiAssisted): split constant
    
    * refactor(Editform_Patch2017): replace placeholder with message texts

--- src/Editform_Patch2017/Editform_Patch2017.ts
+++ src/Editform_Patch2017/Editform_Patch2017.ts
@@ -1,12 +1,12 @@
 import {getBody} from 'ext.gadget.Util';
-import {getMessage} from './modules/i18n';
 import {processVisualEditor} from './modules/processVisualEditor';
+import {setMessages} from './modules/i18n';
 
 /**
  * @description 修正2017版源代码编辑器界面错误
  */
 void getBody().then(function editForm($body: JQuery<HTMLBodyElement>): void {
-	mw.messages.set({minoredit: getMessage('minoredit'), watchthis: getMessage('watchthis')});
+	setMessages();
 
 	mw.hook('ve.saveDialog.stateChanged').add((): void => {
 		processVisualEditor({
--- src/Editform_Patch2017/modules/processVisualEditor.ts
+++ src/Editform_Patch2017/modules/processVisualEditor.ts
@@ -1,7 +1,22 @@
-import {DIALOG_LICENSE_ID} from './constant';
+import {DIALOG_LICENSE_ID, TARGET_CLASS} from './constant';
 import {getMessage} from './i18n';
 
 const processVisualEditor = ({$body}: {$body: JQuery<HTMLBodyElement>}) => {
+	// Missing label messages
+	const $labels: JQuery = $body.find(`.${TARGET_CLASS}`).find('label');
+	for (const label of $labels) {
+		if (!label.textContent?.startsWith('⧼')) {
+			continue;
+		}
+
+		const labelName = label.textContent.replace('⧼', '').replace('⧽', '');
+
+		if (['minoredit', 'watchthis'].includes(labelName)) {
+			label.textContent = getMessage(labelName as 'minoredit' | 'watchthis');
+			label.title = getMessage(labelName as 'minoredit' | 'watchthis');
+		}
+	}
+
 	// Missing copyright warning
 	if (!$body.find(`.${DIALOG_LICENSE_ID}`).children().length) {
 		$body.find(`.${DIALOG_LICENSE_ID}`).append(
