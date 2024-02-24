d2ddd20e99bbca864b4acce727e8a2a6eff138af
安忆 <i@anyi.in>
2024-02-24 17:37:57 +0800

    refactor(StickyTableHeaders): optimize logic (#1089)
    * refactor(StickyTableHeaders): optimize logic

--- src/StickyTableHeaders/StickyTableHeaders.ts
+++ src/StickyTableHeaders/StickyTableHeaders.ts
@@ -1,3 +1,22 @@
-import {stickyTableHeaders} from './modules/core';
+import {getBody} from 'ext.gadget.Util';
 
-$(stickyTableHeaders);
+void getBody().then(($body: JQuery<HTMLBodyElement>): void => {
+	for (const table of $body.find<HTMLTableElement>('.wikitable:not(.sortable)')) {
+		const $table: JQuery<HTMLTableElement> = $(table);
+
+		const $thead: JQuery<HTMLTableSectionElement> = $table.find('thead');
+		const $trTh = $table.find('tbody>tr>th').parent().eq(0) as JQuery<HTMLTableRowElement>;
+		const $trTd = $table.find('tbody>tr>td').parent() as JQuery<HTMLTableRowElement>;
+
+		const $target: JQuery<HTMLTableSectionElement> | JQuery<HTMLTableRowElement> | null = $thead.length
+			? $thead
+			: $trTh.length
+				? $trTh
+				: null;
+
+		if ($target && $trTd.length >= 5) {
+			$table.addClass('mw-sticky-header');
+			$target.addClass('thead');
+		}
+	}
+});
