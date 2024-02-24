1f4e60bf00c0cdd2309f06748bb541190fe5bf08
WaitSpring <me@waitspring.com>
2024-02-24 19:06:22 +0800

    chore(PreviewWithVariant): add oid operator

--- src/PreviewWithVariant/PreviewWithVariant.ts
+++ src/PreviewWithVariant/PreviewWithVariant.ts
@@ -2,11 +2,11 @@ import {processVisualEditor} from './modules/processVisualEditor';
 import {processWikiEditor} from './modules/processWikiEditor';
 
 (function previewWithVariants(): void {
-	mw.hook('ve.saveDialog.stateChanged').add((): void => {
-		processVisualEditor();
-	});
-
 	mw.hook('wikipage.editform').add((): void => {
 		processWikiEditor();
+	});
+
+	mw.hook('ve.saveDialog.stateChanged').add((): void => {
+		processVisualEditor();
 	});
 })();
