65902c3b9c95ee8f881171105fb1840e1f10b321
WaitSpring <me@waitspring.com>
2024-02-25 15:55:32 +0800

    fix(Group-user_JS): messages should change when value of the text-input changes

--- src/Group-user_JS/modules/fixSummary.ts
+++ src/Group-user_JS/modules/fixSummary.ts
@@ -11,16 +11,32 @@ const fixSummary = ($body: JQuery<HTMLBodyElement>): void => {
 						.html()}|此处]]［页面文字原许可：[[cc-by-sa:4.0|CC BY-SA 4.0]]；文件许可请参见页面描述］`
 				);
 			break;
-		case 'Import':
-			$body.find('input[name=usernamePrefix]').val('zhwiki');
-			$body
-				.find('#mw-import-upload-form input[name=log-comment]')
-				.val('导入自[[zhwiki:|此网站]]的同名页面［页面文字原许可：[[cc-by-sa:4.0|CC BY-SA 4.0]]］');
+		case 'Import': {
+			// #mw-import-upload-form
+			const userNamePrefixInput = document.querySelectorAll(
+				'#mw-import-upload-form input[name=usernamePrefix]'
+			)[0] as HTMLInputElement;
+			const logCommentInput = document.querySelectorAll(
+				'#mw-import-upload-form input[name=log-comment]'
+			)[0] as HTMLInputElement;
+			const importUploadPrefix = userNamePrefixInput?.value;
+			if (importUploadPrefix) {
+				logCommentInput.value = `导入自[[${importUploadPrefix}:|此网站]]的同名页面［页面文字原许可：[[cc-by-sa:4.0|CC BY-SA 4.0]]］`;
+			}
+			userNamePrefixInput.addEventListener('input', () => {
+				const newValue = logCommentInput.value.replace(
+					/\[\[.*?:\|此网站\]\]/,
+					`[[${userNamePrefixInput.value}:|此网站]]`
+				);
+				logCommentInput.value = newValue;
+			});
+			// #mw-import-interwiki-form
 			$body
 				.find('#mw-import-interwiki-form input[name=log-comment]')
 				.val('［页面文字原许可：[[cc-by-sa:4.0|CC BY-SA 4.0]]；作者请参见来源页面历史］');
 			$body.find('input[name=assignKnownUsers]').prop('checked', true);
 			break;
+		}
 		case 'MassEditRegex':
 			$body.find('#wpSummaryLabel').html(
 				$body
