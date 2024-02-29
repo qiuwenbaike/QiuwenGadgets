e762fc07130a1dea3b280e032561bc03fed658e0
WaitSpring <me@waitspring.com>
2024-02-29 11:55:29 +0800

    fix(PreviewWithVariant): fix typo in messages

--- src/PreviewWithVariant/modules/messages.ts
+++ src/PreviewWithVariant/modules/messages.ts
@@ -4,12 +4,12 @@ const PWV2017messages = () => {
 		'pwv-2017-zh': window.wgULS('不转换', '不轉換'),
 		'pwv-2017-zh-hans': '简体',
 		'pwv-2017-zh-hant': '繁體',
-		'pwv-2017-zh-cn': '大陆简体',
-		'pwv-2017-zh-hk': '香港繁體',
-		'pwv-2017-zh-mo': '澳門繁體',
-		'pwv-2017-zh-my': '大马简体',
+		'pwv-2017-zh-cn': '中国大陆简体',
+		'pwv-2017-zh-hk': '中國香港繁體',
+		'pwv-2017-zh-mo': '中國澳門繁體',
+		'pwv-2017-zh-my': '马来西亚简体',
 		'pwv-2017-zh-sg': '新加坡简体',
-		'pwv-2017-zh-tw': '臺灣正體',
+		'pwv-2017-zh-tw': '中國臺灣繁體',
 	});
 };
 
