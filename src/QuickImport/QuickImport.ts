97d42ad781a59f480a0443a4107ed6fd16769fdc
安忆 <i@anyi.in>
2024-03-02 14:28:39 +0800

    refactor(QuickImport): optimize logic

--- src/QuickImport/QuickImport.ts
+++ src/QuickImport/QuickImport.ts
@@ -1,24 +1,59 @@
-import {quickImport} from './modules/core';
+import {detectIfFileRedirect, importPage, refreshPage} from './modules/quickImport';
+import {api} from './modules/api';
+import {getBody} from 'ext.gadget.Util';
 
-$(function quickImportLoad() {
-	const label: string =
-		mw.config.get('wgNamespaceNumber') === 6 && !document.querySelector('#mw-noarticletext')
-			? document.querySelectorAll("#ca-view-foreign a[href*='zh.wikipedia.org']")[0]
-				? 'zhwiki'
-				: 'commons'
-			: '';
-	const buttonLabel: string = document.querySelectorAll('.redirectText a')[0] ? '重定向目标' : '页面';
-	const portletId: 'p-cactions' | 'p-tb' = document.querySelector('#p-cactions') ? 'p-cactions' : 'p-tb';
+void getBody().then(($body: JQuery<HTMLBodyElement>): void => {
+	const {wgNamespaceNumber, wgPageName} = mw.config.get();
+
+	const caViewForeignZhwiki: HTMLElement | undefined = $body
+		.find("#ca-view-foreign a[href*='zh.wikipedia.org']")
+		.get(0);
+	const redirectTextA: HTMLElement | undefined = $body.find('.redirectText a').get(0);
+
+	const isFileNS: boolean = wgNamespaceNumber === 6;
+	const hasMwNoarticletext: boolean = !!$body.find('#mw-noarticletext').length;
+	const iwprefix: string = caViewForeignZhwiki ? 'zhwiki' : 'commons';
+
+	const label: string = isFileNS && !hasMwNoarticletext ? iwprefix : '';
+	const buttonLabel: string = redirectTextA ? '重定向目标' : '页面';
+
+	const portletId: 'p-cactions' | 'p-tb' = $body.find('#p-cactions').length ? 'p-cactions' : 'p-tb';
 	const element: HTMLLIElement | null = mw.util.addPortletLink(
 		portletId,
 		'#',
-		`导入${mw.config.get('wgNamespaceNumber') === 6 ? (label === '' ? '页面' : `文件（${label}）`) : `${buttonLabel}`}`,
+		`导入${isFileNS ? (label ? `文件（${label}）` : '页面') : buttonLabel}`,
 		't-import'
 	);
-
-	if (element) {
-		$(element).on('click', () => {
-			void quickImport();
-		});
+	if (!element) {
+		return;
 	}
+
+	// eslint-disable-next-line @typescript-eslint/no-misused-promises
+	element.addEventListener('click', async (): Promise<void> => {
+		const pageName: string = redirectTextA?.textContent || wgPageName;
+
+		if (isFileNS && !hasMwNoarticletext) {
+			await importPage(pageName, iwprefix, isFileNS);
+
+			const queryParams: ApiQueryParams = {
+				action: 'query',
+				prop: 'info',
+				titles: pageName,
+			};
+			const response = await api.get(queryParams);
+
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+			for (const [, pageinfo] of Object.entries(response['query'].pages)) {
+				if ((pageinfo as Record<string, never>)['missing']) {
+					await detectIfFileRedirect(pageName);
+				} else {
+					await importPage(pageName, 'zhwiki', isFileNS);
+					await detectIfFileRedirect(pageName);
+				}
+			}
+		} else {
+			await importPage(pageName, 'zhwiki');
+			refreshPage(wgPageName);
+		}
+	});
 });
