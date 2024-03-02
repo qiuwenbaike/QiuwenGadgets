9051eaa7b60feb08adf64c445afa26690f0596a6
安忆 <i@anyi.in>
2024-03-02 13:35:26 +0800

    refactor(QuickPatrol): clean up

--- src/QuickPatrol/QuickPatrol.ts
+++ src/QuickPatrol/QuickPatrol.ts
@@ -1,4 +1,4 @@
-import {PatrolAllPages, PatrolSinglePage} from './modules/PatrolPage';
+import {patrolAllPages, patrolSinglePage} from './modules/patrolPages';
 import {getBody} from 'ext.gadget.Util';
 
 void getBody().then(function quickPatrol($body: JQuery<HTMLBodyElement>): void {
@@ -7,25 +7,27 @@ void getBody().then(function quickPatrol($body: JQuery<HTMLBodyElement>): void {
 	}
 
 	/* Patrol single page */
-	for (const [index, element] of Object.entries(document.querySelectorAll('.not-patrolled'))) {
-		const $element: JQuery = $(element as HTMLElement);
+	for (const [index, element] of Object.entries($body.find('.not-patrolled'))) {
+		const $element: JQuery = $(element);
 
 		const $time: JQuery = $element.find('a');
 		if (!$time.length) {
 			continue;
 		}
+
 		const timeHref: string | undefined = $time.attr('href');
 		if (!timeHref) {
 			continue;
 		}
+
 		const revIds: RegExpMatchArray | null = timeHref?.match(/oldid=([0-9]+)/);
 		if (!revIds || !revIds[1]) {
 			continue;
 		}
 
-		element.append(PatrolSinglePage(index, revIds[1]));
+		element.append(patrolSinglePage(index, revIds[1]));
 	}
 
 	/* Patrol all pages */
-	$body.find('#mw-content-text ul').append(PatrolAllPages($body));
+	$body.find('#mw-content-text ul').append(patrolAllPages($body));
 });
