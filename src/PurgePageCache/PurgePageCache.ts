88df2018d1e9ce0fa9a847d3dc9ea2445e4acc37
安忆 <i@anyi.in>
2024-03-03 13:58:29 +0800

    refactor(PurgePageCache): optimize logic (#1124)
    * refactor(PurgePageCache): optimize logic

--- src/PurgePageCache/PurgePageCache.ts
+++ src/PurgePageCache/PurgePageCache.ts
@@ -1,11 +1,42 @@
-import {purgePageCache} from './modules/core';
+import {getBody} from 'ext.gadget.Util';
+import {getMessage} from './modules/i18n';
+import {purge} from './modules/purge';
 
-if (
-	mw.config.get('wgAction') === 'view' &&
-	mw.config.get('wgIsArticle') &&
-	mw.config.get('wgCurRevisionId') !== 0 &&
-	mw.config.get('wgRevisionId') !== 0 &&
-	mw.config.get('wgCurRevisionId') === mw.config.get('wgRevisionId')
-) {
-	$(purgePageCache);
-}
+void getBody().then(function purgePageCache($body: JQuery<HTMLBodyElement>): void {
+	const {wgAction, wgCurRevisionId, wgIsArticle, wgPageName, wgRevisionId} = mw.config.get();
+
+	if (
+		wgAction === 'view' &&
+		wgIsArticle &&
+		wgCurRevisionId !== 0 &&
+		wgRevisionId !== 0 &&
+		wgCurRevisionId === wgRevisionId
+	) {
+		return;
+	}
+
+	const portletId: 'p-cactions' | 'p-tb' = $body.find('#p-cactions').length ? 'p-cactions' : 'p-tb';
+	const portletElement: HTMLLIElement | null = mw.util.addPortletLink(
+		portletId,
+		'#',
+		getMessage('Purge'),
+		'ca-purge',
+		getMessage('PurgeFromServer')
+	);
+	if (!portletElement) {
+		return;
+	}
+
+	(portletElement.querySelector('a') as HTMLAnchorElement).addEventListener('click', (event: MouseEvent): void => {
+		event.preventDefault();
+		void purge(wgPageName);
+	});
+
+	for (const element of $body.find<HTMLAnchorElement>('a[href*="action=purge"]')) {
+		const title: string = mw.util.getParamValue('title', element.href) || wgPageName;
+		element.addEventListener('click', (event: MouseEvent): void => {
+			event.preventDefault();
+			void purge(title);
+		});
+	}
+});
