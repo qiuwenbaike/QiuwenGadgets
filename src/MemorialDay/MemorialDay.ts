4a27cc642ffe760c51a0e7e4b575f8832568ff61
WaitSpring <me@waitspring.com>
2024-03-04 17:12:10 +0800

    refactor(MemorialDay): support multiple titles

--- src/MemorialDay/MemorialDay.ts
+++ src/MemorialDay/MemorialDay.ts
@@ -10,17 +10,19 @@ const DAY: number = DATENOW.getDate();
 const {wgPageName} = mw.config.get();
 
 (function memorialDay(): void {
-	for (const {title, dates} of pageList) {
-		if (!(wgPageName === title)) {
-			continue;
-		}
-
-		for (const date of generateArray(dates)) {
-			if (!(MONTH * 100 + DAY === date || YEAR * 1e4 + MONTH * 100 + DAY === date || !date)) {
+	for (const {titles, dates} of pageList) {
+		for (const title of titles) {
+			if (!(wgPageName === title)) {
 				continue;
 			}
 
-			addStyleTag();
+			for (const date of generateArray(dates)) {
+				if (!(MONTH * 100 + DAY === date || YEAR * 1e4 + MONTH * 100 + DAY === date || !date)) {
+					continue;
+				}
+
+				addStyleTag();
+			}
 		}
 	}
 })();
--- src/MemorialDay/modules/pageList.ts
+++ src/MemorialDay/modules/pageList.ts
@@ -1,18 +1,10 @@
 export const pageList: {
-	title: string;
+	titles: string | string[];
 	dates?: number | number[];
 }[] = [
 	// In memorial of victims of the Nanjing Massacre
 	{
-		title: 'Qiuwen:首页',
-		dates: 1213,
-	},
-	{
-		title: '南京大屠杀死难者国家公祭日',
-		dates: 1213,
-	},
-	{
-		title: '侵华日军南京大屠杀遇难同胞纪念馆',
+		titles: ['南京大屠杀死难者国家公祭日', '侵华日军南京大屠杀遇难同胞纪念馆'],
 		dates: 1213,
 	},
 ];
