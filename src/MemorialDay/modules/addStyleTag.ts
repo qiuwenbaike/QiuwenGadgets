2d10fa942caae20bf1ed94d12228498a1b11fcef
WaitSpring <me@waitspring.com>
2024-03-04 17:08:16 +0800

    refactor(MemorialDay): support multiple dates

---
+++ src/MemorialDay/modules/addStyleTag.ts
@@ -0,0 +1,15 @@
+const styles = `.skin-citizen .mw-body,
+.skin-vector .mw-body,
+.skin-gongbi #mw-content {
+	-webkit-filter: grayscale(1) !important;
+	-moz-filter: grayscale(1) !important;
+	-ms-filter: grayscale(1) !important;
+	-o-filter: grayscale(1) !important;
+	filter: grayscale(1) !important;
+}`;
+
+const addStyleTag = () => {
+	mw.loader.addStyleTag(styles);
+};
+
+export {addStyleTag};
--- src/MemorialDay/modules/pageList.ts
+++ src/MemorialDay/modules/pageList.ts
@@ -1,17 +1,18 @@
 export const pageList: {
 	title: string;
-	date?: number;
+	dates?: number | number[];
 }[] = [
+	// In memorial of victims of the Nanjing Massacre
 	{
 		title: 'Qiuwen:首页',
-		date: 1213,
+		dates: 1213,
 	},
 	{
 		title: '南京大屠杀死难者国家公祭日',
-		date: 1213,
+		dates: 1213,
 	},
 	{
 		title: '侵华日军南京大屠杀遇难同胞纪念馆',
-		date: 1213,
+		dates: 1213,
 	},
 ];
