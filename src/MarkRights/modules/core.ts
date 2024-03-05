04b35892a23165df67048c6ab710236fab5d9762
WaitSpring <me@waitspring.com>
2024-03-05 13:09:57 +0800

    refactor(MarkRights): split initMwApi to api.ts

--- src/MarkRights/modules/core.ts
+++ src/MarkRights/modules/core.ts
@@ -1,6 +1,6 @@
 import type {UserRights} from './types';
+import {api} from './api';
 import {getMessage} from './i18n';
-import {initMwApi} from 'ext.gadget.Util';
 
 const groups: Record<UserRights, string[]> = {
 	// 全站管理型权限
@@ -30,109 +30,116 @@ const groups: Record<UserRights, string[]> = {
 	'rnrsverify-exempt': [],
 };
 
-export const markUserRights = async ($content: JQuery): Promise<void> => {
+const getUsername = (url: string): string => {
+	if (!url) {
+		return '';
+	}
+
+	const username: string | null = mw.util.getParamValue('title', url);
+
+	const decode = (string: string, replace: (_string: string) => string): string => {
+		return decodeURIComponent(
+			((): string => {
+				try {
+					return decodeURIComponent(replace(string));
+				} catch {
+					return replace(string).replace(/%(?!\d+)/g, '%25');
+				}
+			})()
+		);
+	};
+
+	if (username) {
+		return decode(username, (string: string): string => {
+			return string.replace('User:', '').replace(/_/g, ' ');
+		});
+	}
+	const usernameMatch: RegExpMatchArray | null = url.match(/\/wiki\/User:(.+?)$/);
+	if (usernameMatch?.[1]) {
+		return decode(usernameMatch[1], (string: string): string => {
+			return string.replace(/_/g, ' ');
+		});
+	}
+	return '';
+};
+
+const done = ($userLinks: JQuery<HTMLElement>): void => {
+	$userLinks.each((_index: number, element: HTMLElement): void => {
+		const $element: JQuery = $(element);
+		if ($element.parents('li').find('.gadgets-markrights').length) {
+			return;
+		}
+		const username: string = getUsername($element.attr('href') ?? '');
+		if (!username) {
+			return;
+		}
+		const $sups: JQuery = $('<span>').addClass('gadgets-markrights');
+		for (const group in groups) {
+			if (!Object.hasOwn(groups, group)) {
+				continue;
+			}
+			const groupsGroup: string[] = groups[group as never] ?? [];
+			if (!groupsGroup.includes(username)) {
+				continue;
+			}
+			const className: string = `gadgets-markrights__${group}`;
+			if ($sups.find('sup').hasClass(className)) {
+				return;
+			}
+			$sups.append(
+				// The following classes are used here:
+				// * gadget-markrights__qiuwen
+				// * gadget-markrights__steward
+				// * gadget-markrights__checkuser
+				// * gadget-markrights__suppress
+				// * gadget-markrights__sysop
+				// * gadget-markrights__interface-admin
+				// * gadget-markrights__templateeditor
+				// * gadget-markrights__transwiki
+				// * gadget-markrights__patroller
+				// * gadget-markrights__autoreviewer
+				// * gadget-markrights__senioreditor
+				// * gadget-markrights__eventsponsor
+				// * gadget-markrights__massmessage-sender
+				// * gadget-markrights__confirmed
+				// * gadget-markrights__autoconfirmed
+				// * gadget-markrights__bot
+				// * gadget-markrights__flood
+				// * gadget-markrights__ipblock-exempt
+				// * gadget-markrights__rnrsverify-exempt
+				$('<sup>')
+					.addClass(className)
+					.attr({
+						alt: getMessage(group as UserRights),
+						title: getMessage(group as UserRights),
+					})
+			);
+		}
+		$element.after($sups);
+	});
+};
+
+const markUserRights = async ($content: JQuery): Promise<void> => {
 	const $userLinks: JQuery = $content.find('a.mw-userlink:not(.mw-anonuserlink)');
 	let users: string[] = [];
 	const queue: Array<typeof users> = [];
+
 	$userLinks.each((_index: number, {textContent}: {textContent: string | null}): void => {
 		const userLinkText: string | undefined = textContent?.toString();
 		if (userLinkText) {
-			users[users.length] = userLinkText; // Replace `users.push(userLinkText)` to avoid polyfilling core-js
+			users[users.length] = userLinkText; // Replace `[].push` to avoid polyfilling
 		}
 		users = [...new Set(users)];
 		if (users.length === 50) {
-			queue[queue.length] = users; // Replace `queue.push(users)` to avoid polyfilling core-js
+			queue[queue.length] = users; // Replace `[].push` to avoid polyfilling
 			users = [];
 		}
 	});
+
 	if (users.length > 0) {
-		queue[queue.length] = users; // Replace `queue.push(users)` to avoid polyfilling core-js
+		queue[queue.length] = users; // Replace `[].push` to avoid polyfilling
 	}
-	const getUsername = (url: string): string => {
-		if (!url) {
-			return '';
-		}
-		const username: string | null = mw.util.getParamValue('title', url);
-		const decode = (string: string, replace: (_string: string) => string): string => {
-			return decodeURIComponent(
-				((): string => {
-					try {
-						return decodeURIComponent(replace(string));
-					} catch {
-						return replace(string).replace(/%(?!\d+)/g, '%25');
-					}
-				})()
-			);
-		};
-		if (username) {
-			return decode(username, (string: string): string => {
-				return string.replace('User:', '').replace(/_/g, ' ');
-			});
-		}
-		const usernameMatch: RegExpMatchArray | null = url.match(/\/wiki\/User:(.+?)$/);
-		if (usernameMatch?.[1]) {
-			return decode(usernameMatch[1], (string: string): string => {
-				return string.replace(/_/g, ' ');
-			});
-		}
-		return '';
-	};
-	const done = (): void => {
-		$userLinks.each((_index: number, element: HTMLElement): void => {
-			const $element: JQuery = $(element);
-			if ($element.parents('li').find('.gadgets-markrights').length) {
-				return;
-			}
-			const username: string = getUsername($element.attr('href') ?? '');
-			if (!username) {
-				return;
-			}
-			const $sups: JQuery = $('<span>').addClass('gadgets-markrights');
-			for (const group in groups) {
-				if (!Object.hasOwn(groups, group)) {
-					continue;
-				}
-				const groupsGroup: string[] = groups[group as never] ?? [];
-				if (!groupsGroup.includes(username)) {
-					continue;
-				}
-				const className: string = `gadgets-markrights__${group}`;
-				if ($sups.find('sup').hasClass(className)) {
-					return;
-				}
-				$sups.append(
-					// The following classes are used here:
-					// * gadget-markrights__qiuwen
-					// * gadget-markrights__steward
-					// * gadget-markrights__checkuser
-					// * gadget-markrights__suppress
-					// * gadget-markrights__sysop
-					// * gadget-markrights__interface-admin
-					// * gadget-markrights__templateeditor
-					// * gadget-markrights__transwiki
-					// * gadget-markrights__patroller
-					// * gadget-markrights__autoreviewer
-					// * gadget-markrights__senioreditor
-					// * gadget-markrights__eventsponsor
-					// * gadget-markrights__massmessage-sender
-					// * gadget-markrights__confirmed
-					// * gadget-markrights__autoconfirmed
-					// * gadget-markrights__bot
-					// * gadget-markrights__flood
-					// * gadget-markrights__ipblock-exempt
-					// * gadget-markrights__rnrsverify-exempt
-					$('<sup>')
-						.addClass(className)
-						.attr({
-							alt: getMessage(group as UserRights),
-							title: getMessage(group as UserRights),
-						})
-				);
-			}
-			$element.after($sups);
-		});
-	};
-	const api: mw.Api = initMwApi('MarkRights/1.1');
+
 	for (const ususers of queue) {
 		const params: ApiQueryUsersParams = {
 			action: 'query',
@@ -144,8 +151,9 @@ export const markUserRights = async ($content: JQuery): Promise<void> => {
 		};
 		const response = await api.get(params);
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-		const _users: {groups: string; name: string}[] = response['query']?.users ?? [];
-		for (const user of _users) {
+		const queryUsers: {groups: string; name: string}[] = response['query']?.users ?? [];
+
+		for (const user of queryUsers) {
 			if (!user || !user.groups) {
 				continue;
 			}
@@ -155,10 +163,13 @@ export const markUserRights = async ($content: JQuery): Promise<void> => {
 				}
 				const groupsGroup: string[] = groups[group as never] as string[];
 				if (user.groups.includes(group)) {
-					groupsGroup[groupsGroup.length] = user.name; // Replace `groupsGroup.push(user.name)` to avoid polyfilling core-js
+					groupsGroup[groupsGroup.length] = user.name; // Replace `[].push` to avoid polyfilling
 				}
 			}
 		}
-		done();
+
+		done($userLinks);
 	}
 };
+
+export {markUserRights};
