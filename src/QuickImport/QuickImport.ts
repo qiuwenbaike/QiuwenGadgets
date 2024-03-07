a0e0b65862b2d60d2144eaf7b363ddbd6a333c99
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-03-07 15:44:00 +0800

    refactor(QuickImport): add function of importing redirected files; optimize logic (#1159)
    * refactor(QuickImport): add function of importing redirected files; optimize logic

--- src/QuickImport/QuickImport.ts
+++ src/QuickImport/QuickImport.ts
@@ -1,23 +1,20 @@
 import {detectIfFileRedirect, importPage, refreshPage} from './modules/quickImport';
 import {api} from './modules/api';
-import {getBody} from 'ext.gadget.Util';
 
-void getBody().then(($body: JQuery<HTMLBodyElement>): void => {
+(function quickImport(): void {
 	const {wgNamespaceNumber, wgPageName} = mw.config.get();
 
-	const caViewForeignZhwiki: HTMLElement | undefined = $body
-		.find("#ca-view-foreign a[href*='zh.wikipedia.org']")
-		.get(0);
-	const redirectTextA: HTMLElement | undefined = $body.find('.redirectText a').get(0);
+	const [caViewForeignZhwiki] = document.querySelectorAll("#ca-view-foreign a[href*='zh.wikipedia.org']");
+	const [redirectTextA] = document.querySelectorAll('.redirectText a');
 
 	const isFileNS: boolean = wgNamespaceNumber === 6;
-	const hasMwNoarticletext: boolean = !!$body.find('#mw-noarticletext').length;
+	const hasMwNoarticletext: boolean = !!document.querySelector('#mw-noarticletext');
 	const iwprefix: string = caViewForeignZhwiki ? 'zhwiki' : 'commons';
 
 	const label: string = isFileNS && !hasMwNoarticletext ? iwprefix : '';
 	const buttonLabel: string = redirectTextA ? '重定向目标' : '页面';
 
-	const portletId: 'p-cactions' | 'p-tb' = $body.find('#p-cactions').length ? 'p-cactions' : 'p-tb';
+	const portletId: 'p-cactions' | 'p-tb' = document.querySelector('#p-cactions') ? 'p-cactions' : 'p-tb';
 	const element: HTMLLIElement | null = mw.util.addPortletLink(
 		portletId,
 		'#',
@@ -56,4 +53,4 @@ void getBody().then(($body: JQuery<HTMLBodyElement>): void => {
 			refreshPage(wgPageName);
 		}
 	});
-});
+})();
