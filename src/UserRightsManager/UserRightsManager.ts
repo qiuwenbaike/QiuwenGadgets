f27d3d38e35b77808bf7aaa4d8ed261800be724d
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-03-14 21:48:42 +0800

    refactor(URM): add i18n.ts (#1167)

--- src/UserRightsManager/UserRightsManager.ts
+++ src/UserRightsManager/UserRightsManager.ts
@@ -1,15 +1,15 @@
-import {PAGE_PERM} from './modules/constant';
 import {getBody} from 'ext.gadget.Util';
-import {showDialog} from './modules/core';
+import {getPermissionRequested} from './modules/i18n';
+import {showDialog} from './modules/core.js';
 
 void getBody().then(($body: JQuery<HTMLBodyElement>): void => {
 	const {wgPageName} = mw.config.get();
 
-	if (!Object.keys(PAGE_PERM).includes(wgPageName)) {
+	if (!getPermissionRequested(wgPageName) || getPermissionRequested(wgPageName) === wgPageName) {
 		return;
 	}
 
-	const permission: string | undefined = PAGE_PERM[wgPageName];
+	const permission: string | undefined = getPermissionRequested(wgPageName);
 
 	$body.find('.perm-assign-permissions a').on('click', (event: JQuery.ClickEvent): void => {
 		if (permission === 'AutoWikiBrowser') {
@@ -20,16 +20,16 @@ void getBody().then(($body: JQuery<HTMLBodyElement>): void => {
 
 		const $element: JQuery<HTMLAnchorElement> = $(event.target as HTMLAnchorElement);
 
-		const username: string = mw.util.getParamValue('user', $element.attr('href')) ?? '';
+		const userName: string = mw.util.getParamValue('user', $element.attr('href')) ?? '';
 		const sectionId: string = $element.parents('dl').prev('h4').find('.mw-headline').attr('id') ?? '';
 		const index: string =
-			sectionId === `User:${username}` ? '' : sectionId.replace('User:', '').replace(username, '');
+			sectionId === `User:${userName}` ? '' : sectionId.replace('User:', '').replace(userName, '');
 
 		showDialog({
 			$body,
-			_index: index,
-			_userName: username,
-			_permission: permission,
+			index,
+			userName,
+			permission,
 		});
 	});
 });
