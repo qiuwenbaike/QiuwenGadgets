e4604806e90347fede0329f2307b1f0fa4a593d2
WaitSpring <me@waitspring.com>
2024-03-28 14:47:40 +0800

    refactor(c, f): optimize logic

--- src/FullwidthSearchFix/FullwidthSearchFix.ts
+++ src/FullwidthSearchFix/FullwidthSearchFix.ts
@@ -1,7 +1,9 @@
-import {URL_FULLTEXT, URL_SEARCH, WG_NAMESPACE_IDS} from './modules/constant';
 import {redirect} from './modules/redirect';
 
 (function fullwidthSearchFix(): void {
+	const URL_FULLTEXT: string | null = mw.util.getParamValue('fulltext');
+	const URL_SEARCH: string | null = mw.util.getParamValue('search');
+
 	// don't be too aggresive. just fix some obvious typos
 	if (URL_FULLTEXT || !URL_SEARCH) {
 		return;
@@ -15,10 +17,12 @@ import {redirect} from './modules/redirect';
 
 	// if namespace is valid
 	const nameSpace: string = URL_SEARCH.slice(0, Math.max(0, colonIndex));
-	const pageName: string = URL_SEARCH.slice(Math.max(0, colonIndex + 1));
-	if (!WG_NAMESPACE_IDS[nameSpace.toLowerCase()]) {
+	const {wgNamespaceIds} = mw.config.get();
+	if (!wgNamespaceIds[nameSpace.toLowerCase()]) {
 		return;
 	}
+
+	const pageName: string = URL_SEARCH.slice(Math.max(0, colonIndex + 1));
 
 	redirect(nameSpace, pageName);
 })();
