4c64d29e4e1d2c95d54c6fe083eaf66c7882c594
WaitSpring <me@waitspring.com>
2024-04-08 21:07:19 +0800

    fix(QuickImport*): optimize logic

--- src/QuickImport/modules/core.ts
+++ src/QuickImport/modules/core.ts
@@ -37,27 +37,25 @@ const importPage = async (pageName: string, iwprefix: string, isFileNS: boolean 
 		summary += '；文件作者请参见此页面及来源页面记载';
 	}
 
-	try {
-		const params: ApiImportParams = {
-			action: 'import',
-			format: 'json',
-			assignknownusers: true,
-			interwikipage: pageName,
-			interwikiprefix: iwprefix,
-			interwikisource: iwprefix as NonNullable<ApiImportParams['interwikisource']>,
-			summary: `［${summary}］`,
-		};
-		await api.postWithEditToken(params);
+	const params: ApiImportParams = {
+		action: 'import',
+		format: 'json',
+		assignknownusers: true,
+		interwikipage: pageName,
+		interwikiprefix: iwprefix,
+		interwikisource: iwprefix as NonNullable<ApiImportParams['interwikisource']>,
+		summary: `［${summary}］`,
+	};
+	await api.postWithEditToken(params);
 
-		toastifyInstance.hideToast();
-		toastifyInstance = toastify(
-			{
-				text: `页面导入完成：${pageName}`,
-				duration: -1,
-			},
-			'success'
-		);
-	} catch {}
+	toastifyInstance.hideToast();
+	toastifyInstance = toastify(
+		{
+			text: `页面导入完成：${pageName}`,
+			duration: -1,
+		},
+		'success'
+	);
 };
 
 const uploadFile = async (target: string, url?: string): Promise<void> => {
@@ -70,50 +68,45 @@ const uploadFile = async (target: string, url?: string): Promise<void> => {
 		'info'
 	);
 
-	try {
-		const uploadParams: ApiUploadParams = {
-			url: url ?? `https://zh.wikipedia.org/wiki/Special:Redirect/file/${mw.util.rawurlencode(target)}`,
-			action: 'upload',
-			format: 'json',
-			filename: target,
-			comment: '自其他网站迁移文件',
-			ignorewarnings: true,
-		};
-		await api.postWithEditToken(uploadParams);
+	const uploadParams: ApiUploadParams = {
+		url: url ?? `https://zh.wikipedia.org/wiki/Special:Redirect/file/${mw.util.rawurlencode(target)}`,
+		action: 'upload',
+		format: 'json',
+		filename: target,
+		comment: '自其他网站迁移文件',
+		ignorewarnings: true,
+	};
+	await api.postWithEditToken(uploadParams);
 
-		toastifyInstance.hideToast();
-		toastifyInstance = toastify(
-			{
-				text: `文件迁移完成：${target}`,
-				duration: -1,
-			},
-			'success'
-		);
-	} catch {}
+	toastifyInstance.hideToast();
+	toastifyInstance = toastify(
+		{
+			text: `文件迁移完成：${target}`,
+			duration: -1,
+		},
+		'success'
+	);
 };
 
 const queryImageInfo = async (titles: string | string[]) => {
-	try {
-		const queryParams: ApiQueryInfoParams & ApiQueryImageInfoParams = {
-			action: 'query',
-			format: 'json',
-			formatversion: '2',
-			prop: ['imageinfo', 'info'],
-			iiprop: ['url'],
-			titles,
-			redirects: true,
-		};
-		const response = await api.post(queryParams);
+	const queryParams: ApiQueryInfoParams & ApiQueryImageInfoParams = {
+		action: 'query',
+		format: 'json',
+		formatversion: '2',
+		prop: ['imageinfo', 'info'],
+		iiprop: ['url'],
+		titles,
+		redirects: true,
+	};
+	const response = await api.post(queryParams);
 
-		return response;
-	} catch {}
-
-	return {};
+	return response;
 };
 
 const detectIfFileRedirect: DetectIfFileRedirect = async (pageNames, isFileNS = false) => {
 	pageNames = generateArray(pageNames);
 	const promises: (() => Promise<void>)[] = [];
+	const tos: string[] = [];
 
 	for (let i: number = 0; i < (pageNames.length + 50) / 50; i++) {
 		promises[promises.length] = async (): Promise<void> => {
@@ -152,11 +145,17 @@ const detectIfFileRedirect: DetectIfFileRedirect = async (pageNames, isFileNS = 
 				await importPage(title, 'zhwiki', isFileNS);
 			}
 
+			//// Push redirect targets into array
+			if (response['query'].redirects) {
+				for (const {to} of response['query'].redirects as {from: string; to: string}[]) {
+					tos[tos.length] = to;
+				}
+			}
+
 			// Analyze step 2: for files, check if it is a redirect
-			let response2;
 			if (isFileNS) {
 				//// Query
-				response2 = await queryImageInfo(titles);
+				const response2 = await queryImageInfo(titles);
 				if (!response2['query']) {
 					return;
 				}
@@ -182,24 +181,15 @@ const detectIfFileRedirect: DetectIfFileRedirect = async (pageNames, isFileNS = 
 						await uploadFile(title, page2.imageinfo[0].url as string);
 					}
 				}
+
+				if (response2['query'].redirects) {
+					for (const {to} of response2['query'].redirects as {from: string; to: string}[]) {
+						tos[tos.length] = to;
+					}
+				}
 			}
 
 			// Analyze step 3: import pages as redirect target
-			const tos: string[] = [];
-
-			//// Push redirect targets into array
-			if (response['query'].redirects) {
-				for (const {to} of response['query'].redirects as {from: string; to: string}[]) {
-					tos[tos.length] = to;
-				}
-			}
-
-			if (response2 && response2['query'].redirects) {
-				for (const {to} of response2['query'].redirects as {from: string; to: string}[]) {
-					tos[tos.length] = to;
-				}
-			}
-
 			//// Queue requests to import redirect targets
 			await detectIfFileRedirect(tos);
 		};
