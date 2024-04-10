66f508042d672adc98bdc7ce4f2716b6d6e14793
WaitSpring <me@waitspring.com>
2024-04-10 22:35:10 +0800

    fix(navframe, collapsible): follow #1269

--- src/Collapsible/modules/navFrame.ts
+++ src/Collapsible/modules/navFrame.ts
@@ -1,16 +1,14 @@
 const navFrame = ($content: JQuery): void => {
-	for (const element of $content.find<HTMLDivElement>('.NavFrame')) {
-		if (!element.classList.contains('collapsible')) {
-			element.classList.add('collapsible');
-		}
+	for (const element of $content.find<HTMLSpanElement>('.NavToggle, .toggleShow, .toggleHide, .NavEnd')) {
+		element.remove();
 	}
 
-	for (const element of $content.find<HTMLDivElement>('.NavHead')) {
-		if (!element.querySelectorAll<HTMLElement>('.collapsible-toggle-append-here')) {
-			const toggle = document.createElement('span');
-			toggle.classList.add('collapsible-toggle-append-here');
-			element.append(toggle);
+	for (const element of $content.find<HTMLDivElement>('.NavFrame')) {
+		if (element.classList.contains('collapsible')) {
+			continue;
 		}
+
+		element.classList.add('collapsible');
 	}
 };
 
