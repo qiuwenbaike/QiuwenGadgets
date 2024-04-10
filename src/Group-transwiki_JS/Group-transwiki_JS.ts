f50d3afb01d5bcd860bdf7ebd14259f6b2334678
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-04-10 09:38:50 +0800

    refactor(Group-transwiki_JS): remove getBody() (#1268)
    * fix(Group-*): split Group-transwiki_JS; optimize logic
    
    * refactor(Group-transwiki_JS): remove getBody()
    
    ---------
    
    Signed-off-by: WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>

--- src/Group-transwiki_JS/Group-transwiki_JS.ts
+++ src/Group-transwiki_JS/Group-transwiki_JS.ts
@@ -1,7 +1,6 @@
 import * as OPTIONS from './options.json';
 import {fixCheckbox} from './modules/fixCheckbox';
 import {fixSummary} from './modules/fixSummary';
-import {getBody} from 'ext.gadget.Util';
 
 (function userJS(): void {
 	const {configKey} = OPTIONS;
@@ -13,10 +12,8 @@ import {getBody} from 'ext.gadget.Util';
 	// Set guard
 	mw.config.set(configKey, true);
 
-	void getBody().then(($body: JQuery<HTMLBodyElement>): void => {
-		/* 取消复选框 */
-		fixCheckbox();
-		/* 修改编辑摘要 */
-		fixSummary($body);
-	});
+	/* 取消复选框 */
+	fixCheckbox();
+	/* 修改编辑摘要 */
+	fixSummary();
 })();
