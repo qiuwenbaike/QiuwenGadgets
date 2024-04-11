5ae4c6596ea96222e4f413d757ebf98f7ac9eb15
WaitSpring <me@waitspring.com>
2024-04-11 23:33:32 +0800

    fix(ShortURL): fix fullLink

--- src/ShortURL/ShortURL.ts
+++ src/ShortURL/ShortURL.ts
@@ -2,23 +2,29 @@ import {checkRevisionPage} from './modules/util/checkRevisionPage';
 import {getDomains} from './modules/util/getDomains';
 import {processId} from './modules/processId';
 
-const {wgAction, wgArticleId, wgNamespaceNumber} = mw.config.get();
+(function shortURL() {
+	const {wgAction, wgArticleId, wgNamespaceNumber} = mw.config.get();
 
-const domains = getDomains();
+	const domains = getDomains();
 
-if (domains.length > 0 && wgAction === 'view' && wgArticleId && wgNamespaceNumber >= 0) {
-	mw.hook('wikipage.content').add(function shortURL($content): void {
-		if ($content.attr('id') !== 'mw-content-text') {
-			return;
-		}
+	if (!domains.length || wgNamespaceNumber < 0) {
+		return;
+	}
 
-		const isRevisionPage: boolean = checkRevisionPage($content);
+	if (wgAction === 'view' && wgArticleId) {
+		mw.hook('wikipage.content').add(($content): void => {
+			if ($content.attr('id') !== 'mw-content-text') {
+				return;
+			}
 
-		processId(isRevisionPage, {
-			articleId: mw.config.get('wgArticleId'),
-			diffId: mw.config.get('wgDiffNewId'),
-			oldId: mw.config.get('wgDiffOldId'),
-			revisionId: mw.config.get('wgRevisionId'),
+			const isRevisionPage: boolean = checkRevisionPage($content);
+
+			processId(isRevisionPage, {
+				articleId: mw.config.get('wgArticleId'),
+				diffId: mw.config.get('wgDiffNewId'),
+				oldId: mw.config.get('wgDiffOldId'),
+				revisionId: mw.config.get('wgRevisionId'),
+			});
 		});
-	});
-}
+	}
+})();
