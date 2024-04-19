739163b8c8ae835ab108d58d163c3f76c0f13460
WaitSpring <me@waitspring.com>
2024-04-19 22:45:39 +0800

    chore(DefaultSummaries, PreviewWithVariant): use \ instead

--- src/DefaultSummaries/modules/processWikiEditor.ts
+++ src/DefaultSummaries/modules/processWikiEditor.ts
@@ -1,7 +1,7 @@
 import * as OPTIONS from '~/DefaultSummaries/options.json';
 import {generateSummaryDropdown} from './util/generateSummaryDropdown';
 
-const processWikiEditor = ($body: JQuery<HTMLBodyElement>): void => {
+const processWikiEditor = ($editForm: JQuery<HTMLElement>): void => {
 	// Guard against double inclusions
 	if (mw.config.get(OPTIONS.configKey)) {
 		return;
@@ -10,19 +10,19 @@ const processWikiEditor = ($body: JQuery<HTMLBodyElement>): void => {
 	// Set guard
 	mw.config.set(OPTIONS.configKey, true);
 
-	const $editCheckboxes: JQuery = $body.find('.editCheckboxes');
+	const $editCheckboxes: JQuery = $editForm.find('.editCheckboxes');
 	if (!$editCheckboxes.length) {
 		return;
 	}
 
-	const $dropdowns: JQuery = generateSummaryDropdown($body.find('input[name=wpSummary]'));
+	const $dropdowns: JQuery = generateSummaryDropdown($editForm.find('input[name=wpSummary]'));
 
 	$dropdowns.css({
 		'padding-bottom': '1em',
 		width: '48%',
 	});
 
-	if (!$body.find(`#${OPTIONS.dropdownId}`).length) {
+	if (!$editForm.find(`#${OPTIONS.dropdownId}`).length) {
 		$editCheckboxes.before($dropdowns);
 	}
 };
--- src/PreviewWithVariant/PreviewWithVariant.ts
+++ src/PreviewWithVariant/PreviewWithVariant.ts
@@ -1,7 +1,7 @@
 import {processWikiEditor} from './modules/processWikiEditor';
 
 (function previewWithVariants(): void {
-	mw.hook('wikipage.editform').add((): void => {
-		processWikiEditor();
+	mw.hook('wikipage.editform').add(($editForm): void => {
+		processWikiEditor($editForm);
 	});
 })();
