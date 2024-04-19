cf394deb9d855eb0d6dd72628f33b9c1855fb052
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-04-19 22:46:46 +0800

    chore(DefaultSummaries, PreviewWithVariant): use $editForm instead (#1313)
    * chore(DefaultSummaries, PreviewWithVariant): use $editForm instead

--- src/DefaultSummaries/DefaultSummaries.ts
+++ src/DefaultSummaries/DefaultSummaries.ts
@@ -4,8 +4,8 @@ import {processVisualEditor} from './modules/processVisualEditor';
 import {processWikiEditor} from './modules/processWikiEditor';
 
 void getBody().then(function defaultSummaries($body: JQuery<HTMLBodyElement>): void {
-	mw.hook('wikipage.editform').add((): void => {
-		processWikiEditor($body);
+	mw.hook('wikipage.editform').add(($editForm): void => {
+		processWikiEditor($editForm);
 	});
 
 	mw.hook('ve.saveDialog.stateChanged').add((): void => {
