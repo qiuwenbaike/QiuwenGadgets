d980e3f9625550fd3d545210c7bf163550b0ac37
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-04-19 22:22:07 +0800

    refactor(Wikicache): optimize logic (follow #1309) (#1311)
    * refactor(Wikicache): optimize logic (follow #1309)

--- src/Wikicache/Wikicache.ts
+++ src/Wikicache/Wikicache.ts
@@ -1,19 +1,12 @@
 import * as OPTIONS from './options.json';
-import {getCache} from './modules/getCache';
-import {getCacheKey} from './modules/getCacheKey';
-import {setCache} from './modules/setCache';
+import {getCache, setCache} from './modules/cache';
 
-(function wikicache(): void {
+(async function wikicache(): Promise<void> {
 	if (mw.config.get(OPTIONS.configKey)) {
 		return;
 	}
 	mw.config.set(OPTIONS.configKey, true);
 
-	const cacheKey = getCacheKey();
-
-	void getCache(cacheKey);
-
-	void (async () => {
-		await setCache(cacheKey);
-	})();
+	await getCache();
+	await setCache();
 })();
