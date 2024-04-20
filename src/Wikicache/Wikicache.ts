d9ca71a06c772872e508ea150d2413e88d165587
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-04-20 18:18:25 +0800

    fix(wikicache): guard against dobule inclusions (#1314)
    * fix(wikicache): guard against dobule inclusions

--- src/Wikicache/Wikicache.ts
+++ src/Wikicache/Wikicache.ts
@@ -1,16 +1,6 @@
-import * as OPTIONS from './options.json';
 import {getCache, setCache} from './modules/cache';
 
-(function wikicache(): void {
-	if (mw.config.get(OPTIONS.configKey)) {
-		return;
-	}
-	mw.config.set(OPTIONS.configKey, true);
-
-	mw.hook('wikipage.editform').add(($editForm): void => {
-		void (async () => {
-			await getCache({$editForm});
-			await setCache({$editForm});
-		})();
-	});
-})();
+mw.hook('wikipage.editform').add(async ($editForm): Promise<void> => {
+	await getCache({$editForm});
+	await setCache({$editForm});
+});
