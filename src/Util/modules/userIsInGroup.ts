8d2eda66a1631c0843a8f708869475cde6c2b8cb
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-04-30 14:01:07 +0800

    fix: replace with userIsInGroup (#1334)

--- src/Util/modules/userIsInGroup.ts
+++ src/Util/modules/userIsInGroup.ts
@@ -2,7 +2,7 @@ type UserIsInGroup = typeof userIsInGroup;
 
 const userIsInGroup = (group: string) => {
 	const {wgUserGroups, wgGlobalGroups} = mw.config.get();
-	return !!(wgUserGroups?.includes(group) || (wgGlobalGroups as string[])?.includes(group));
+	return wgUserGroups?.includes(group) || (wgGlobalGroups as string[])?.includes(group);
 };
 
 export {type UserIsInGroup, userIsInGroup};
