f3236ae3f221cab82d4353214c3a88cd1cf70bf7
WaitSpring <me@waitspring.com>
2024-05-24 16:02:18 +0800

    refactor(QueryContributors): add seperator and user page link

--- src/QueryContributors/QueryContributors.ts
+++ src/QueryContributors/QueryContributors.ts
@@ -1,25 +1,37 @@
 import {getBody} from 'ext.gadget.Util';
 import {getContributors} from './modules/getContributors';
 import {getMessage} from './modules/i18n';
+import {sanitize} from './modules/sanitize';
 
 void getBody().then(async ($body: JQuery<HTMLBodyElement>) => {
-	const {wgAction, wgArticleId, wgPageName} = mw.config.get();
+	const {wgAction, wgArticleId, wgCurRevisionId, wgPageName, wgRevisionId} = mw.config.get();
 
-	if (!wgPageName) {
+	if (wgAction !== 'view' || !wgArticleId || !wgPageName) {
 		return;
 	}
 
-	if (wgAction !== 'view' || !wgArticleId) {
+	if (wgCurRevisionId > wgRevisionId) {
 		return;
 	}
 
-	const contributors = await getContributors(wgPageName);
+	const contributors: string[] = await getContributors(wgPageName);
 
-	$body
-		.find('#footer-info-lastmod')
-		.append(
-			$('<span>').text(
-				getMessage('Based on contributions of').replace('$1', contributors.join(getMessage('Seperator')))
-			)
+	const $contributorsList: JQuery[] = contributors.map((userName, index) => {
+		return $('<span>').append(
+			$('<a>')
+				.attr('href', mw.util.getUrl(`User:${sanitize(userName)}`))
+				.text(userName),
+			index < contributors.length - 1 ? getMessage('Seperator') : ''
 		);
+	});
+
+	const lastMod = $body.find('#footer-info-lastmod');
+
+	if (lastMod) {
+		lastMod.append(
+			$('<span>').append(getMessage('Based on contributions of')),
+			$contributorsList,
+			getMessage('Period')
+		);
+	}
 });
