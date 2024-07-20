08d6a7cd23943b7ce44e25bbb27c477c130f21a3
WaitSpring <me@waitspring.com>
2024-07-20 17:49:54 +0800

    chore: replace Qiuwen:首页 with wgIsMainPage variable

--- src/PagePatroller/PagePatroller.ts
+++ src/PagePatroller/PagePatroller.ts
@@ -1,7 +1,7 @@
 import {pagePatroller} from './modules/core';
 
-const {wgNamespaceNumber, wgPageName, wgArticleId} = mw.config.get();
+const {wgNamespaceNumber, wgArticleId, wgIsMainPage} = mw.config.get();
 
-if (wgNamespaceNumber >= 0 && wgPageName !== 'Qiuwen:首页' && wgArticleId > 0) {
+if (wgNamespaceNumber >= 0 && !wgIsMainPage && wgArticleId > 0) {
 	void pagePatroller();
 }
