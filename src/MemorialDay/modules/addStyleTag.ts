7d50ddf72d725535b2ed9ba0b022b55965ab972a
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-08-16 07:34:09 +0800

    chore(MemorialDay, Wikiplus): remove support of IE and Edge(old) (#1481)
    * chore(MemorialDay, Wikiplus): remove support of IE and Edge(old)

--- src/MemorialDay/modules/addStyleTag.ts
+++ src/MemorialDay/modules/addStyleTag.ts
@@ -3,7 +3,6 @@ const styles = `.skin-citizen .mw-body,
 .skin-gongbi #mw-content {
 	-webkit-filter: grayscale(1) !important;
 	-moz-filter: grayscale(1) !important;
-	-ms-filter: grayscale(1) !important;
 	-o-filter: grayscale(1) !important;
 	filter: grayscale(1) !important;
 }`;
