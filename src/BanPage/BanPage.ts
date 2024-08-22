aebaf35d3b9f3ee38ad2ea44f5b42fc6abe8f491
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-08-22 22:11:48 +0800

    chore(BanPage): optimize logic (#1488)
    * chore(BanPage): optimize logic

--- src/BanPage/BanPage.ts
+++ src/BanPage/BanPage.ts
@@ -3,6 +3,6 @@ import {getBody} from 'ext.gadget.Util';
 
 const {wgNamespaceNumber} = mw.config.get();
 
-if (wgNamespaceNumber >= 0) {
+if (wgNamespaceNumber >= 0 && wgNamespaceNumber !== 14) {
 	void getBody().then(addPortletLink);
 }
