e16ec48486cded20427662f4a90660c1322f3b19
WaitSpring <me@waitspring.com>
2024-08-27 20:09:32 +0800

    build(Twinkle): remove unused var; replace null with empty string; optimize logic

--- src/Twinkle/modules/twinkle.js
+++ src/Twinkle/modules/twinkle.js
@@ -450,11 +450,6 @@ import {api} from './api';
 	/**
 	 * **************** General initialization code ****************
 	 */
-	const scripturl = mw.util.getUrl(`User:${mw.config.get('wgUserName')}/twinkleoptions.js`, {
-		action: 'raw',
-		ctype: 'text/javascript',
-		happy: 'yes',
-	});
 	// Retrieve the user's Twinkle preferences (window.Twinkle.prefs)
 	Twinkle.prefs ||= {};
 	void api
@@ -468,25 +463,11 @@ import {api} from './api';
 			rvlimit: '1',
 		})
 		.then((response) => {
-			let isSuccess;
-			const content = response['query'].pages[0].revisions[0].content ?? null;
-			if (content) {
-				try {
-					// eslint-disable-next-line no-eval
-					window.eval(content);
-					isSuccess = true;
-				} catch {
-					isSuccess = false;
-				}
-			} else {
-				isSuccess = false;
-			}
-			if (!isSuccess) {
-				void mw.notify(window.wgULS('未能加载您的Twinkle参数设置', '未能載入您的Twinkle偏好設定'), {
-					type: 'error',
-					tag: 'twinkle',
-				});
-			}
+			const content = response['query'].pages[0].revisions[0].content ?? '';
+			try {
+				// eslint-disable-next-line no-eval
+				window.eval(content);
+			} catch {}
 		})
 		.fail(() => {
 			void mw.notify(window.wgULS('未能加载您的Twinkle参数设置', '未能載入您的Twinkle偏好設定'), {
