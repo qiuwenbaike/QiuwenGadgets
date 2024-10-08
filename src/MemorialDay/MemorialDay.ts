352e0fae1a017e6c726f1ca70550e5967944ff4d
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-10-08 18:54:30 +0800

    refactor(MemorialDay): optimize logic (#1524)
    * refactor(MemorialDay): optimize logic

--- src/MemorialDay/MemorialDay.ts
+++ src/MemorialDay/MemorialDay.ts
@@ -1,5 +1,5 @@
+import {generateArray, getBody} from 'ext.gadget.Util';
 import {addStyleTag} from './modules/addStyleTag';
-import {generateArray} from 'ext.gadget.Util';
 import {pageList} from './modules/pageList';
 
 const DATENOW: Date = new Date();
@@ -10,11 +10,15 @@ const DAY: number = DATENOW.getDate();
 const {wgPageName} = mw.config.get();
 
 for (const {titles, dates} of pageList) {
-	if (titles.includes(wgPageName)) {
-		const allDates = generateArray(dates);
-
-		if (allDates.includes(MONTH * 100 + DAY) || allDates.includes(YEAR * 1e4 + MONTH * 100 + DAY)) {
-			addStyleTag();
-		}
+	if (!titles.includes(wgPageName)) {
+		continue;
 	}
+
+	const allDates = generateArray(dates);
+
+	if (!allDates.includes(MONTH * 1e2 + DAY) && !allDates.includes(YEAR * 1e4 + MONTH * 1e2 + DAY)) {
+		continue;
+	}
+
+	void getBody().then(addStyleTag);
 }
--- src/MemorialDay/modules/addStyleTag.ts
+++ src/MemorialDay/modules/addStyleTag.ts
@@ -1,14 +1,10 @@
-const styles = `.skin-citizen .mw-body,
-.skin-vector .mw-body,
-.skin-gongbi #mw-content {
-	-webkit-filter: grayscale(1) !important;
-	-moz-filter: grayscale(1) !important;
-	-o-filter: grayscale(1) !important;
-	filter: grayscale(1) !important;
-}`;
+import {memorial} from './MemorialDay.module.less';
 
-const addStyleTag = () => {
-	mw.loader.addStyleTag(styles);
+const CLASS_LIST = ['.skin-citizen .mw-body', '.skin-vector .mw-body', '.skin-gongbi #mw-content'];
+
+const addStyleTag = ($body: JQuery<HTMLBodyElement>) => {
+	// eslint-disable-next-line mediawiki/class-doc
+	$body.find(CLASS_LIST.join(',')).addClass(memorial as string);
 };
 
 export {addStyleTag};
