e5f8d7599f993671716b4d5af609be2f87b16b5f
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-10-08 18:23:52 +0800

    build(MemorialDay): temp add page (#1523)
    * build(MemorialDay): temp add page

--- src/MemorialDay/modules/pageList.ts
+++ src/MemorialDay/modules/pageList.ts
@@ -7,6 +7,11 @@ const pageList: {
 		titles: ['南京大屠杀死难者国家公祭日', '侵华日军南京大屠杀遇难同胞纪念馆'],
 		dates: [1213],
 	},
+	// Im memorial of Wu Bangguo
+	{
+		titles: ['吴邦国'],
+		dates: [20_241_008, 20_241_009, 20_241_010],
+	},
 ];
 
 export {pageList};
