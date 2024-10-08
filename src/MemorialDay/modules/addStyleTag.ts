2513f93d49fb8cf8d7060ad4fb7a2fe7cf38f2b7
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-10-08 23:46:26 +0800

    fix(MemorialDay): optimize load logic (#1530)
    * fix(MemorialDay): optimize load logic

--- src/MemorialDay/modules/addStyleTag.ts
+++ src/MemorialDay/modules/addStyleTag.ts
@@ -1,13 +1,13 @@
 import {memorial} from './MemorialDay.module.less';
 
-const CLASS_LIST = [
-	'.skin-citizen .mw-body',
-	'.skin-gongbi #mw-content',
-	'.skin-vector .mw-body',
-	'.skin-vector-2022 .mw-body',
-];
-
 const addStyleTag = () => {
+	const CLASS_LIST = [
+		'.skin-citizen .mw-body',
+		'.skin-gongbi #mw-content',
+		'.skin-vector .mw-body',
+		'.skin-vector-2022 .mw-body',
+	];
+
 	for (const className of CLASS_LIST) {
 		// eslint-disable-next-line mediawiki/class-doc
 		document.querySelector(className)?.classList.add(memorial as string);
