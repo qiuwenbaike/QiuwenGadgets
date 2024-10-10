a5f957f96da18c9945a1c12321dd75749abaff46
WaitSpring <me@waitspring.com>
2024-10-10 15:39:21 +0800

    refactor(MemorialDay): optimize styles; optimize load logic

--- src/MemorialDay/modules/addStyleTag.ts
+++ src/MemorialDay/modules/addStyleTag.ts
@@ -7,10 +7,15 @@ const addStyleTag = () => {
 		'.skin-vector .mw-body',
 		'.skin-vector-2022 .mw-body',
 	];
+	const {wgIsMainPage} = mw.config.get();
 
 	for (const className of CLASS_LIST) {
 		// eslint-disable-next-line mediawiki/class-doc
 		document.querySelector(className)?.classList.add(memorial as string);
+	}
+
+	if (wgIsMainPage) {
+		void mw.loader.using(['ext.gadget.Mainpage-Mourning']);
 	}
 };
 
