79225754b1fc6154f9afc1e61e517be48bb13b5f
WaitSpring <me@waitspring.com>
2024-10-21 11:29:29 +0800

    fix(Mainpage*): turn off Mainpage-ChineseNewYear for all users

--- src/Mainpage-ChineseNewYear_JS/Mainpage-ChineseNewYear_JS.ts
+++ src/Mainpage-ChineseNewYear_JS/Mainpage-ChineseNewYear_JS.ts
@@ -1,6 +1,9 @@
+import {checkDependencies} from 'ext.gadget.Util';
 import {loadHolidayStyle} from './modules/loadHolidayStyle';
 
 (function mainpageChineseNewYearJS(): void {
+	void checkDependencies('Mainpage-ChineseNewYear', '0');
+
 	const {wgIsMainPage, wgAction} = mw.config.get();
 
 	if (wgAction !== 'view' || !wgIsMainPage) {
