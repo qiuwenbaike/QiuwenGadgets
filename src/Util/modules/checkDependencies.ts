94a67eed25ffc233749ec39afad8fdb8558e702b
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-10-21 11:28:27 +0800

    feat(Util): add mode definition to checkDependencies (#1555)
    * feat(Util): add mode definition to checkDependencies

--- src/Util/modules/checkDependencies.ts
+++ src/Util/modules/checkDependencies.ts
@@ -2,17 +2,20 @@ import {generateArray} from './generateArray';
 import {initMwApi} from './initMwApi';
 import {uniqueArray} from './uniqueArray';
 
-type CheckDependencies = (gadgetNames: string | string[]) => Promise<void>;
+type CheckDependencies = (gadgetNames: string | string[], option?: '0' | '1') => Promise<void>;
 
-const checkDependencies = async (gadgetNames: string | string[]): Promise<void> => {
+const checkDependencies = async (gadgetNames: string | string[], option: '0' | '1' = '1'): Promise<void> => {
 	const api: mw.Api = initMwApi('Util-CheckDependencies');
 	gadgetNames = uniqueArray(generateArray(gadgetNames));
 
 	for (const gadgetName of gadgetNames) {
-		if (!mw.user.options.get(`gadget-${gadgetName}`)) {
+		if (
+			(option === '0' && mw.user.options.get(`gadget-${gadgetName}`)) ||
+			(option === '1' && !mw.user.options.get(`gadget-${gadgetName}`))
+		) {
 			await api.postWithEditToken({
 				action: 'options',
-				change: `gadget-${gadgetName}=1`,
+				change: `gadget-${gadgetName}=${option}`,
 			} as ApiOptionsParams);
 			await mw.loader.using(`ext.gadget.${gadgetName}`);
 		}
