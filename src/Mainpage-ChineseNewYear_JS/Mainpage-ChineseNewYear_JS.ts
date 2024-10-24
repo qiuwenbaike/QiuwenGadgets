e803c2fa1bfbe35ced3dffd1520ad3f22240aceb
WaitSpring <me@waitspring.com>
2024-10-24 18:09:53 +0800

    refactor(Mainpage-ChineseNewYear): re-enable this gadget on other pages

--- src/Mainpage-ChineseNewYear_JS/Mainpage-ChineseNewYear_JS.ts
+++ src/Mainpage-ChineseNewYear_JS/Mainpage-ChineseNewYear_JS.ts
@@ -1,9 +1,6 @@
-import {checkDependencies} from 'ext.gadget.Util';
 import {loadHolidayStyle} from './modules/loadHolidayStyle';
 
 (function mainpageChineseNewYearJS(): void {
-	void checkDependencies('Mainpage-ChineseNewYear', '0');
-
 	const {wgIsMainPage, wgAction} = mw.config.get();
 
 	if (wgAction !== 'view' || !wgIsMainPage) {
