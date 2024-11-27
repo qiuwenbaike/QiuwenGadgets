22b806545330b2cdcd09cf8f5f19f9ba9b4f7ebe
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2024-11-27 15:49:40 +0800

    chore(WordCount): add passive: true to ontouchstart (follow #1583) (#1586)
    * follow #1583

--- src/WordCount/modules/addListener.ts
+++ src/WordCount/modules/addListener.ts
@@ -2,7 +2,8 @@ import {wordCount} from './wordCount';
 
 const addListener = ($body: JQuery<HTMLBodyElement>): void => {
 	let events: string;
-	let data: {passive?: true} = {};
+	let data: {passive?: true} | null = null;
+
 	if ('ontouchstart' in document) {
 		events = 'touchstart touchend';
 		data = {passive: true};
@@ -10,9 +11,15 @@ const addListener = ($body: JQuery<HTMLBodyElement>): void => {
 		events = 'mouseup keyup';
 	}
 
-	$body.on(events, data, (): void => {
-		wordCount($body);
-	});
+	if (data === null) {
+		$body.on(events, (): void => {
+			wordCount($body);
+		});
+	} else {
+		$body.on(events, data, (): void => {
+			wordCount($body);
+		});
+	}
 };
 
 export {addListener};
