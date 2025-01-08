0dcc4034927a24e3c5da7155c8e4ed90ec5ea355
WaitSpring <me@waitspring.com>
2025-01-08 11:04:48 +0800

    docs: update LICENSE blocks

--- src/InPageEdit/InPageEdit.ts
+++ src/InPageEdit/InPageEdit.ts
@@ -1,6 +1,9 @@
 (function inPageEditLoader(): void {
-	/**
-	 * @see {@link https://git.qiuwen.net.cn/AnYi/inpageedit-v2/src/branch/master}
+	/**!
+	 * SPDX-License-Identifier: GPL-3.0-or-later
+	 *
+	 * @name InPageEdit
+	 * @source {@link https://git.qiuwen.net.cn/AnYi/inpageedit-v2/src/branch/master}
 	 * @author 机智的小鱼君 Dragon-Fish <dragon-fish@qq.com>
 	 * @license GPL-3.0-or-later {@link https://git.qiuwen.net.cn/AnYi/inpageedit-v2/src/branch/master/LICENSE}
 	 */
--- src/Util/modules/uniqueArray.ts
+++ src/Util/modules/uniqueArray.ts
@@ -1,8 +1,10 @@
 type UniqueArray = typeof uniqueArray;
 
 const uniqueArray = function uniqueArray<T>(args: T[]): T[] {
-	/**
-	 * @see {@link https://stackoverflow.com/questions/9229645/remove-duplicate-values-from-js-array/922982}
+	/**!
+	 * SPDX-License-Identifier: CC-BY-SA-4.0
+	 *
+	 * @source {@link https://stackoverflow.com/questions/9229645/remove-duplicate-values-from-js-array/922982}
 	 * @license CC-BY-SA-4.0
 	 */
 	const result: typeof args = [];
