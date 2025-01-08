00dd6904cf9c4061662b502ea0eccbed9551ccad
WaitSpring <me@waitspring.com>
2025-01-08 11:11:46 +0800

    refactor(QueryContributors): optimize logic

--- src/QueryContributors/modules/getContributors.ts
+++ src/QueryContributors/modules/getContributors.ts
@@ -1,27 +1,5 @@
-import {api} from './api';
+import {queryContributors} from './queryContributors';
 import {uniqueArray} from 'ext.gadget.Util';
-
-const queryContributors = async (titles: string, pccontinue?: string) => {
-	const params: ApiQueryContributorsParams = {
-		action: 'query',
-		format: 'json',
-		formatversion: '2',
-		prop: 'contributors',
-		pcexcludegroup: ['bot'],
-		pclimit: 5000,
-		titles,
-		smaxage: 600,
-		maxage: 600,
-	};
-
-	if (pccontinue) {
-		params.pccontinue = pccontinue;
-	}
-
-	const data = await api.get(params);
-
-	return data;
-};
 
 const getContributors = async (titles: string) => {
 	const pclist: string[] = [];
@@ -31,13 +9,13 @@ const getContributors = async (titles: string) => {
 		const data = await queryContributors(titles, pccontinue);
 
 		try {
-			if (data['query'] && data['query'].pages) {
+			if (data['query']?.pages) {
 				for (const page of data['query'].pages) {
 					const {contributors} = page as {
 						contributors: {userid: number; name: string}[];
 					};
 
-					if (contributors && contributors[0] && contributors[0].name) {
+					if (contributors && contributors[0] && contributors[0]?.name) {
 						for (const {name} of contributors) {
 							pclist[pclist.length] = name;
 						}
@@ -47,7 +25,7 @@ const getContributors = async (titles: string) => {
 				break;
 			}
 
-			if (data['continue'] && data['continue'].pccontinue) {
+			if (data['continue']?.pccontinue) {
 				({pccontinue} = data['continue'] as {pccontinue: string});
 			} else {
 				break;
