c880231448da5e69dc5beda1402ffe62e467ae0c
WaitSpring <me@waitspring.com>
2025-03-08 17:47:59 +0800

    fix: remove unneeded prefixed CSS rules (#1740)
    * fix: remove unneeded prefixed CSS rules

--- src/FloatTOC/modules/setMwNotifyStyle.ts
+++ src/FloatTOC/modules/setMwNotifyStyle.ts
@@ -1,6 +1,6 @@
 const setMwNotifyStyle = (): HTMLStyleElement => {
 	const style: HTMLStyleElement = mw.loader.addStyleTag(
-		'.mw-notification-area{right:unset;width:auto;max-width:20em}.mw-notification{-webkit-transform:translateX(-999px);-moz-transform:translateX(-999px);transform:translateX(-999px)}.mw-notification-visible{-webkit-transform:translateX(0);-moz-transform:translateX(0);transform:translateX(0)}'
+		'.mw-notification-area{right:unset;width:auto;max-width:20em}.mw-notification{transform:translateX(-999px)}.mw-notification-visible{transform:translateX(0)}'
 	);
 
 	style.disabled = true;
