65359ce6272111139f230b4cc1bd8dd9f37ec32c
WaitSpring <me@waitspring.com>
2025-11-10 19:34:06 +0800

    fix(CAL): preload cache before perform editing

---
+++ src/Cat-a-lot/modules/getCachedKeys.ts
@@ -0,0 +1,15 @@
+import * as OPTIONS from '../options.json';
+
+const getCachedKeys = () => {
+	const variantCache: Record<string, string[]> = {};
+	for (const [key, value] of Object.entries(
+		(mw.storage as unknown as Storage)['store'] as Record<string, string[]>
+	)) {
+		if (key.startsWith(OPTIONS.storageKey)) {
+			const cacheKey = key.replace(OPTIONS.storageKey, '');
+			variantCache[cacheKey] = value;
+		}
+	}
+	return variantCache;
+};
+export {getCachedKeys};
