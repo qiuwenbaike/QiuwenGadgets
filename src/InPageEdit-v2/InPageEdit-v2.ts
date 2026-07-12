085d7622823760cf3782e6f1c8a360831f28df42
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2026-07-12 11:30:04 +0800

    fix(InPageEdit-v2): replace with the updated version (#1919)
    * fix(InPageEdit-v2): replace with the updated version

--- src/InPageEdit-v2/InPageEdit-v2.ts
+++ src/InPageEdit-v2/InPageEdit-v2.ts
@@ -5,9 +5,11 @@ import './InPageEdit-v2.less';
 	 * SPDX-License-Identifier: GPL-3.0-or-later
 	 *
 	 * @name InPageEdit
-	 * @source {@link https://git.qiuwen.net.cn/AnYi/inpageedit-v2/src/branch/master}
+	 * @source {@link https://git.qiuwen.net.cn/InterfaceAdmin/inpageedit-v2/src/branch/master}
 	 * @author 机智的小鱼君 Dragon-Fish <dragon-fish@qq.com>
-	 * @license GPL-3.0-or-later {@link https://git.qiuwen.net.cn/AnYi/inpageedit-v2/src/branch/master/LICENSE}
+	 * @license GPL-3.0-or-later {@link https://git.qiuwen.net.cn/InterfaceAdmin/inpageedit-v2/src/branch/master/LICENSE}
 	 */
-	mw.loader.load('https://gitcdn.qiuwen.net.cn/AnYi/inpageedit-v2/raw/branch/master/dist/InPageEdit.min.js');
+	mw.loader.load(
+		'https://gitcdn.qiuwen.net.cn/InterfaceAdmin/inpageedit-v2/raw/branch/master/dist/InPageEdit.min.js'
+	);
 })();
