bd912fcdc6fde59edcc2dc756c7bcdb7290baac6
叮咚叮咚 <fyjyqs2023@163.com>
2026-08-04 14:28:58 +0800

    feat(RRD): 增加监督请求提报；在reason参数内填写代码，以便模板自动转换为详细信息
    - Updated DialogInner.tsx to include new options for hiding reasons related to OS categories.
    - Enhanced i18n.ts with translations for new hide reasons: OS1 to OS4.
    - Modified submit.ts to handle new OS hide reasons in the submission logic.

--- src/RRD/modules/submit.ts
+++ src/RRD/modules/submit.ts
@@ -37,6 +37,20 @@ const edit = async (title: string, text: string, summary?: string) => {
 const submit = async (ids: string[], toHide: string, reason: string, otherReasons: string): Promise<void> => {
 	const {wgPageName} = mw.config.get();
 
+	for (const RDid of [1, 2, 3, 4, 5]) {
+		if (reason.includes(`RD${RDid}`)) {
+			reason = `RD${RDid}`;
+			break;
+		}
+	}
+
+	for (const OSid of [1, 2, 3, 4]) {
+		if (reason.includes(`OS${OSid}`)) {
+			reason = `OS${OSid}`;
+			break;
+		}
+	}
+
 	const rrdArr: string[] = [
 		'{{Revdel',
 		'|status = ',
