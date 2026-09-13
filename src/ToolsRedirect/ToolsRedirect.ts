97898e2a153f96e253217154e8eb0d847e64bbac
WaitSpring <me@waitspring.com>
2026-09-13 20:16:14 +0800

    refactor(ToolsRedirect): rewrite in Wikimedia Codex and Vue

--- src/ToolsRedirect/ToolsRedirect.ts
+++ src/ToolsRedirect/ToolsRedirect.ts
@@ -1,12 +1,17 @@
 import './ToolsRedirect.less';
 import {ToolsRedirect} from './modules/core';
 import {getBody} from 'ext.gadget.Util';
+import {mountToolsRedirect} from './modules/ui';
 import {setMessages} from './modules/messages';
 
 setMessages();
 
 void getBody().then(($body: JQuery<HTMLBodyElement>): void => {
-	ToolsRedirect.init($body);
+	mountToolsRedirect(ToolsRedirect, () => undefined);
+	ToolsRedirect.init($body, () => {
+		const trigger = document.querySelector<HTMLButtonElement>('.tools-redirect-trigger');
+		trigger?.click();
+	});
 });
 
 export {findRedirectCallback, findRedirectBySelector, setRedirectTextSuffix} from './modules/core';
