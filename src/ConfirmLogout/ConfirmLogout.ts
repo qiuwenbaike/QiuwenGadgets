263650223a419324696489569ba39de08c9016d4
WaitSpring <98642411+WaitSpringQW@users.noreply.github.com>
2026-09-14 21:33:23 +0800

    refactor(ConfirmLogout): rewrite in Codex and Vue (#1947)

--- src/ConfirmLogout/ConfirmLogout.ts
+++ src/ConfirmLogout/ConfirmLogout.ts
@@ -1,6 +1,10 @@
 import * as OPTIONS from './options.json';
 import {addListener} from './modules/addListener';
 import {getBody} from 'ext.gadget.Util';
+import {getMessage} from './modules/i18n';
+import {showConfirmDialog} from './modules/showConfirmDialog';
+import {toastify} from 'ext.gadget.Toastify';
+import {tryLogout} from './modules/tryLogout';
 
 const {wgUserName} = mw.config.get();
 
@@ -14,5 +18,20 @@ void getBody().then(function confirmLogout($body: JQuery<HTMLBodyElement>): void
 		return;
 	}
 
-	addListener($element);
+	const openConfirmDialog = showConfirmDialog(
+		(): void => {
+			const toastifyInstance: ToastifyInstance = toastify(
+				{
+					text: mw.message('logging-out-notify').parse(),
+					duration: -1,
+				},
+				'info'
+			);
+			void tryLogout(toastifyInstance);
+		},
+		$body,
+		getMessage('Confirm')
+	);
+
+	addListener($element, openConfirmDialog);
 });
