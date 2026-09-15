824cffc35c998c6a734f45ae72bdedac226affac
WaitSpring <me@waitspring.com>
2026-09-16 01:36:46 +0800

    fix(i18n): avoid cirrular dependecy

--- src/i18n/modules/util/generateDefaultFallbackList.ts
+++ src/i18n/modules/util/generateDefaultFallbackList.ts
@@ -1,6 +1,6 @@
 import * as OPTIONS from '../../options.json';
-import {isValidKey, uniqueArray} from 'ext.gadget.Util';
 import {generateLanguageCodeSplitArray} from './generateLanguageCodeSplitArray';
+import {isValidKey} from './isValidKey';
 
 const generateDefaultFallbackList = (): string[] => {
 	const documentLanguageSplitArray: string[] = generateLanguageCodeSplitArray(document.documentElement.lang);
@@ -24,7 +24,7 @@ const generateDefaultFallbackList = (): string[] => {
 		}
 	}
 
-	return uniqueArray([languageCode, OPTIONS.defaultLanguageCode]); // Replace Set with uniqueArray, avoiding core-js polyfilling
+	return [...new Set([languageCode, OPTIONS.defaultLanguageCode])];
 };
 
 export {generateDefaultFallbackList};
